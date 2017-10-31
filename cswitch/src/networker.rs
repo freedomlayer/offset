00c9fd300bdf7ed740019e007bd23727fb24580f
real <real@freedomlayer.org>
2017-10-31 19:28:47 +0200

    Initial commit of cswitch.

---
+++ cswitch/src/networker.rs
@@ -0,0 +1,123 @@
+
+
+struct NetworkerStateMachine {
+
+    // - Sqlite connection?
+    // - outgoing messages queue
+
+}
+
+impl NetworkerStateMachine {
+    // Interface with Timer:
+    // --------------------
+    fn timer_time_tick() {
+    }
+
+    // Interface with Database:
+    // -----------------------
+    
+    // - database_request_signature
+    
+    fn database_response_signature() {
+    }
+
+    // Interface with Plugin Manager:
+    // -----------------------------
+    
+    fn plugin_manager_set_neighbor_capacity() {
+    }
+
+    fn plugin_manager_set_neighbor_max_token_channels() {
+    }
+
+    fn plugin_manager_add_neighbor() {
+    }
+
+    fn plugin_manager_remove_neighbor() {
+    }
+
+    fn plugin_manager_get_neighbors_list() {
+    }
+
+    fn plugin_manager_indexer_announce_self() {
+    }
+
+    // - message_received
+    // - neighbors_update
+    // - invalid_neighbor_move_token
+    
+
+    // Interface with Channeler:
+    // ------------------------
+
+    fn channeler_channel_opened() {
+    }
+
+    fn channeler_channel_closed() {
+    }
+
+    // - close_channel
+    // - send_channel_message
+    
+
+    fn channeler_channel_message_received() {
+    }
+
+    // - request_add_neighbor_relation
+    
+    fn channeler_response_add_neighbor_relation() {
+    }
+
+    // - request_remove_neighbor_relation
+    
+    fn channeler_response_remove_neighbor_relation() {
+    }
+
+    // - request_neighbors_relation_list
+    
+    fn channeler_response_neighbors_relation_list() {
+    }
+
+    // - request_set_neighbor_max_token_channels
+    
+    fn channeler_response_set_neighbor_max_token_channels() {
+    }
+
+    // Interface with Indexer Client
+    // -----------------------------
+
+    // - indexer_announce
+    // - message_received
+    
+    fn indexer_client_request_send_message() {
+    }
+
+    // - response_send_message
+
+    // - notify_structure_change
+    
+
+    // Interface with Pather
+    // ---------------------
+    
+
+    fn pather_request_send_message() {
+    }
+
+    // - response_send_message
+    
+
+    // Interface with Funder
+    // ---------------------
+    
+    // - funder_request_send_funds
+    
+    fn funder_response_send_funds() {
+    }
+
+    // - message_received
+    
+    // - fund_received
+}
+
+
