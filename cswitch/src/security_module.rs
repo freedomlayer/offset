039f2d052c6f010e2c087fa35a7fa7a4cfb9564c
real <real@freedomlayer.org>
2017-11-08 15:14:29 +0200

    Began implementing Future for SecurityModule.

--- cswitch/src/security_module.rs
+++ cswitch/src/security_module.rs
@@ -1,9 +1,10 @@
 extern crate futures;
+
+use self::futures::{Future, Poll, Async, AsyncSink};
+use self::futures::sink::Sink;
 use self::futures::sync::mpsc;
 use self::futures::sync::oneshot;
 
-// use self::futures::sync::mpsc::{channel, Sender, Receiver};
-// use self::futures::sync::mpsc::{channel, Sender, Receiver};
 
 use ::inner_messages::{ToSecurityModule, FromSecurityModule};
 use ::identity::{Identity};
@@ -21,10 +22,17 @@ impl SecurityModuleHandle {
     }
 }
 
+struct PendingSend {
+    dest_index: usize,
+    value: FromSecurityModule,
+}
+
+
 struct SecurityModule<I> {
     identity: I,
-    client_endpoints: Vec<(mpsc::Sender<FromSecurityModule>, mpsc::Receiver<ToSecurityModule>)>,
     close_receiver: oneshot::Receiver<()>,
+    client_endpoints: Vec<(mpsc::Sender<FromSecurityModule>, mpsc::Receiver<ToSecurityModule>)>,
+    pending_send: Option<PendingSend>,
 }
 
 impl<I: Identity> SecurityModule<I> {
@@ -43,6 +51,7 @@ impl<I: Identity> SecurityModule<I> {
             identity,
             client_endpoints: Vec::new(),
             close_receiver: receiver,
+            pending_send: None,
         };
 
         (sm_handle, sm)
@@ -51,7 +60,8 @@ impl<I: Identity> SecurityModule<I> {
 
     /// Create a new client for the security module.
     /// Remote side is given a sender side of a channel, and a receiver side of a channel.
-    fn new_client(&mut self) -> (mpsc::Sender<ToSecurityModule>, mpsc::Receiver<FromSecurityModule>) {
+    fn new_client(&mut self) -> (mpsc::Sender<ToSecurityModule>, 
+                                 mpsc::Receiver<FromSecurityModule>) {
        let (sm_sender, client_receiver) = mpsc::channel(0);
        let (client_sender, sm_receiver) = mpsc::channel(0);
 
@@ -59,4 +69,48 @@ impl<I: Identity> SecurityModule<I> {
        (client_sender, client_receiver)
 
     }
+}
+
+enum SecurityModuleError {
+    SenderError(mpsc::SendError<FromSecurityModule>),
+}
+
+impl<I: Identity> Future for SecurityModule<I> {
+    type Item = ();
+    type Error = SecurityModuleError;
+
+    /// The main loop for SecurityModule operation.
+    /// Receives requests from clients and returns responses.
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+
+        
+        // If there is something waiting to be sent, try to send it first:
+        self.pending_send = match self.pending_send.take() {
+            None => None,
+            Some(PendingSend {dest_index, value}) => {
+                let &mut (ref mut sender, _) = &mut self.client_endpoints[dest_index];
+                match sender.start_send(value) {
+                    Ok(AsyncSink::Ready) => None,
+                    Ok(AsyncSink::NotReady(value)) => Some(PendingSend {dest_index, value}),
+                    Err(e) => {return Err(SecurityModuleError::SenderError(e))},
+                }
+            }
+        };
+
+        // Wait on all the receivers, to see if there is a message in any of them.
+        // In addition, check if close receiver has a message.
+        
+
+        for &(_, ref receiver) in &self.client_endpoints {
+            // receiver.poll
+        }
+
+        
+        // - Calculate responses for each of the requests.
+        
+        // - Send back the responses to the correct requesters.
+
+        Ok(Async::Ready(()))
+    }
+
 }
