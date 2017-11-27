cbd77406ca2302b9b75ddac9b6852347da840140
real <real@freedomlayer.org>
2017-11-27 11:41:57 +0200

    Began writing a simple test for ServiceClient. Test still doesn't pass.

--- cswitch/src/service_client.rs
+++ cswitch/src/service_client.rs
@@ -7,6 +7,7 @@ use self::futures::sink::Sink;
 
 use ::async_mutex::{AsyncMutex, AsyncMutexError};
 
+#[derive(Debug)]
 pub enum ServiceClientError {
     SendFailed,
     ReceiveFailed,
@@ -35,14 +36,17 @@ impl<S,R> ServiceClient<S,R> {
 
     pub fn request(&self, request: S) -> impl Future<Item=R, Error=ServiceClientError> {
         self.inner.acquire(|inner| {
+            println!("Acquired!");
             let ServiceClientInner { sender, receiver } = inner;
             sender.send(request)
                 .map_err(|_e: mpsc::SendError<S>| ServiceClientError::SendFailed)
                 .and_then(|sender| {
+                    println!("Sent!");
                     receiver.into_future()
                         .map_err(|(e, receiver): ((), _)| ServiceClientError::ReceiveFailed)
                         .map(|(opt_item, receiver)| (opt_item, receiver, sender))
                 }).and_then(|(opt_item, receiver, sender)| {
+                    println!("Received!");
                     let item = match opt_item {
                         Some(item) => item,
                         None => return Err(ServiceClientError::NoResponseReceived),
@@ -58,3 +62,40 @@ impl<S,R> ServiceClient<S,R> {
     }
 }
 
+
+#[cfg(test)]
+mod tests {
+    extern crate tokio_core;
+    use super::*;
+    use self::tokio_core::reactor::Core;
+
+    #[test]
+    fn test_service_client() {
+        let (sender1,receiver1) = mpsc::channel::<usize>(0);
+        let (sender2,receiver2) = mpsc::channel::<usize>(0);
+
+        let fut_inc = receiver2
+            .map(|x| {
+                println!("Increasing by 1!");
+                x + 1
+            })
+            .forward(sender1.sink_map_err(|e| {
+                println!("e = {:?}",e);
+                println!("Sink error occured!");
+                ()
+            }));
+
+        let mut core = Core::new().unwrap();
+        let handle = core.handle();
+
+        // Start the increaser future:
+        handle.spawn(fut_inc.then(|_| Ok(())));
+
+        let service_client = ServiceClient::new(sender2, receiver1);
+        assert_eq!(core.run(service_client.request(0)).unwrap(),1);
+        // assert_eq!(core.run(service_client.request(6)).unwrap(),7);
+        // assert_eq!(core.run(service_client.request(8)).unwrap(),9);
+
+        
+    }
+}
