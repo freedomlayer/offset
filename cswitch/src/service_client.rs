690320be2557b16bdf3d89cc3f06e2d68fd076cc
Kam Y. Tse <kevin.xjy@gmail.com>
2017-11-29 11:25:12 +0800

    refactor(lib): cleanup and reorganize
    1. clean some trailing whitespaces
    2. move dep: futures to the root of lib

--- cswitch/src/service_client.rs
+++ cswitch/src/service_client.rs
@@ -16,7 +16,7 @@ pub enum ServiceClientError {
 }
 
 struct ServiceClientInner<S,R> {
-    sender: mpsc::Sender<S>, 
+    sender: mpsc::Sender<S>,
     receiver: mpsc::Receiver<R>,
 }
 
@@ -99,7 +99,7 @@ mod tests {
             });
         let fut_inc = sender1
             .sink_map_err(|e| ())
-            .send_all(receiver2_inc); 
+            .send_all(receiver2_inc);
 
 
         let mut core = Core::new().unwrap();
@@ -127,7 +127,7 @@ mod tests {
             });
         let fut_inc = sender1
             .sink_map_err(|e| ())
-            .send_all(receiver2_inc); 
+            .send_all(receiver2_inc);
 
 
         let mut core = Core::new().unwrap();
