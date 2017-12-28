85a0b19792f2327ef0bc3e0e48b3dc6f274008bd
real <real@freedomlayer.org>
2017-12-28 17:23:50 +0200

    Added interface IndexerClient <-> Database.

--- cswitch/src/indexer_client/mod.rs
+++ cswitch/src/indexer_client/mod.rs
@@ -6,8 +6,10 @@ fn create_indexer_client(
     timer_receiver: mpsc::Receiver<FromTimer>,
     plugin_manager_receiver: mpsc::Receiver<PluginManagerToIndexerClient>,
     plugin_manager_sender: mpsc::Sender<IndexerClientToPluginManager>,
-    funder_receiver: mpsc::Funder<FunderToIndexerClient>,
-    funder_sender: mpsc::Funder<IndexerClientToFunder>)
+    funder_receiver: mpsc::Receiver<FunderToIndexerClient>,
+    funder_sender: mpsc::Sender<IndexerClientToFunder>
+    database_receiver: mpsc::Receiver<DatabaseToIndexerClient>,
+    database_sender: mpsc::Sender<IndexerClientToDatabase>)
         -> (CloseHandle, IndexerClient) 
 {
 }
