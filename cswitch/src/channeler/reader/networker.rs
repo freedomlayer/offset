5643899761c78585e717c3417737f2a31f3ecd9b
real <real@freedomlayer.org>
2018-01-18 18:04:10 +0200

    Added some CR comments.

--- cswitch/src/channeler/reader/networker.rs
+++ cswitch/src/channeler/reader/networker.rs
@@ -18,16 +18,20 @@ use crypto::identity::PublicKey;
 pub enum NetworkerReaderError {
     MessageReceiveFailed,
     RemoteCloseHandleClosed,
+    // TODO CR: I think that this is error is unused:
     FutMutex,
 }
 
 impl From<oneshot::Canceled> for NetworkerReaderError {
+    // TODO CR: Unless we know about a specific performance problem with this function, I don't
+    // think that we need to tell the compiler to inline this.
     #[inline]
     fn from(_e: oneshot::Canceled) -> NetworkerReaderError {
         NetworkerReaderError::RemoteCloseHandleClosed
     }
 }
 
+// TODO CR: What happens if we don't have this? I thought that all Futures have this by default.
 #[must_use = "futures do nothing unless polled"]
 pub struct NetworkerReader {
     handle: Handle,
@@ -42,10 +46,16 @@ pub struct NetworkerReader {
 }
 
 impl NetworkerReader {
+    // TODO CR:
+    // Maybe we should rename this function? The Rust convention is that Object::new() returns
+    // Object type, but here we return a tuple.
+    
     // Create a new `NetworkerReader`, return a new `NetworkerReader` with its `CloseHandle`.
     pub fn new(
         handle: Handle,
         receiver: mpsc::Receiver<NetworkerToChanneler>,
+        // TODO CR: We can probably alias the neighbors type or give it its own type, in order to
+        // avoid repeating this type.
         neighbors: AsyncMutex<HashMap<PublicKey, ChannelerNeighbor>>,
     ) -> (CloseHandle, NetworkerReader) {
         let (close_handle, (close_tx, close_rx)) = CloseHandle::new();
@@ -61,6 +71,7 @@ impl NetworkerReader {
         (close_handle, reader)
     }
 
+    // TODO CR: See other comments about using #[inline]
     #[inline]
     fn add_neighbor(&self, info: ChannelerNeighborInfo) {
         let task = self.neighbors
@@ -83,12 +94,16 @@ impl NetworkerReader {
         self.handle.spawn(task);
     }
 
+    // TODO CR: See other comments about using #[inline]
     #[inline]
     fn del_neighbor(&self, public_key: PublicKey) {
         let task = self.neighbors
             .acquire(move |mut neighbors| {
                 if neighbors.remove(&public_key).is_some() {
                     info!("neighbor {:?} removed", public_key);
+                    // TODO CR: Will this close all current TCP connections with the neighbor?
+                    // If there are any Channels of communication with this neighbor 
+                    // currently working, do we need to close them here?
                 } else {
                     info!("nonexistent neighbor: {:?}", public_key);
                 }
@@ -98,9 +113,33 @@ impl NetworkerReader {
                 info!("failed to remove neighbor");
             });
 
+
+        // TODO CR: I don't think that we need to spawn a task here.
+        // Here are my reasons:
+        //
+        // 1. del_neighbor barely does anything async related.  Theoretically we don't even need
+        //    AsyncMutex over neighbors here, as all of the futures that touch it live in the same
+        //    thread, and it is never possible for two of them to run at the same time.
+        //
+        // 2. We lose control over the order in which things happen. It makes it hard for me to reason
+        //    about the order that things happen. For example: Assume that the Networker has just
+        //    sent two requests, one to delete a neighbor and one to add a neighbor. If we spawn
+        //    tasks to do those things, we have the risk of doing the two tasks (adding and
+        //    deleting a neighbor) in the wrong order.
+        //
+        // Instead, I think that we have the following options:
+        //
+        // 1. Use combinators. We wait until the acquiring of neighbors and its modification is
+        //    done, and only then we move on to process the next message. This could be done as a
+        //    pattern of applying for_each over a Stream of events.
+        //
+        // 2. Keep the current operation in progress inside the state machine. We won't start doing
+        //    other operations until the current operation is not done.
+        //
         self.handle.spawn(task);
     }
 
+    // TODO CR: See other comments about using #[inline]
     #[inline]
     fn send_message(&self, index: u32, public_key: PublicKey, content: Bytes) {
         let task = self.neighbors
@@ -108,7 +147,7 @@ impl NetworkerReader {
                 match neighbors.get_mut(&public_key) {
                     None => {
                         warn!(
-                            "nonexistent neighbor: {:?}, message would be discard",
+                            "nonexistent neighbor: {:?}, message will be discarded",
                             public_key
                         );
                     }
@@ -116,7 +155,7 @@ impl NetworkerReader {
                         match neighbor.channels.get_mut(&index) {
                             None => {
                                 warn!(
-                                    "unknown channel index: {:?}, message would be discard",
+                                    "unknown channel index: {:?}, message will be discarded",
                                     index
                                 );
                             }
@@ -137,9 +176,31 @@ impl NetworkerReader {
                 info!("failed to pass send message request to channel");
             });
 
+        // TODO CR: I think that here spawning a task is actually the correct thing to do , because
+        // it is possible that the neighbor sender is blocked, and we don't want to block the whole
+        // NetworkerReader Future because of that. 
+        //
+        // However, it seems like we allocate a new task for every incoming message. This could be
+        // a very big overhead. I have an idea of how to fix this, but it requires a small
+        // change to the messages interface between Channeler and Networker.
+        //
+        // Refactor Proposal
+        // -----------------
+        //
+        // Whenever a new channel is opened, Channeler will send Networker a ChannelEvent::Opened.
+        // ChannelEvent::Opened will contain an mpsc Sender and an mpsc Receiver.
+        // The Receiver will allow receiving messages from the Channel. The Sender will allow
+        // sending messages to the Channel.
+        // 
+        // Using this method the Networker will be able to experience backpressure for every new
+        // Channel separately, and we won't need to spawn a new task for every message.
+        //
+        // Please tell me you opinion about this, you might have an idea to improve this.
+        //
         self.handle.spawn(task);
     }
 
+    // TODO CR: See other comments about using #[inline]
     #[inline]
     fn set_max_channels(&self, public_key: PublicKey, max_channels: u32) {
         let task = self.neighbors
@@ -158,10 +219,16 @@ impl NetworkerReader {
                 info!("failed to set maximum amount of channels");
             });
 
+        // TODO CR: We also need to close at this point all current TCP connections that have an
+        // index larger than max_channels (This could happen if we lowered max_channels).
+
+        // TODO CR: I don't think that we need to spawn a task here.
+        // See earlier comments about this.
         self.handle.spawn(task);
     }
 
     // TODO: Consume all message before closing actually.
+    // TODO CR: See other comments about using #[inline]
     #[inline]
     fn close(&mut self) {
         self.inner_rx.close();
@@ -185,6 +252,7 @@ impl Future for NetworkerReader {
     fn poll(&mut self) -> Poll<(), Self::Error> {
         trace!("poll - {:?}", ::std::time::Instant::now());
 
+        // Check if we have received a request to close:
         match self.close_rx.poll()? {
             Async::NotReady => (),
             Async::Ready(()) => {
