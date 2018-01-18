d0aab3aabf1c2146cc6fe5029569a893fe80c3c2
real <real@freedomlayer.org>
2018-01-18 19:24:56 +0200

    Added some CR comments.

--- cswitch/src/channeler/reader/timer.rs
+++ cswitch/src/channeler/reader/timer.rs
@@ -23,16 +23,30 @@ pub enum TimerReaderError {
     TimerReceiveFailed,
     RemoteCloseHandleClosed,
     SendToNetworkerFailed,
+    // TODO CR: I think that this error type is not used:
     FutMutex,
 }
 
+// TODO CR: Maybe we should produce the TimerReaderError::RemoteCloseHandleClosed at the right
+// place, instead of having it here?
+// I could understand this kind of error conversion if it meant something generic, but it seems
+// like this conversion means something local to a specific place in the code. It will probably be
+// easier to understand what happens here if this code will be moved to the relevant place.
+//
+// As an example, what if we had more than one oneshots used inside the TimerReader in the future?
+// For one of those oneshots we will have TimerReaderError::RemoteOneshot1Closed, and for the
+// other one we will have TimerReaderError::RemoteOneshot2Closed. We won't be able to use the impl
+// From<oneshot::Canceled> for TimerReaderError> in that case.
 impl From<oneshot::Canceled> for TimerReaderError {
+    // TODO CR: I don't think that we need the #[inline] hint here. 
+    // See my other comments about this.
     #[inline]
     fn from(_e: oneshot::Canceled) -> TimerReaderError {
         TimerReaderError::RemoteCloseHandleClosed
     }
 }
 
+// TODO CR: What happens if we don't have the must_use here?
 #[must_use = "futures do nothing unless polled"]
 pub struct TimerReader {
     handle: Handle,
@@ -49,11 +63,17 @@ pub struct TimerReader {
 }
 
 impl TimerReader {
+    // TODO CR: Maybe we should change the name of this function to something else?
+    // We might be going against the Rust convention by having Object::new() return something that
+    // is not of type Object. We might be able to pick a different name to this function. What do
+    // you think?
     pub fn new(
         handle: Handle,
         timer_receiver: mpsc::Receiver<FromTimer>,
         networker_sender: mpsc::Sender<ChannelerToNetworker>,
         sm_client: SecurityModuleClient,
+        // TODO CR: I think that we should have an alias for this neighbors type, or define a new
+        // type (structure) for this, as we type it in many places inside the Channeler code.
         neighbors: AsyncMutex<HashMap<PublicKey, ChannelerNeighbor>>,
     ) -> (CloseHandle, TimerReader) {
         let (close_handle, (close_tx, close_rx)) = CloseHandle::new();
@@ -71,6 +91,8 @@ impl TimerReader {
         (close_handle, timer_reader)
     }
 
+    // TODO CR: I don't think that we need the #[inline] hint here. 
+    // See my other comments about this.
     #[inline]
     fn retry_conn(&self) {
         let handle_for_task = self.handle.clone();
@@ -78,12 +100,16 @@ impl TimerReader {
         let sm_client_for_task = self.sm_client.clone();
         let networker_sender_for_task = self.inner_tx.clone();
 
+        // TODO CR: We acquire neighbors for too long here.
         let retry_conn_task = self.neighbors
             .acquire(move |mut neighbors| {
                 let mut need_retry = Vec::new();
 
                 for (neighbor_public_key, mut neighbor) in &mut neighbors.iter_mut() {
                     if neighbor.info.socket_addr.is_some() && neighbor.num_pending == 0 {
+                        // TODO CR: Maybe we should move the logic of this scope to a separate
+                        // function? It seems like this part tries to decide whether we should
+                        // continue, or execute the next code after this scope ends.
                         match neighbor
                             .channels
                             .len()
@@ -105,8 +131,9 @@ impl TimerReader {
                             }
                         }
 
+                        // TODO CR: We can implement this logic as a separate function
                         let mut channel_index: Option<u32> = None;
-                        // Find the minimum index which isn't is used
+                        // Find the minimum index which isn't in use
                         for index in 0..neighbor.info.max_channels {
                             if neighbor.channels.get(&index).is_none() {
                                 channel_index = Some(index);
@@ -117,16 +144,27 @@ impl TimerReader {
                         if let Some(index) = channel_index {
                             let addr = neighbor.info.socket_addr.clone().unwrap();
                             need_retry.push((addr, neighbor_public_key.clone(), index));
+                            // TODO CR: Is this the right place to increase num_pending? Maybe we
+                            // should do it right before we open the new Channel? I need to read
+                            // the rest of the code in this file to be sure about this.
                             neighbor.num_pending += 1;
                         }
                     }
                 }
 
+                // TODO CR: At this point we don't use the acquired neighbors anymore. We probably
+                // keep the AsyncMutex acquired for too long.  We should close the scope of the
+                // acquire here, possibly use an "and_then" clause to add the next code:
+
+                // TODO CR: Move the following logic into a separate function. retry_con is pretty
+                // long, if we separate it to smaller parts we can make it easier to read and test.
                 for (addr, neighbor_public_key, index) in need_retry {
                     let neighbors = neighbors_for_task.clone();
                     let mut networker_sender = networker_sender_for_task.clone();
 
                     let new_channel = Channel::connect(
+                        // TODO CR: addr: SockAddr implements Copy, we might be able to move it without a
+                        // reference.
                         &addr,
                         &neighbor_public_key,
                         index,
@@ -138,7 +176,9 @@ impl TimerReader {
                         neighbors
                             .acquire(move |mut neighbors| {
                                 let res = match neighbors.get_mut(&neighbor_public_key) {
-                                    None => Err(ChannelError::Closed("can't find this neighbor")),
+                                    // TODO CR: Use Enum to denote closing reason instead of using
+                                    // a string.
+                                    None => Err(ChannelError::Closed("Can't find this neighbor")),
                                     Some(neighbor) => {
                                         neighbor.num_pending -= 1;
                                         match conn_result {
@@ -158,6 +198,8 @@ impl TimerReader {
                                                     Ok(channel)
                                                 }
                                             }
+                                            // TODO CR: We might be able to use the "?" syntax here
+                                            // for conn_result.
                                             Err(e) => Err(e),
                                         }
                                     }
@@ -169,13 +211,17 @@ impl TimerReader {
                                 }
                             })
                             .map_err(|e| {
-                                error!("failed to attempt a new connection: {:?}", e);
+                                // TODO CR: Can we really arrive here? Or do we have this because
+                                // of the AsyncMutex design?
+                                error!("Failed to initialize a new connection: {:?}", e);
                                 ()
                             })
                     });
 
                     let handle_for_channel = handle_for_task.clone();
 
+                    // TODO CR: Why do we spawn twice here? 
+                    // I think that we may drop the second spawn.
                     handle_for_task.spawn(new_channel.and_then(move |channel| {
                         handle_for_channel.spawn(channel.map_err(|_| ()));
                         Ok(())
@@ -188,9 +234,17 @@ impl TimerReader {
                 error!("retry conn failed");
             });
 
+        // TODO CR: I think that we don't really need a task spawn for this.
+        // retry_conn_task doesn't really block. It just acquires neighbors, this should happen
+        // instantly.
+        //
+        // We are probably better off if we wait for retry_conn_task to end instead of spawning it.
+        // What do you think?
         self.handle.spawn(retry_conn_task);
     }
 
+    // TODO CR: I don't think that we need the #[inline] hint here. 
+    // See my other comments about this.
     #[inline]
     fn broadcast_tick(&self) {
         let handle_for_task = self.handle.clone();
