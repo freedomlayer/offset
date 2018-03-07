621f83b53c5505a949eecaf3cb726c4244b0a9f4
real <real@freedomlayer.org>
2018-03-07 13:19:43 +0200

    Removed unnecessary use std::Vec.

--- src/networker/messenger/balance_state.rs
+++ src/networker/messenger/balance_state.rs
@@ -1,10 +1,9 @@
-use std::Vec;
 use std::collections::HashMap;
 use super::token_channel::TokenChannel;
 use crypto::identity::PublicKey;
 
 
-pub struct BalanceState{
+pub struct BalanceState {
     token_channels: HashMap<PublicKey, Vec<TokenChannel>>,
     local_pk: PublicKey,
 
@@ -18,17 +17,13 @@ impl BalanceState{
     pub fn app_remove_neighbor(remote_key: PublicKey){
     }
 
-
     pub fn atomic_process_trans_list(token_channel_index: u32,
                                      remote_public_key: &PublicKey,
                                      transactions: Vec<NetworkerTCTransaction>) {
-
     }
-
 }
 
 
 fn maximal_remote_pending(max_remote_debts: Vec<u64>, dest_max_remote_debt: u64,
                           src_max_remote_debt: u64) -> u64{
-
 }
