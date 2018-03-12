e17172279d6e112ac604e2509210f67e4ec8fc82
Kam Y. Tse <kevin.xjy@gmail.com>
2018-03-12 09:10:47 +0800

    Changed the signature of `DhPrivateKey::derive_symmetric_key(...)`

--- src/crypto/dh.rs
+++ src/crypto/dh.rs
@@ -1,8 +1,5 @@
 extern crate untrusted;
 
-use std::mem;
-use std::convert::TryFrom;
-
 use ring::digest;
 use ring::agreement::{self, EphemeralPrivateKey};
 use ring::rand::SecureRandom;
@@ -56,32 +53,36 @@ impl DhPrivateKey {
     }
 
     /// Derive a symmetric key from our private key and remote's public key.
-    pub fn derive_symmetric_key(&self, public_key: &DhPublicKey, salt: &Salt)
-        -> Result<SymmetricKey, CryptoError> {
-        let peer_public_key = untrusted::Input::from(&public_key);
+    pub fn derive_symmetric_key(
+        self,
+        remote_public_key: DhPublicKey,
+        sent_salt: Salt,
+        recv_salt: Salt,
+    ) -> Result<(SymmetricKey, SymmetricKey), CryptoError> {
+        let u_remote_public_key = untrusted::Input::from(&remote_public_key);
 
-        // Force a copy of our private key, so that we can use it more than once.
-        // This is a hack due to current limitation of the *ring* utils.crypto library.
-        let my_private_key: EphemeralPrivateKey = unsafe {
-            mem::transmute_copy(&self.inner)
-        };
-
-        let kdf = |shared_key: &[u8]| -> Result<SymmetricKey, CryptoError> {
+        let kdf = |shared_key: &[u8]| -> Result<(SymmetricKey, SymmetricKey), CryptoError> {
             if shared_key.len() != SHARED_SECRET_LEN {
                 Err(CryptoError)
             } else {
-                let sk = SigningKey::new(&digest::SHA512_256, &salt);
-                let info: [u8; 0] = [];
-                let mut key = [0x00u8; SYMMETRIC_KEY_LEN];
-                extract_and_expand(&sk, &shared_key, &[][..], &mut key);
+                let sent_sk = SigningKey::new(&digest::SHA512_256, &sent_salt);
+                let recv_sk = SigningKey::new(&digest::SHA512_256, &recv_salt);
 
-                SymmetricKey::try_from(&key[..]).map_err(|_| CryptoError)
+                let mut send_key = [0x00u8; SYMMETRIC_KEY_LEN];
+                let mut recv_key = [0x00u8; SYMMETRIC_KEY_LEN];
+                extract_and_expand(&sent_sk, &shared_key, &[], &mut send_key);
+                extract_and_expand(&recv_sk, &shared_key, &[], &mut recv_key);
+
+                Ok((SymmetricKey::from(&send_key), SymmetricKey::from(&recv_key)))
             }
         };
 
-        // Perform Diffie-Hellman and derive the symmetric key
-        agreement::agree_ephemeral(my_private_key, &agreement::X25519,
-                                   peer_public_key, CryptoError, kdf)
+        agreement::agree_ephemeral(
+            self.inner,
+            &agreement::X25519,
+            u_remote_public_key,
+            CryptoError, kdf,
+        )
     }
 }
 
@@ -102,24 +103,31 @@ mod tests {
     #[test]
     fn test_derive_symmetric_key() {
         let rng = DummyRandom::new(&[1, 2, 3, 4, 5]);
-        let dh_private_key1 = DhPrivateKey::new(&rng).unwrap();
-        let dh_private_key2 = DhPrivateKey::new(&rng).unwrap();
+        let dh_private_a = DhPrivateKey::new(&rng).unwrap();
+        let dh_private_b = DhPrivateKey::new(&rng).unwrap();
 
-        let public_key1 = dh_private_key1.compute_public_key().unwrap();
-        let public_key2 = dh_private_key2.compute_public_key().unwrap();
+        let public_key_a = dh_private_a.compute_public_key().unwrap();
+        let public_key_b = dh_private_b.compute_public_key().unwrap();
 
-        // Same salt for both sides:
-        let salt = Salt::new(&rng).unwrap();
+        let salt_a = Salt::new(&rng).unwrap();
+        let salt_b = Salt::new(&rng).unwrap();
 
         // Each side derives the symmetric key from the remote's public key
         // and the salt:
-        let symmetric_key1 =
-            dh_private_key1.derive_symmetric_key(&public_key2, &salt).unwrap();
+        let (send_key_a, recv_key_a) = dh_private_a.derive_symmetric_key(
+            public_key_b,
+            salt_a.clone(),
+            salt_b.clone(),
+        ).unwrap();
 
-        let symmetric_key2 =
-            dh_private_key2.derive_symmetric_key(&public_key1, &salt).unwrap();
+        let (send_key_b, recv_key_b) = dh_private_b.derive_symmetric_key(
+            public_key_a,
+            salt_b,
+            salt_a,
+        ).unwrap();
 
         // Both sides should get the same derived symmetric key:
-        assert_eq!(symmetric_key1, symmetric_key2);
+        assert_eq!(send_key_a, recv_key_b);
+        assert_eq!(send_key_b, recv_key_a)
     }
 }
