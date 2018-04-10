295883126ca826f3a5ce031aa1fe0ab6c412b166
A4Vision <asafyi@gmail.com>
2018-04-10 13:48:57 +0300

    Refactor invoice_validator, use composition.

--- src/networker/messenger/invoice_validator.rs
+++ src/networker/messenger/invoice_validator.rs
@@ -6,42 +6,83 @@ use super::token_channel::ProcessMessageError;
 #[derive(Clone)]
 pub struct InvoiceIds {
     /// The invoice id that my neighbor randomized.
-    remote_invoice_id: Option<InvoiceId>,
+    remote_invoice_id: InvoiceIdStore,
     /// The invoice id which I randomized locally.
-    local_invoice_id: Option<InvoiceId>,
+    local_invoice_id: InvoiceIdStore,
 }
 
 
-
-// TODO(a4vision): consider implementing through composition of two invoice id stores.
 impl InvoiceIds {
     pub fn new(local_invoice_id: Option<InvoiceId>, remote_invoice_id: Option<InvoiceId>)
     -> InvoiceIds {
-        InvoiceIds {remote_invoice_id, local_invoice_id}
+        InvoiceIds {
+            remote_invoice_id: InvoiceIdStore{invoice_id: remote_invoice_id},
+            local_invoice_id: InvoiceIdStore{invoice_id: local_invoice_id},
+        }
     }
 
     /// Sets the invoice id randomized  by the neighbor.
     /// Returns `true` if the invoice id was successfully set to the given value.
     pub fn set_remote_invoice_id(&mut self, invoice_id: InvoiceId) -> bool {
-        if self.remote_invoice_id.is_none(){
-            self.remote_invoice_id = Some(invoice_id);
-            true
-        }else{
-            false
-        }
+        self.remote_invoice_id.set_invoice_id(invoice_id)
     }
 
     /// Validate the signature and the invoice id of the given receipt.
     pub fn validate_receipt(&mut self, send_funds_receipt: &SendFundsReceipt,
                             public_key: &PublicKey) ->
     Result<(), ProcessMessageError> {
-        if !send_funds_receipt.verify_signature(public_key) {
+        self.local_invoice_id.validate_receipt(send_funds_receipt, public_key)
+    }
+
+    pub fn get_local_invoice_id(&self) -> &Option<InvoiceId>{
+        &self.local_invoice_id.get_invoice_id()
+    }
+
+    pub fn get_remote_invoice_id(&self) -> &Option<InvoiceId>{
+        self.remote_invoice_id.get_invoice_id()
+    }
+
+    pub fn reset_local_invoice_id(&mut self){
+        self.local_invoice_id.reset();
+    }
+
+    /// Sets the invoice id locally randomized.
+    /// Returns `true` if the invoice id was successfully set to the given value.
+    pub fn set_local_invoice_id(&mut self, invoice_id: InvoiceId) -> bool {
+        self.local_invoice_id.set_invoice_id(invoice_id)
+    }
+}
+
+#[derive(Clone)]
+struct InvoiceIdStore {
+    invoice_id: Option<InvoiceId>,
+}
+
+impl InvoiceIdStore {
+    /// Returns `true` if the invoice id was successfully set to the given value.
+    fn set_invoice_id(&mut self, new_invoice_id: InvoiceId) -> bool{
+        if self.invoice_id.is_none(){
+            self.invoice_id = Some(new_invoice_id);
+            true
+        }else{
+            false
+        }
+    }
+
+    fn get_invoice_id(&self) -> &Option<InvoiceId> {
+        &self.invoice_id
+    }
+
+    fn validate_receipt(&mut self, receipt: &SendFundsReceipt,
+                            public_key: &PublicKey) ->
+    Result<(), ProcessMessageError> {
+        if !receipt.verify_signature(public_key) {
             return Err(ProcessMessageError::InvalidFundsReceipt);
         }
 
-        match self.local_invoice_id{
-            Some(ref local_invoice_id) => {
-                if local_invoice_id != &send_funds_receipt.invoice_id {
+        match self.invoice_id{
+            Some(ref invoice_id) => {
+                if invoice_id != &receipt.invoice_id {
                     return Err(ProcessMessageError::InvalidInvoiceId);
                 }
             },
@@ -51,30 +92,10 @@ impl InvoiceIds {
         Ok(())
     }
 
-    pub fn get_local_invoice_id(&self) -> &Option<InvoiceId>{
-        &self.local_invoice_id
-    }
-
-    pub fn get_remote_invoice_id(&self) -> &Option<InvoiceId>{
-        &self.remote_invoice_id
-    }
-
-    pub fn reset_local_invoice_id(&mut self){
-        self.local_invoice_id = None;
-    }
-
-    /// Sets the invoice id randomized  by the neighbor.
-    /// Returns `true` if the invoice id was successfully set to the given value.
-    pub fn set_local_invoice_id(&mut self, invoice_id: InvoiceId) -> bool {
-        if self.local_invoice_id.is_none(){
-            self.local_invoice_id = Some(invoice_id);
-            true
-        }else{
-            false
-        }
+    fn reset(&mut self) {
+        self.invoice_id = None;
     }
 }
-
 
 
 #[cfg(test)]
