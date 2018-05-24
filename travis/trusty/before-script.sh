89922c0834ec957114207c8fe3ad31c4ff04e0f7
Kam Y. Tse <kevin.xjy@gmail.com>
2018-05-24 17:15:21 +0800

    Upgrade toolchain to 2018-05-23, and use clippy as cargo's subcommand.

--- travis/trusty/before-script.sh
+++ travis/trusty/before-script.sh
@@ -2,6 +2,9 @@
 
 set -eux -o pipefail
 
+CLIPPY_VERSION="0.0.204"
+CLIPPY_COMMIT_HASH="e7a3e03c6e"
+
 if [[ -f ".cargo/config" ]]; then
     rm .cargo/config
 elif [[ ! -d ".cargo" ]]; then
@@ -10,6 +13,14 @@ fi
 
 echo "[target.$TARGET]" > .cargo/config
 echo "linker= \"$CC\"" >> .cargo/config
+
+if [[ -f "$HOME/.cargo/bin/cargo-clippy" ]]; then
+    if [[ "$(cargo clippy --version)" -ne "$CLIPPY_VERSION" ]]; then
+        cargo install --force clippy --rev $CLIPPY_COMMIT_HASH
+    fi
+else
+    cargo install clippy --rev $CLIPPY_COMMIT_HASH
+fi
 
 cat .cargo/config
 
