8d7a709c87c2e3894cf81cad4ecd04fbe466d43b
real <real@freedomlayer.org>
2018-06-06 17:21:14 +0300

    Changes signature hash prefixes to be constants.

--- src/networker/messenger/signature_buff.rs
+++ src/networker/messenger/signature_buff.rs
@@ -6,6 +6,9 @@ use crypto::hash;
 
 use super::types::{ResponseSendMessage, FailureSendMessage, PendingNeighborRequest};
 
+const REQUEST_SUCCESS_PREFIX: &[u8] = b"REQUEST_SUCCESS";
+const REQUEST_FAILURE_PREFIX: &[u8] = b"REQUEST_FAILURE";
+
 /// Create the buffer we sign over at the Response message.
 /// Note that the signature is not just over the Response message bytes. The signed buffer also
 /// contains information from the Request message.
@@ -15,7 +18,7 @@ pub fn create_response_signature_buffer(response_send_msg: &ResponseSendMessage,
     let mut sbuffer = Vec::new();
 
     // TODO: Add a const for this:
-    sbuffer.extend_from_slice(&hash::sha_512_256(b"REQUEST_SUCCESS"));
+    sbuffer.extend_from_slice(&hash::sha_512_256(REQUEST_SUCCESS_PREFIX));
     sbuffer.extend_from_slice(&pending_request.request_id);
     sbuffer.write_u32::<BigEndian>(pending_request.max_response_len).expect("Serialization failure!");
     sbuffer.write_u64::<BigEndian>(pending_request.processing_fee_proposal).expect("Serialization failure!");
@@ -37,7 +40,7 @@ pub fn create_failure_signature_buffer(failure_send_msg: &FailureSendMessage,
     let mut sbuffer = Vec::new();
 
     // TODO: Add a const for this:
-    sbuffer.extend_from_slice(&hash::sha_512_256(b"REQUEST_FAILURE"));
+    sbuffer.extend_from_slice(&hash::sha_512_256(REQUEST_FAILURE_PREFIX));
     sbuffer.extend_from_slice(&pending_request.request_id);
     sbuffer.write_u32::<BigEndian>(pending_request.max_response_len).expect("Serialization failure!");
     sbuffer.write_u64::<BigEndian>(pending_request.processing_fee_proposal).expect("Serialization failure!");
