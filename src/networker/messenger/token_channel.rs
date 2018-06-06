3491018996a87715c2e30503fd7cd296501a03c7
real <real@freedomlayer.org>
2018-06-06 16:32:23 +0300

    Reordered imports.

--- src/networker/messenger/token_channel.rs
+++ src/networker/messenger/token_channel.rs
@@ -13,14 +13,14 @@ use proto::common::SendFundsReceipt;
 use proto::funder::InvoiceId;
 use proto::networker::NetworkerSendPrice;
 
-use super::pending_neighbor_request::PendingNeighborRequest;
-use super::types::{ResponseSendMessage, FailureSendMessage, RequestSendMessage,
-                                NeighborTcOp, NeighborsRoute, PkPairPosition};
-use super::credit_calc::CreditCalculator;
 use utils::trans_hashmap::TransHashMap;
 use utils::int_convert::usize_to_u32;
 use utils::safe_arithmetic::SafeArithmetic;
 
+use super::pending_neighbor_request::PendingNeighborRequest;
+use super::types::{ResponseSendMessage, FailureSendMessage, RequestSendMessage,
+                                NeighborTcOp, NeighborsRoute, PkPairPosition};
+use super::credit_calc::CreditCalculator;
 use super::signature_buff::{create_failure_signature_buffer, 
     create_response_signature_buffer};
 
