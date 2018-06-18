18150c65edaa4e8311ae07a001c0bfde8f8b3c91
real <real@freedomlayer.org>
2018-06-18 11:29:43 +0300

    Changed 'received_move_token' to be a class method.

--- src/networker/messenger/neighbor_tc_logic.rs
+++ src/networker/messenger/neighbor_tc_logic.rs
@@ -25,25 +25,8 @@ struct ChainState {
 
 pub struct NeighborTCState {
     chain_state: ChainState,
-    token_channel: TokenChannel,
+    token_channel: Option<TokenChannel>,
 }
-
-impl NeighborTCState {
-    pub fn new_from_reset(local_public_key: &PublicKey, 
-                      remote_public_key: &PublicKey, 
-                      current_token: &ChannelToken, 
-                      balance: i64) -> NeighborTCState {
-        NeighborTCState {
-            chain_state: ChainState {
-                direction: MoveTokenDirection::Incoming,
-                old_token: current_token.clone(), // TODO: What to put here?
-                new_token: current_token.clone(),
-            },
-            token_channel: TokenChannel::new(local_public_key, remote_public_key, balance),
-        }
-    }
-}
-
 
 #[derive(Debug)]
 pub enum NeighborTCStateError {
@@ -58,64 +41,77 @@ pub enum ReceiveTokenOutput {
 }
 
 
-#[allow(unused)]
-pub fn receive_move_token(neighbor_tc_state: NeighborTCState, 
-                          move_token_message: NeighborMoveTokenInner, 
-                          new_token: ChannelToken) 
-    -> (NeighborTCState, Result<ReceiveTokenOutput, NeighborTCStateError>) {
 
+impl NeighborTCState {
+    pub fn new_from_reset(local_public_key: &PublicKey, 
+                      remote_public_key: &PublicKey, 
+                      current_token: &ChannelToken, 
+                      balance: i64) -> NeighborTCState {
+        NeighborTCState {
+            chain_state: ChainState {
+                direction: MoveTokenDirection::Incoming,
+                old_token: current_token.clone(), // TODO: What to put here?
+                new_token: current_token.clone(),
+            },
+            token_channel: Some(TokenChannel::new(local_public_key, remote_public_key, balance)),
+        }
+    }
 
-    match neighbor_tc_state.chain_state.direction {
-        MoveTokenDirection::Incoming => {
-            if new_token == neighbor_tc_state.chain_state.new_token {
-                // Duplicate
-                (neighbor_tc_state, Ok(ReceiveTokenOutput::Duplicate))
-            } else {
-                // Inconsistency
-                (neighbor_tc_state, Err(NeighborTCStateError::ChainInconsistency))
-            }
-        },
-        MoveTokenDirection::Outgoing => {
-            if move_token_message.old_token == neighbor_tc_state.chain_state.new_token {
-                match atomic_process_operations_list(neighbor_tc_state.token_channel, 
-                                                move_token_message.operations) {
-                    (token_channel, Ok(output)) => {
-                        // If processing the transactions was successful, we 
-                        // set old_token, new_token and direction:
-                        let neighbor_tc_state = NeighborTCState {
-                            chain_state: ChainState {
-                                old_token: neighbor_tc_state.chain_state.new_token.clone(),
+    #[allow(unused)]
+    pub fn receive_move_token(&mut self, 
+                              move_token_message: NeighborMoveTokenInner, 
+                              new_token: ChannelToken) 
+        -> Result<ReceiveTokenOutput, NeighborTCStateError> {
+
+        match self.chain_state.direction {
+            MoveTokenDirection::Incoming => {
+                if new_token == self.chain_state.new_token {
+                    // Duplicate
+                    Ok(ReceiveTokenOutput::Duplicate)
+                } else {
+                    // Inconsistency
+                    Err(NeighborTCStateError::ChainInconsistency)
+                }
+            },
+            MoveTokenDirection::Outgoing => {
+                if move_token_message.old_token == self.chain_state.new_token {
+                    let token_channel = self.token_channel.take().expect("TokenChannel not present!");
+                    match atomic_process_operations_list(token_channel, 
+                                                    move_token_message.operations) {
+                        (token_channel, Ok(output)) => {
+                            // If processing the transactions was successful, we 
+                            // set old_token, new_token and direction:
+                            self.token_channel = Some(token_channel);
+                            self.chain_state = ChainState {
+                                old_token: self.chain_state.new_token.clone(),
                                 new_token,
                                 direction: MoveTokenDirection::Incoming,
-                            },
-                            token_channel,
-                        };
-                        (neighbor_tc_state, Ok(ReceiveTokenOutput::ProcessTransListOutput(output)))
-                    },
-                    (token_channel, Err(e)) => {
-                        let neighbor_tc_state = NeighborTCState {
-                            chain_state: neighbor_tc_state.chain_state,
-                            token_channel,
-                        };
-                        (neighbor_tc_state, Err(NeighborTCStateError::InvalidTransaction(e)))
-                    },
+                            };
+                            Ok(ReceiveTokenOutput::ProcessTransListOutput(output))
+                        },
+                        (token_channel, Err(e)) => {
+                            self.token_channel = Some(token_channel);
+                            Err(NeighborTCStateError::InvalidTransaction(e))
+                        },
+                    }
+                } else if self.chain_state.old_token == new_token {
+                    // We should retransmit send our message to the remote side.
+                    Ok(ReceiveTokenOutput::RetransmitOutgoing)
+                } else {
+                    Err(NeighborTCStateError::ChainInconsistency)
                 }
-            } else if neighbor_tc_state.chain_state.old_token == new_token {
-                // We should retransmit send our message to the remote side.
-                (neighbor_tc_state, Ok(ReceiveTokenOutput::RetransmitOutgoing))
-            } else {
-                (neighbor_tc_state, Err(NeighborTCStateError::ChainInconsistency))
-            }
-        },
+            },
+        }
+    }
+
+    #[allow(unused)]
+    pub fn send_move_token(&mut self, 
+                           move_token_message: &NeighborMoveTokenInner, 
+                           new_token: ChannelToken) -> Result<(), ()> {
+        
+        // TODO:
+        Ok(())
     }
 }
 
-#[allow(unused)]
-pub fn send_move_token(neighbor_tc_state: NeighborTCState, 
-                       move_token_message: &NeighborMoveTokenInner, 
-                       new_token: ChannelToken) -> (NeighborTCState, Result<(), ()>) {
-    
-    // TODO:
 
-    (neighbor_tc_state, Ok(()))
-}
