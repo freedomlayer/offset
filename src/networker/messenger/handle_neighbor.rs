dd83abdee9a097bbab5151deda18882cb99f0ee5
real <real@freedomlayer.org>
2018-06-18 11:05:41 +0300

    Added skeleton for MessengerState Neighbor message handlers.

--- src/networker/messenger/handle_neighbor.rs
+++ src/networker/messenger/handle_neighbor.rs
@@ -39,12 +39,33 @@ pub enum HandleNeighborMessageError {
 
 #[allow(unused)]
 impl MessengerState {
+    fn handle_move_token(&mut self, neighbor_move_token: NeighborMoveToken) 
+         -> Result<(Option<DatabaseMessage>, Vec<MessengerTask>), HandleNeighborMessageError> {
+        unreachable!();
+    }
+
+    fn handle_inconsistency_error(&mut self, neighbor_inconsistency_error: NeighborInconsistencyError)
+         -> Result<(Option<DatabaseMessage>, Vec<MessengerTask>), HandleNeighborMessageError> {
+        unreachable!();
+    }
+
+    fn handle_set_max_token_channels(&mut self, neighbor_set_max_token_channels: NeighborSetMaxTokenChannels)
+         -> Result<(Option<DatabaseMessage>, Vec<MessengerTask>), HandleNeighborMessageError> {
+        unreachable!();
+    }
+
     pub fn handle_neighbor_message(&mut self, 
                                    remote_public_key: &PublicKey, 
                                    neighbor_message: NeighborMessage)
         -> Result<(Option<DatabaseMessage>, Vec<MessengerTask>), HandleNeighborMessageError> {
 
-        // TODO
-        unreachable!();
+        match neighbor_message {
+            NeighborMessage::MoveToken(neighbor_move_token) =>
+                self.handle_move_token(neighbor_move_token),
+            NeighborMessage::InconsistencyError(neighbor_inconsistency_error) =>
+                self.handle_inconsistency_error(neighbor_inconsistency_error),
+            NeighborMessage::SetMaxTokenChannels(neighbor_set_max_token_channels) =>
+                self.handle_set_max_token_channels(neighbor_set_max_token_channels),
+        }
     }
 }
