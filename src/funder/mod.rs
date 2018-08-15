092aa3d91bf4480c539f6e02189abb9e26fda220
real <real@freedomlayer.org>
2018-08-15 19:17:02 +0300

    Updated state.rs for Funder.

--- src/funder/mod.rs
+++ src/funder/mod.rs
@@ -7,7 +7,7 @@ mod credit_calc;
 mod freeze_guard;
 mod signature_buff;
 mod friend;
-// mod state;
+mod state;
 mod types;
 mod token_channel;
 // mod handler;
--- src/funder/state.rs
+++ src/funder/state.rs
@@ -1,36 +1,30 @@
-use std::net::SocketAddr;
 use im::hashmap::HashMap as ImHashMap;
 
 use num_bigint::BigUint;
 use num_traits::identities::Zero;
 
 use crypto::identity::PublicKey;
-// use crypto::rand_values::RandValue;
-
 use super::friend::{FriendState, FriendMutation};
 
 
 #[allow(unused)]
 #[derive(Clone)]
-pub struct MessengerState {
+pub struct MessengerState<A:Clone> {
     pub local_public_key: PublicKey,
-    pub incoming_path_fee: u64,
-    pub friends: ImHashMap<PublicKey, FriendState>,
+    pub friends: ImHashMap<PublicKey, FriendState<A>>,
 }
 
 #[allow(unused)]
-pub enum MessengerMutation {
+pub enum MessengerMutation<A> {
     FriendMutation((PublicKey, FriendMutation)),
-    AddFriend((PublicKey, Option<SocketAddr>, u16)),
-    // (friend_public_key, friend_addr, max_channels)
+    AddFriend((PublicKey, Option<A>)), // (friend_public_key, opt_address)
     RemoveFriend(PublicKey),
-    SetIncomingPathFee(u64),
 }
 
 
 #[allow(unused)]
-impl MessengerState {
-    pub fn new() -> MessengerState {
+impl<A:Clone> MessengerState<A> {
+    pub fn new() -> MessengerState<A> {
         // TODO: Initialize from database somehow.
         unreachable!();
     }
@@ -39,12 +33,13 @@ impl MessengerState {
     pub fn get_total_trust(&self) -> BigUint {
         let mut sum: BigUint = BigUint::zero();
         for friend in self.friends.values() {
-            sum += friend.get_trust();
+            let remote_max_debt: BigUint = friend.directional.token_channel.state().balance.remote_max_debt.into();
+            sum += remote_max_debt;
         }
         sum
     }
 
-    pub fn get_friends(&self) -> &ImHashMap<PublicKey, FriendState> {
+    pub fn get_friends(&self) -> &ImHashMap<PublicKey, FriendState<A>> {
         &self.friends
     }
 
@@ -52,17 +47,16 @@ impl MessengerState {
         &self.local_public_key
     }
 
-    pub fn mutate(&mut self, messenger_mutation: &MessengerMutation) {
+    pub fn mutate(&mut self, messenger_mutation: &MessengerMutation<A>) {
         match messenger_mutation {
             MessengerMutation::FriendMutation((public_key, friend_mutation)) => {
                 let friend = self.friends.get_mut(&public_key).unwrap();
                 friend.mutate(friend_mutation);
             },
-            MessengerMutation::AddFriend((friend_public_key, opt_socket_addr, max_channels)) => {
+            MessengerMutation::AddFriend((friend_public_key, opt_address)) => {
                 let friend = FriendState::new(&self.local_public_key,
                                                   friend_public_key,
-                                                  *opt_socket_addr, 
-                                                  *max_channels);
+                                                  opt_address.clone());
                 // Insert friend, but also make sure that we did not remove any existing friend
                 // with the same public key:
                 let _ = self.friends.insert(friend_public_key.clone(), friend).unwrap();
@@ -71,10 +65,6 @@ impl MessengerState {
             MessengerMutation::RemoveFriend(public_key) => {
                 let _ = self.friends.remove(&public_key);
             },
-            MessengerMutation::SetIncomingPathFee(incoming_path_fee) => {
-                self.incoming_path_fee = *incoming_path_fee;
-            },
         }
-
     }
 }
