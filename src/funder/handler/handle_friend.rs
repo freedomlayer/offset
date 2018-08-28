4606c31e453172984d83b5b84708f1a155573aea
real <real@freedomlayer.org>
2018-08-28 11:24:35 +0300

    Moved liveness_friend.cancel_inconsistency() to the beginning of the function.

--- src/funder/handler/handle_friend.rs
+++ src/funder/handler/handle_friend.rs
@@ -362,6 +362,8 @@ impl<A: Clone + 'static, R: SecureRandom + 'static> MutableFunderHandler<A,R> {
         -> Result<Self, !> {
 
         self.clear_inconsistency_status(&remote_public_key);
+        let liveness_friend = self.ephemeral.liveness.friends.get_mut(&remote_public_key).unwrap();
+        liveness_friend.cancel_inconsistency();
 
         match receive_move_token_output {
             ReceiveMoveTokenOutput::Duplicate => {
@@ -383,7 +385,6 @@ impl<A: Clone + 'static, R: SecureRandom + 'static> MutableFunderHandler<A,R> {
             },
             ReceiveMoveTokenOutput::Received(move_token_received) => {
                 let liveness_friend = self.ephemeral.liveness.friends.get_mut(&remote_public_key).unwrap();
-                liveness_friend.cancel_inconsistency();
                 liveness_friend.cancel_request_token();
 
                 let MoveTokenReceived {incoming_messages, mutations} = 
