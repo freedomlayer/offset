44fe7cef93020f13037124e05dc1bcf049978522
real <real@freedomlayer.org>
2018-08-31 15:24:20 +0300

    Updated handle_init.rs to only send information about currently enabled friends.

--- src/funder/handler/handle_init.rs
+++ src/funder/handler/handle_init.rs
@@ -2,10 +2,11 @@ use futures::prelude::{async, await};
 use ring::rand::SecureRandom;
 
 use super::MutableFunderHandler;
-use super::super::friend::{InconsistencyStatus, ResetTerms};
+use super::super::friend::ResetTerms;
 use super::super::token_channel::directional::MoveTokenDirection;
 use super::super::types::{FunderTask, FriendMessage,
-                            FriendInconsistencyError};
+                            FriendInconsistencyError, ChannelerConfig,
+                            FriendStatus};
 
 pub enum HandleInitError {
 }
@@ -14,10 +15,20 @@ pub enum HandleInitError {
 impl<A: Clone, R: SecureRandom> MutableFunderHandler<A,R> {
 
     pub fn handle_init(&mut self) {
-        let mut retransmit_pks = Vec::new();
-        let mut inconsistents = Vec::new();
+        // let mut retransmit_pks = Vec::new();
+        // let mut inconsistents = Vec::new();
 
+        let mut enabled_friends = Vec::new();
         for (friend_public_key, friend) in self.state.get_friends() {
+            match friend.status {
+                FriendStatus::Enable => {
+                    enabled_friends.push((friend.remote_public_key.clone(),
+                        friend.opt_remote_address.clone()));
+                },
+                FriendStatus::Disable => continue,
+            };
+
+            /*
             // match friend.inconsistency_status.incoming {
             match &friend.inconsistency_status {
                 InconsistencyStatus::Empty => {
@@ -37,8 +48,17 @@ impl<A: Clone, R: SecureRandom> MutableFunderHandler<A,R> {
                         out_reset_terms.clone()))
                 },
             };
+            */
         }
 
+        for enabled_friend in enabled_friends {
+            // Notify Channeler:
+            let channeler_config = ChannelerConfig::AddFriend(enabled_friend);
+            self.funder_tasks.push(FunderTask::ChannelerConfig(channeler_config));
+        }
+
+
+        /*
         for friend_public_key in retransmit_pks {
             self.transmit_outgoing(&friend_public_key);
         }
@@ -53,5 +73,6 @@ impl<A: Clone, R: SecureRandom> MutableFunderHandler<A,R> {
                 FunderTask::FriendMessage((friend_public_key,
                     FriendMessage::InconsistencyError(inconsistency_error))));
         }
+        */
     }
 }
