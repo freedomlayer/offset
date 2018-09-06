1607c6e8eae10f66aab651e1f8345444bb4d419f
real <real@freedomlayer.org>
2018-09-06 18:20:59 +0300

    Removed old comments.

--- src/funder/handler/handle_init.rs
+++ src/funder/handler/handle_init.rs
@@ -15,9 +15,6 @@ pub enum HandleInitError {
 impl<A: Clone, R: SecureRandom> MutableFunderHandler<A,R> {
 
     pub fn handle_init(&mut self) {
-        // let mut retransmit_pks = Vec::new();
-        // let mut inconsistents = Vec::new();
-
         let mut enabled_friends = Vec::new();
         for (friend_public_key, friend) in self.state.get_friends() {
             match friend.status {
@@ -27,28 +24,6 @@ impl<A: Clone, R: SecureRandom> MutableFunderHandler<A,R> {
                 },
                 FriendStatus::Disable => continue,
             };
-
-            /*
-            // match friend.inconsistency_status.incoming {
-            match &friend.inconsistency_status {
-                InconsistencyStatus::Empty => {
-                    match &friend.directional.direction {
-                        MoveTokenDirection::Outgoing(_) => {
-                            retransmit_pks.push(friend_public_key.clone());
-                        },
-                        MoveTokenDirection::Incoming(_) => {}
-                    }
-                },
-                InconsistencyStatus::Outgoing(out_reset_terms) => {
-                    inconsistents.push((friend_public_key.clone(),
-                        out_reset_terms.clone()))
-                },
-                InconsistencyStatus::IncomingOutgoing((_in_reset_terms, out_reset_terms)) => {
-                    inconsistents.push((friend_public_key.clone(),
-                        out_reset_terms.clone()))
-                },
-            };
-            */
         }
 
         for enabled_friend in enabled_friends {
@@ -56,23 +31,5 @@ impl<A: Clone, R: SecureRandom> MutableFunderHandler<A,R> {
             let channeler_config = ChannelerConfig::AddFriend(enabled_friend);
             self.add_outgoing_comm(FunderOutgoingComm::ChannelerConfig(channeler_config));
         }
-
-
-        /*
-        for friend_public_key in retransmit_pks {
-            self.transmit_outgoing(&friend_public_key);
-        }
-
-        for (friend_public_key, out_reset_terms) in inconsistents {
-            let ResetTerms {reset_token, balance_for_reset} = out_reset_terms;
-            let inconsistency_error = FriendInconsistencyError {
-                reset_token,
-                balance_for_reset,
-            };
-            self.funder_tasks.push(
-                FunderTask::FriendMessage((friend_public_key,
-                    FriendMessage::InconsistencyError(inconsistency_error))));
-        }
-        */
     }
 }
