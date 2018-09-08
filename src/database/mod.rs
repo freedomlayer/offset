72f98aa0441b03c8e895039b8c9809a604d743cc
real <real@freedomlayer.org>
2018-09-08 11:54:02 +0300

    Initial 'shortcut' implementation for Database using bincode and atomicwrites.

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -301,24 +301,68 @@
 //}
 
 
+use std::io;
+use std::io::prelude::*;
 use std::string::String;
+use std::fs::File;
+
+use serde::Serialize;
+use serde::de::DeserializeOwned;
+
+use bincode;
+use atomicwrites;
+
 use funder::state::{FunderMutation, FunderState};
 
+pub enum DatabaseError {
+    ReadError(io::Error),
+    WriteError(atomicwrites::Error<io::Error>),
+    DeserializeError(bincode::Error),
+    SerializeError(bincode::Error),
+}
 
 #[allow(unused)]
-struct Database<A: Clone> {
+pub struct DbConn {
     /// Database file path
     db_path: String,
+}
+
+#[allow(unused)]
+impl DbConn {
+    fn new(db_path: &str) -> DbConn {
+        DbConn {
+            db_path: String::from(db_path),
+        }
+    }
+}
+
+#[allow(unused)]
+pub struct Database<A: Clone> {
+    /// Connection to the database
+    db_conn: DbConn,
     /// Current FunderState represented by the database
     funder_state: FunderState<A>,
 }
 
+
 #[allow(unused)]
-impl<A: Clone> Database<A> {
-    fn new(db_path: &str) -> Database<A> {
-        // TODO:
-        // - Read database from file into funder_state
-        unimplemented!();
+impl<A: Clone + Serialize + DeserializeOwned> Database<A> {
+    fn new(db_conn: DbConn) -> Result<Database<A>, DatabaseError> {
+
+        let mut fr = File::open(&db_conn.db_path)
+            .map_err(DatabaseError::ReadError)?;
+
+        let mut data = Vec::new();
+        fr.read_to_end(&mut data)
+            .map_err(DatabaseError::ReadError)?;
+
+        let funder_state: FunderState<A> = bincode::deserialize(&data)
+            .map_err(DatabaseError::DeserializeError)?;
+
+        Ok(Database {
+            db_conn,
+            funder_state,
+        })
     }
 
     /// Get current FunderState represented by the database
@@ -327,11 +371,21 @@ impl<A: Clone> Database<A> {
     }
 
     /// Apply a mutation over the database, and save it.
-    fn mutate(&mut self, funder_mutation: FunderMutation<A>) {
-        // - Apply mutation to funder_state
+    fn mutate(&mut self, funder_mutation: FunderMutation<A>) -> Result<(), DatabaseError> {
+        // Apply mutation to funder_state:
         self.funder_state.mutate(&funder_mutation);
-        // TODO:
-        // - Save the new funder_state to file, atomically.
-        unimplemented!();
+
+        // Serialize the funder_state:
+        let data = bincode::serialize(&self.funder_state)
+            .map_err(DatabaseError::SerializeError)?;
+
+        // Save the new funder_state to file, atomically:
+        let af = atomicwrites::AtomicFile::new(&self.db_conn.db_path, atomicwrites::AllowOverwrite);
+        af.write(|fw| {
+            fw.write_all(&data)
+        }).map_err(DatabaseError::WriteError)?;
+
+        
+        Ok(())
     }
 }
