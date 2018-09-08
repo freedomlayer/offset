c9229ba6cd7dbc2f2f6c41c5fdacc0e5c9d939bb
real <real@freedomlayer.org>
2018-09-08 13:23:11 +0300

    Added initial implementation of db_service().

--- src/database/service.rs
+++ src/database/service.rs
@@ -1,7 +1,10 @@
 #![allow(unused)]
 
-use futures::sync::{oneshot, mpsc};
+use std::marker::Send;
 use futures::prelude::{async, await};
+use futures::sync::{oneshot, mpsc};
+use futures::Stream;
+use futures_cpupool::CpuPool;
 
 use serde::Serialize;
 use serde::de::DeserializeOwned;
@@ -10,18 +13,54 @@ use funder::state::{FunderMutation};
 use super::core::{DbCore, DbCoreError};
 
 
-pub struct ApplyFunderMutation<A> {
-    funder_mutation: FunderMutation<A>,
+pub struct IncomingMutationsBatch<A> {
+    funder_mutations: Vec<FunderMutation<A>>,
     /// A oneshot to respond that the mutation was applied and the new state was saved.
-    resposne: oneshot::Sender<()>,
+    ack_sender: oneshot::Sender<()>,
 }
 
 pub enum DbServiceError {
+    IncomingClosed,
+    IncomingError,
     DbCoreError(DbCoreError),
+    AckFailure,
+}
+
+fn apply_funder_mutation<A: Clone + Serialize + DeserializeOwned>(
+    mut db_core: DbCore<A>, funder_mutations: Vec<FunderMutation<A>>) -> Result<DbCore<A>,DbCoreError> {
+
+    db_core.mutate(funder_mutations)?;
+    Ok(db_core)
+
 }
 
 #[async]
-pub fn db_service<A: Clone + Serialize + DeserializeOwned>(db_core: DbCore<A>, 
-                incoming: mpsc::Receiver<ApplyFunderMutation<A>>) -> Result<!, DbServiceError> {
-    unimplemented!();
+pub fn db_service<A: Clone + Serialize + DeserializeOwned + Send + Sync + 'static>(mut db_core: DbCore<A>, 
+                mut incoming_batches: mpsc::Receiver<IncomingMutationsBatch<A>>) -> Result<!, DbServiceError> {
+
+    // Start a pool to run slow database operations:
+    let pool = CpuPool::new(1);
+
+    loop {
+        // Read one incoming 
+        let incoming_mutations_batch = match await!(incoming_batches.into_future()) {
+            Ok((opt_incoming_mutations_batch, ret_incoming_batches)) => {
+                incoming_batches = ret_incoming_batches;
+                match opt_incoming_mutations_batch {
+                    Some(incoming_mutations_batch) => incoming_mutations_batch,
+                    None => return Err(DbServiceError::IncomingClosed),
+                }
+            },
+            Err(_) => return Err(DbServiceError::IncomingError),
+        };
+
+        let IncomingMutationsBatch {funder_mutations, ack_sender} = incoming_mutations_batch;
+
+        db_core = await!(pool.spawn_fn(move || apply_funder_mutation(db_core, funder_mutations)))
+            .map_err(DbServiceError::DbCoreError)?;
+
+        // Send an ack to signal that the operation has completed:
+        ack_sender.send(())
+            .map_err(|()| DbServiceError::AckFailure)?;
+    }
 }
