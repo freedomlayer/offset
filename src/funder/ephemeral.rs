51895f00e4ff1125541c93e318b6d382ce7a8980
real <real@freedomlayer.org>
2018-09-09 10:14:47 +0300

    Added constructor from FunderState.

--- src/funder/ephemeral.rs
+++ src/funder/ephemeral.rs
@@ -1,5 +1,6 @@
 use super::freeze_guard::FreezeGuard;
 use super::liveness::Liveness;
+use super::state::FunderState;
 
 #[derive(Clone)]
 pub struct FunderEphemeral {
@@ -8,4 +9,10 @@ pub struct FunderEphemeral {
 }
 
 impl FunderEphemeral {
+    pub fn new<A: Clone>(funder_state: &FunderState<A>) -> FunderEphemeral {
+        FunderEphemeral {
+            freeze_guard: FreezeGuard::new(funder_state),
+            liveness: Liveness::new(),
+        }
+    }
 }
