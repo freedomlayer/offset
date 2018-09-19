b6ad0e50e49db7c43b7ab9a0466f49885f090cc1
real <real@freedomlayer.org>
2018-09-19 17:58:03 +0300

    Added basic execution of Diffie-Hellman.

--- src/dh/mod.rs
+++ src/dh/mod.rs
@@ -10,6 +10,10 @@ use crypto::identity::PublicKey;
 use identity::client::IdentityClient;
 use timer::TimerClient;
 
+use self::state::{DhStateInitial, DhStateHalf, DhState, DhError};
+use self::serialize::{serialize_exchange_rand_nonce, deserialize_exchange_rand_nonce,
+                        serialize_exchange_dh, deserialize_exchange_dh};
+
 mod messages;
 mod serialize;
 mod state;
@@ -21,10 +25,35 @@ struct SecureChannel;
 #[derive(Debug)]
 enum SecureChannelError {
     IdentityFailure,
+    WriterError,
+    ReaderClosed,
+    ReaderError,
+    DeserializeRandNonceError,
+    HandleExchangeRandNonceError(DhError),
+    DeserializeExchangeDhError,
+    HandleExchangeDhError(DhError),
 }
 
 #[async]
-fn create_secure_channel<M,K,R>(reader: M, writer: K, 
+/// Read one message from reader
+fn read_from_reader<M: 'static>(reader: M) -> Result<(Vec<u8>, M), SecureChannelError>
+    where M: Stream<Item=Vec<u8>, Error=()>,
+{
+    match await!(reader.into_future()) {
+        Ok((opt_reader_message, ret_reader)) => {
+            match opt_reader_message {
+                Some(reader_message) => Ok((reader_message, ret_reader)),
+                None => return Err(SecureChannelError::ReaderClosed),
+            }
+        },
+        Err(_) => return Err(SecureChannelError::ReaderError),
+    }
+}
+
+
+
+#[async]
+fn create_secure_channel<M: 'static,K: 'static,R: SecureRandom + 'static>(reader: M, writer: K, 
                               identity_client: IdentityClient,
                               expected_remote: Option<PublicKey>,
                               rng: Rc<R>,
@@ -38,13 +67,34 @@ where
     let local_public_key = await!(identity_client.request_public_key())
         .map_err(|_| SecureChannelError::IdentityFailure)?;
 
-    // let (dh_state_initial, exchange_rand_nonce) = DhStateInitial::new(&local_public_key1, &*rng1);
+    let (dh_state_initial, exchange_rand_nonce) = DhStateInitial::new(&local_public_key, &*rng);
+    let ser_exchange_rand_nonce = serialize_exchange_rand_nonce(&exchange_rand_nonce);
+    let writer = await!(writer.send(ser_exchange_rand_nonce))
+        .map_err(|_| SecureChannelError::WriterError)?;
+
+    let (reader_message, reader) = await!(read_from_reader(reader))?;
+
+    let exchange_rand_nonce = deserialize_exchange_rand_nonce(&reader_message)
+        .map_err(|_| SecureChannelError::DeserializeRandNonceError)?;
+    let (dh_state_half, exchange_dh) = await!(dh_state_initial.handle_exchange_rand_nonce(
+                                                exchange_rand_nonce,
+                                                identity_client.clone(),
+                                                Rc::clone(&rng)))
+        .map_err(SecureChannelError::HandleExchangeRandNonceError)?;
+
+    let ser_exchange_dh = serialize_exchange_dh(&exchange_dh);
+    let writer = await!(writer.send(ser_exchange_dh))
+        .map_err(|_| SecureChannelError::WriterError)?;
+
+    let (reader_message, reader) = await!(read_from_reader(reader))?;
+    let exchange_dh = deserialize_exchange_dh(&reader_message)
+        .map_err(|_| SecureChannelError::DeserializeExchangeDhError)?;
+    let dh_state = dh_state_half.handle_exchange_dh(exchange_dh)
+        .map_err(SecureChannelError::HandleExchangeDhError)?;
+
+
     
     // TODO:
-    // - Send rand nonce exchange
-    // - Wait (timeout)
-    // - Send exchange dh
-    // - Wait (timeout)
     // - Create two pairs of (sender, receiver)
     // - spawn an event loop (Separate async function) over the following:
     //      - Timer event
