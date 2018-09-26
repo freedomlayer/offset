d30c9ff2695cb7871e98f2fe7a8605027ef9077f
real <real@freedomlayer.org>
2018-09-26 10:14:51 +0300

    Continued writing test. Not done yet.

--- src/utils/async_adapter.rs
+++ src/utils/async_adapter.rs
@@ -174,10 +174,12 @@ impl<K,E> AsyncWrite for AsyncWriter<K,E> where K: Sink<SinkItem=Vec<u8>, SinkEr
 #[allow(unused)]
 mod tests {
     use super::*;
-    use futures::sync::mpsc;
+    use futures::sync::{mpsc, oneshot};
     use futures::prelude::{async, await};
     use futures::Future;
     use tokio_core::reactor::Core;
+    use tokio_codec::{FramedRead, FramedWrite};
+    use utils::frame_codec::FrameCodec;
 
     enum ReceiverRes {
         Ready((usize, Vec<u8>)),
@@ -208,15 +210,54 @@ mod tests {
 
     #[async]
     fn basic_stream_receiver() -> Result<(), ()> {
-        let (sender, receiver) = mpsc::channel::<Vec<u8>>(0);
-        let async_reader: AsyncReader<_, ()> = AsyncReader::new(receiver);
+        Ok(())
+    }
+
+    #[async]
+    fn frames_sender(sender: impl Sink<SinkItem=Vec<u8>, SinkError=()> + 'static, 
+                     reader_done_send: oneshot::Sender<bool>) 
+            -> Result<(), ()> {
+        let sender = await!(sender.send(vec![0; 0x200])).unwrap();
+        let sender = await!(sender.send(vec![1; 0x100])).unwrap();
+        let sender = await!(sender.send(vec![2; 0x80])).unwrap();
+
+        reader_done_send.send(true);
+        Ok(())
+    }
+
+    #[async]
+    fn frames_receiver(receiver: impl Stream<Item=Vec<u8>, Error=()> + 'static, 
+                       writer_done_send: oneshot::Sender<bool>) 
+            -> Result<(), ()> {
+        let (opt_data, receiver) = await!(receiver.into_future()).map_err(|_| ())?;
+        writer_done_send.send(true);
         Ok(())
     }
 
     #[test]
     fn test_basic_stream_receiver() {
+        let (sender, receiver) = mpsc::channel::<Vec<u8>>(0);
+        let async_reader: AsyncReader<_, _> = AsyncReader::new(receiver);
+        let async_writer: AsyncWriter<_, _> = AsyncWriter::new(sender, 0x20);
 
+        let reader = FramedRead::new(async_reader, FrameCodec::new())
+            .map_err(|_| ());
+        let writer = FramedWrite::new(async_writer, FrameCodec::new())
+            .sink_map_err(|_| ());
+
+        let (reader_done_send, reader_done_recv) = oneshot::channel::<bool>();
+        let (writer_done_send, writer_done_recv) = oneshot::channel::<bool>();
+
+        // TODO: Continue here.
+        // Required: Port async_adapter to use Bytes instead of Vec<u8>
+
+        /*
         let mut core = Core::new().unwrap();
-        core.run(basic_stream_receiver());
+        let handle = core.handle();
+        handle.spawn(frames_receiver(reader, reader_done_send));
+        handle.spawn(frames_sender(writer, writer_done_send));
+        assert_eq!(true, core.run(reader_done_recv).unwrap());
+        assert_eq!(true, core.run(writer_done_recv).unwrap());
+        */
     }
 }
