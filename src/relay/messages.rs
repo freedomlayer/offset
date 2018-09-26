3ff412ff72915d5ba3fea572362a00981c49feb4
real <real@freedomlayer.org>
2018-09-26 17:19:58 +0300

    Added relay messages (Rust).

---
+++ src/relay/messages.rs
@@ -0,0 +1,24 @@
+use crypto::identity::PublicKey;
+
+pub enum InitConnection {
+    Listen,
+    // remote side wants to accept a connection from public_key
+    Accept(PublicKey),
+    // remote side wants to connect to public_key
+    Connect(PublicKey),
+}
+
+pub enum RelayListenIn {
+    KeepAlive,
+    RejectConnection(PublicKey),
+}
+
+pub enum RelayListenOut {
+    KeepAlive,
+    IncomingConnection(PublicKey),
+}
+
+pub enum TunnelMessage {
+    KeepAlive,
+    Message(Vec<u8>),
+}
