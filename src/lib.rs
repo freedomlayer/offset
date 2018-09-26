95531196e2f46321f6a835dd57e9c392088be801
real <real@freedomlayer.org>
2018-09-26 19:15:49 +0300

    Including relay_capnp schema in lib.rs.

--- src/lib.rs
+++ src/lib.rs
@@ -59,3 +59,4 @@ mod proto;
 include_schema!(channeler_capnp, "channeler_capnp");
 include_schema!(common_capnp, "common_capnp");
 include_schema!(dh_capnp, "dh_capnp");
+include_schema!(relay_capnp, "relay_capnp");
