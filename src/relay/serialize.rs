a913df367e7926c3ed141b7a77051cd805192715
real <real@freedomlayer.org>
2018-09-27 10:15:38 +0300

    Added implementation for ser/de TunnelMessage + test.

--- src/relay/serialize.rs
+++ src/relay/serialize.rs
@@ -32,13 +32,41 @@ pub fn serialize_tunnel_message(tunnel_message: &TunnelMessage) -> Vec<u8> {
     let mut builder = capnp::message::Builder::new_default();
     let mut msg = builder.init_root::<relay_capnp::tunnel_message::Builder>();
 
-    /*
-    write_custom_u_int128(&exchange_rand_nonce.rand_nonce, &mut msg.reborrow().get_rand_nonce().unwrap());
-    write_custom_u_int256(&exchange_rand_nonce.public_key, &mut msg.reborrow().get_public_key().unwrap());
+    match tunnel_message {
+        TunnelMessage::KeepAlive => msg.set_keep_alive(()),
+        TunnelMessage::Message(message) => msg.set_message(message),
+    }
 
     let mut serialized_msg = Vec::new();
     serialize_packed::write_message(&mut serialized_msg, &builder).unwrap();
     serialized_msg
-    */
-    unimplemented!();
+}
+
+pub fn deserialize_tunnel_message(data: &[u8]) -> Result<TunnelMessage, RelaySerializeError> {
+    let mut cursor = io::Cursor::new(data);
+    let reader = serialize_packed::read_message(&mut cursor, ::capnp::message::ReaderOptions::new())?;
+    let msg = reader.get_root::<relay_capnp::tunnel_message::Reader>()?;
+
+    match msg.which() {
+        Ok(relay_capnp::tunnel_message::KeepAlive(())) => 
+           Ok(TunnelMessage::KeepAlive),
+        Ok(relay_capnp::tunnel_message::Message(message)) => 
+           Ok(TunnelMessage::Message(message?.to_vec())),
+        Err(e) => Err(RelaySerializeError::NotInSchema(e)),
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_serialize_tunnel_message() {
+        let msg = TunnelMessage::Message(b"Hello world".to_vec());
+        let serialized = serialize_tunnel_message(&msg);
+        let msg2 = deserialize_tunnel_message(&serialized[..]).unwrap();
+        assert_eq!(msg, msg2);
+    }
 }
