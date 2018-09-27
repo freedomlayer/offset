679075cacf03c98b11781e86292f9015a68c0537
real <real@freedomlayer.org>
2018-09-27 11:44:20 +0300

    Design experimentation, not done yet.

--- src/relay/server.rs
+++ src/relay/server.rs
@@ -3,7 +3,9 @@ use std::collections::HashMap;
 use std::marker::PhantomData;
 use bytes::Bytes;
 use futures::{stream, Stream, Sink};
-use futures::prelude::{async, await}; use crypto::identity::PublicKey;
+use futures::sync::mpsc;
+use futures::prelude::{async, await, async_stream}; 
+use crypto::identity::PublicKey;
 use timer::TimerClient;
 
 use super::messages::{TunnelMessage, RelayListenIn, RelayListenOut};
@@ -31,11 +33,27 @@ pub struct RelayServer<M,K,MT,KT> {
     keepalive_ticks: usize,
 }
 
+pub struct IncomingListen {
+    receiver: Box<Stream<Item=RelayListenIn, Error=()>>,
+    sender: Box<Sink<SinkItem=RelayListenOut, SinkError=()>>,
+}
+
+pub struct IncomingAccept {
+    receiver: Box<Stream<Item=RelayListenIn, Error=()>>,
+    sender: Box<Sink<SinkItem=RelayListenOut, SinkError=()>>,
+    accepted_public_key: PublicKey,
+}
+
+pub struct IncomingConnect {
+    receiver: Box<Stream<Item=RelayListenIn, Error=()>>,
+    sender: Box<Sink<SinkItem=RelayListenOut, SinkError=()>>,
+    dest_public_key: PublicKey,
+}
 
 enum IncomingConnInner {
-    Listen(ConnPair<RelayListenIn, RelayListenOut>),
-    Accept((ConnPair<TunnelMessage, TunnelMessage>, PublicKey)),
-    Connect((ConnPair<TunnelMessage, TunnelMessage>, PublicKey)),
+    Listen(IncomingListen),
+    Accept(IncomingAccept),
+    Connect(IncomingConnect),
 }
 
 struct IncomingConn {
@@ -43,63 +61,53 @@ struct IncomingConn {
     inner: IncomingConnInner,
 }
 
+struct ConnClosed {
+    initiator: PublicKey,
+    listener: PublicKey,
+}
+
+
 enum RelayServerEvent {
     IncomingConn(IncomingConn),
-    ConnClosed((PublicKey, PublicKey)), // Connection to pk1 (listener) from pk2 (initiator) was closed
+    ConnClosed(ConnClosed),
 }
 
 enum RelayServerError {
 }
 
-/*
-impl<M,K,MT,KT> RelayServer<M,K,MT,KT> {
-    fn new(timer_client: TimerClient, 
-           keepalive_ticks: usize) -> RelayServer {
-        RelayServer {
-            listeners: HashMap::new(),
-            timer_client,
-            keepalive_ticks,
-        }
-    }
+#[async_stream(item=IncomingConn)]
+fn conn_processor<T,M,K>(timer_client: TimerClient,
+                    incoming_conns: T,
+                    keepalive_ticks: usize) -> Result<(), RelayServerError>
+where
+    T: Stream<Item=(M, K, PublicKey), Error=()>,
+    M: Stream<Item=Bytes, Error=()>,
+    K: Sink<SinkItem=Bytes, SinkError=()>,
+{
 
-    #[async]
-    fn tick(self) -> Result<Self, RelayServerError> {
-        // TODO:
-        // - Decrease various tick counters.
-        // - Send keepalive messages if required for:
-        //      - Listeners
-        // - Remove if required:
-        //      - Listeners
-        //      - HalfConns
-        unimplemented!();
+    /*
+    match await!(incoming_conns.into_future()) {
+        Ok((opt_reader_message, ret_reader)) => {
+            match opt_reader_message {
+                Some(reader_message) => Ok((reader_message, ret_reader)),
+                None => return Err(SecureChannelError::ReaderClosed),
+            }
+        },
+        Err(_) => return Err(SecureChannelError::ReaderError),
     }
+    */
 
-
-    #[async]
-    fn run(self) -> Result<!, RelayServerError> {
-        // TODO:
-        // check for any event:
-        // - Incoming connection 
-        //      (sender, receiver) pair an a public key
-        //      - Convert the connection into one of three: Listen, Accept or Connect.
-        //          (Should be done using a .map() adapter on the Stream).
-        //          
-        // - A connection was closed
-        //      - Remove from data structures
-        // - Time tick
-        //      - Possibly timeout: Listening conn
-        //      - Send keepalive if required to a listening conn.
-        unimplemented!();
-    }
+    unimplemented!();
 }
 
-*/
+
  
+#[async]
 fn relay_server<T,M,K>(timer_client: TimerClient, 
                 incoming_conns: T,
                 keepalive_ticks: usize) -> Result<!, RelayServerError> 
 where
-    T: Stream<Item=(M, K), Error=()>,
+    T: Stream<Item=(M, K, PublicKey), Error=()>,
     M: Stream<Item=Bytes, Error=()>,
     K: Sink<SinkItem=Bytes, SinkError=()>,
 {
