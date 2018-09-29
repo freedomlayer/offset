e9fe0aac368adf6ae042ed07b02a8fe9b1088bb0
real <real@freedomlayer.org>
2018-09-29 11:36:38 +0300

    Added implementation for conn_processor

--- src/relay/server.rs
+++ src/relay/server.rs
@@ -1,13 +1,17 @@
 #![allow(unused)]
 use std::collections::HashMap;
 use std::marker::PhantomData;
-use futures::{stream, Stream, Sink};
+use futures::{stream, Stream, Sink, Future};
 use futures::sync::mpsc;
 use futures::prelude::{async, await, async_stream}; 
 use crypto::identity::PublicKey;
 use timer::TimerClient;
 
-use super::messages::{TunnelMessage, RelayListenIn, RelayListenOut};
+use super::messages::{InitConnection, TunnelMessage, 
+    RelayListenIn, RelayListenOut};
+use super::serialize::{deserialize_init_connection, deserialize_relay_listen_in,
+                        serialize_relay_listen_out, serialize_tunnel_message,
+                        deserialize_tunnel_message};
 
 struct ConnPair<M,K> {
     receiver: M,
@@ -44,7 +48,7 @@ pub struct IncomingListen<M,K> {
 pub struct IncomingAccept<M,K> {
     receiver: M,
     sender: K,
-    accepted_public_key: PublicKey,
+    accept_public_key: PublicKey,
 }
 
 // M: Stream<Item=TunnelMessage, Error=()>,
@@ -52,7 +56,7 @@ pub struct IncomingAccept<M,K> {
 pub struct IncomingConnect<M,K> {
     receiver: M,
     sender: K,
-    dest_public_key: PublicKey,
+    connect_public_key: PublicKey,
 }
 
 enum IncomingConnInner<ML,KL,MA,KA,MC,KC> {
@@ -62,7 +66,7 @@ enum IncomingConnInner<ML,KL,MA,KA,MC,KC> {
 }
 
 struct IncomingConn<ML,KL,MA,KA,MC,KC> {
-    conn_public_key: PublicKey,
+    public_key: PublicKey,
     inner: IncomingConnInner<ML,KL,MA,KA,MC,KC>,
 }
 
@@ -78,32 +82,79 @@ enum RelayServerEvent<ML,KL,MA,KA,MC,KC> {
 }
 
 enum RelayServerError {
+    IncomingConnsClosed,
+    IncomingConnsError,
 }
+
+fn dispatch_conn<M,K>(receiver: M, sender: K, public_key: PublicKey, first_msg: Vec<u8>) 
+    -> Option<IncomingConn<impl Stream<Item=RelayListenIn,Error=()>,
+                              impl Sink<SinkItem=RelayListenOut,SinkError=()>,
+                              impl Stream<Item=TunnelMessage,Error=()>,
+                              impl Sink<SinkItem=TunnelMessage,SinkError=()>,
+                              impl Stream<Item=TunnelMessage,Error=()>,
+                              impl Sink<SinkItem=TunnelMessage,SinkError=()>>>
+where
+    M: Stream<Item=Vec<u8>, Error=()>,
+    K: Sink<SinkItem=Vec<u8>, SinkError=()>,
+{
+    let inner = match deserialize_init_connection(&first_msg).ok()? {
+        InitConnection::Listen => {
+            IncomingConnInner::Listen(IncomingListen {
+                receiver: receiver.and_then(|data| deserialize_relay_listen_in(&data).map_err(|_| ())),
+                sender: sender.with(|msg| Ok(serialize_relay_listen_out(&msg))),
+            })
+        },
+        InitConnection::Accept(accept_public_key) => 
+            IncomingConnInner::Accept(IncomingAccept {
+                receiver: receiver.and_then(|data| deserialize_tunnel_message(&data).map_err(|_| ())),
+                sender: sender.with(|msg| Ok(serialize_tunnel_message(&msg))),
+                accept_public_key,
+            }),
+        InitConnection::Connect(connect_public_key) => 
+            IncomingConnInner::Connect(IncomingConnect {
+                receiver: receiver.and_then(|data| deserialize_tunnel_message(&data).map_err(|_| ())),
+                sender: sender.with(|msg| Ok(serialize_tunnel_message(&msg))),
+                connect_public_key,
+            }),
+    };
+
+    Some(IncomingConn {
+        public_key,
+        inner,
+    })
+}
+
 
 fn conn_processor<T,M,K>(timer_client: TimerClient,
                     incoming_conns: T,
-                    keepalive_ticks: usize) -> impl Stream 
+                    keepalive_ticks: usize) -> impl Stream<
+                        Item=IncomingConn<impl Stream<Item=RelayListenIn,Error=()>,
+                                          impl Sink<SinkItem=RelayListenOut,SinkError=()>,
+                                          impl Stream<Item=TunnelMessage,Error=()>,
+                                          impl Sink<SinkItem=TunnelMessage,SinkError=()>,
+                                          impl Stream<Item=TunnelMessage,Error=()>,
+                                          impl Sink<SinkItem=TunnelMessage,SinkError=()>>,
+                        Error=()>
 where
     T: Stream<Item=(M, K, PublicKey), Error=()>,
     M: Stream<Item=Vec<u8>, Error=()>,
     K: Sink<SinkItem=Vec<u8>, SinkError=()>,
 {
-
-    incoming_conns
-
-    /*
-    match await!(incoming_conns.into_future()) {
-        Ok((opt_reader_message, ret_reader)) => {
-            match opt_reader_message {
-                Some(reader_message) => Ok((reader_message, ret_reader)),
-                None => return Err(SecureChannelError::ReaderClosed),
-            }
-        },
-        Err(_) => return Err(SecureChannelError::ReaderError),
-    }
-    */
-
-    // unimplemented!();
+    incoming_conns.and_then(|(receiver, sender, public_key)| {
+        receiver
+            .into_future()
+            .then(|res| {
+                Ok(match res {
+                    Ok((opt_first_msg, receiver)) => {
+                        match opt_first_msg {
+                            Some(first_msg) => dispatch_conn(receiver, sender, public_key, first_msg),
+                            None => None,
+                        }
+                    },
+                    Err(_) => None,
+                })
+            })
+    }).filter_map(|opt_conn| opt_conn)
 }
 
 
