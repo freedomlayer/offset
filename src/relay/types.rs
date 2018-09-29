f3b3623323b6b637da0f95b3bcba0f5048aa9859
real <real@freedomlayer.org>
2018-09-29 13:17:28 +0300

    Changed some fields to be public.

--- src/relay/types.rs
+++ src/relay/types.rs
@@ -4,26 +4,26 @@ use crypto::identity::PublicKey;
 // K: Sink<SinkItem=RelayListenOut, SinkError=()>,
 #[allow(unused)]
 pub struct IncomingListen<M,K> {
-    receiver: M,
-    sender: K,
+    pub receiver: M,
+    pub sender: K,
 }
 
 // M: Stream<Item=TunnelMessage, Error=()>>,
 // K: Sink<SinkItem=TunnelMessage, SinkError=()>>,
 #[allow(unused)]
 pub struct IncomingAccept<M,K> {
-    receiver: M,
-    sender: K,
-    accept_public_key: PublicKey,
+    pub receiver: M,
+    pub sender: K,
+    pub accept_public_key: PublicKey,
 }
 
 // M: Stream<Item=TunnelMessage, Error=()>,
 // K: SinkItem=TunnelMessage, SinkError=()>,
 #[allow(unused)]
 pub struct IncomingConnect<M,K> {
-    receiver: M,
-    sender: K,
-    connect_public_key: PublicKey,
+    pub receiver: M,
+    pub sender: K,
+    pub connect_public_key: PublicKey,
 }
 
 #[allow(unused)]
@@ -35,6 +35,6 @@ pub enum IncomingConnInner<ML,KL,MA,KA,MC,KC> {
 
 #[allow(unused)]
 pub struct IncomingConn<ML,KL,MA,KA,MC,KC> {
-    public_key: PublicKey,
-    inner: IncomingConnInner<ML,KL,MA,KA,MC,KC>,
+    pub public_key: PublicKey,
+    pub inner: IncomingConnInner<ML,KL,MA,KA,MC,KC>,
 }
