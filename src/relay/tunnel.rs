ceb4606655a6cd84fc8d21848557f8f7ab808749
real <real@freedomlayer.org>
2018-09-30 11:24:05 +0300

    Added a basic test for tunnel_loop().

--- src/relay/tunnel.rs
+++ src/relay/tunnel.rs
@@ -110,3 +110,125 @@ where
     unreachable!();
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use futures::prelude::{async, await};
+    use futures::sync::{mpsc, oneshot};
+    use futures::Future;
+    use tokio_core::reactor::Core;
+    use timer::create_timer_incoming;
+
+    #[derive(Debug, Eq, PartialEq)]
+    enum ReadError {
+        Closed,
+        Error,
+    }
+
+    #[async]
+    fn receive<T, EM, M: 'static>(reader: M) -> Result<(T, M), ReadError>
+        where M: Stream<Item=T, Error=EM>,
+    {
+        match await!(reader.into_future()) {
+            Ok((opt_reader_message, ret_reader)) => {
+                match opt_reader_message {
+                    Some(reader_message) => Ok((reader_message, ret_reader)),
+                    None => return Err(ReadError::Closed),
+                }
+            },
+            Err(_) => return Err(ReadError::Error),
+        }
+    }
+
+    #[async]
+    fn run_a(mut receiver: mpsc::Receiver<TunnelMessage>, 
+             mut sender: mpsc::Sender<TunnelMessage>,
+             mut tick_sender: mpsc::Sender<()>,
+             fin_sender: oneshot::Sender<bool>) -> Result<(), ()> {
+
+        sender = await!(sender.send(TunnelMessage::KeepAlive)).unwrap();
+        sender = await!(sender.send(TunnelMessage::KeepAlive)).unwrap();
+        sender = await!(sender.send(TunnelMessage::Message(vec![1,2,3,4]))).unwrap();
+        let (data, new_receiver) = await!(receive(receiver)).unwrap();
+        receiver = new_receiver;
+        assert_eq!(data, TunnelMessage::Message(vec![5,6,7]));
+
+        for _ in 0 .. 8usize {
+            tick_sender = await!(tick_sender.send(())).unwrap();
+        }
+        let (data, new_receiver) = await!(receive(receiver)).unwrap();
+        receiver = new_receiver;
+        assert_eq!(data, TunnelMessage::KeepAlive);
+
+        for _ in 0 .. 8usize {
+            tick_sender = await!(tick_sender.send(())).unwrap();
+        }
+        let err = match await!(receive(receiver)) {
+            Ok(_) => unreachable!(),
+            Err(e) => e
+        };
+        assert_eq!(err, ReadError::Closed);
+
+        fin_sender.send(true).unwrap();
+        Ok(())
+    }
+
+    #[async]
+    fn run_b(mut receiver: mpsc::Receiver<TunnelMessage>, 
+             mut sender: mpsc::Sender<TunnelMessage>,
+             _tick_sender: mpsc::Sender<()>,
+             fin_sender: oneshot::Sender<bool>) -> Result<(), ()> {
+
+        let (data, new_receiver) = await!(receive(receiver)).unwrap();
+        receiver = new_receiver;
+        assert_eq!(data, TunnelMessage::Message(vec![1,2,3,4]));
+        sender = await!(sender.send(TunnelMessage::KeepAlive)).unwrap();
+        sender = await!(sender.send(TunnelMessage::Message(vec![5,6,7]))).unwrap();
+
+        fin_sender.send(true).unwrap();
+        Ok(())
+    }
+
+    #[test]
+    fn test_tunnel_basic() {
+        let mut core = Core::new().unwrap();
+        let handle = core.handle();
+
+        // Create a mock time service:
+        let (tick_sender, tick_receiver) = mpsc::channel::<()>(0);
+        let timer_client = create_timer_incoming(tick_receiver, &handle).unwrap();
+
+        /*      a          c          b
+         * a_ca | <-- c_ca | c_cb --> | b_cb
+         *      |          |          |
+         * a_ac | --> c_ac | c_bc <-- | b_bc
+        */
+
+        let (a_ac, c_ac) = mpsc::channel::<TunnelMessage>(0);
+        let (c_ca, a_ca) = mpsc::channel::<TunnelMessage>(0);
+        let (b_bc, c_bc) = mpsc::channel::<TunnelMessage>(0);
+        let (c_cb, b_cb) = mpsc::channel::<TunnelMessage>(0);
+
+        let keepalive_ticks = 16;
+
+        let tloop = tunnel_loop(c_ac, c_ca, 
+                                c_bc, c_cb,
+                                timer_client,
+                                keepalive_ticks);
+        handle.spawn(tloop.then(|e| {
+            println!("tloop error occured: {:?}", e);
+            Ok(())
+        }));
+
+        let (fin_sender_a, fin_receiver_a) = oneshot::channel::<bool>();
+        let (fin_sender_b, fin_receiver_b) = oneshot::channel::<bool>();
+
+        handle.spawn(run_a(a_ca, a_ac, tick_sender.clone(), fin_sender_a));
+        handle.spawn(run_b(b_cb, b_bc, tick_sender.clone(), fin_sender_b));
+
+        assert_eq!(core.run(fin_receiver_a).unwrap(), true);
+        assert_eq!(core.run(fin_receiver_b).unwrap(), true);
+
+    }
+}
+
