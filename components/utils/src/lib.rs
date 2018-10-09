c5b818df6755c5a464f01961c9542e02c50a32da
real <real@freedomlayer.org>
2018-10-09 14:17:48 +0300

    Added define_fixed_bytes macro to utils.

--- components/utils/src/lib.rs
+++ components/utils/src/lib.rs
@@ -23,6 +23,8 @@ pub mod int_convert;
 pub mod safe_arithmetic;
 #[macro_use]
 pub mod big_array;
+#[macro_use]
+pub mod define_fixed_bytes;
 pub mod async_adapter;
 pub mod frame_codec;
 pub mod async_test_utils;
