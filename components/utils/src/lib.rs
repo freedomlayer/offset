c51443043a5270ee4b051d0fe60174d18eaef320
real <real@freedomlayer.org>
2018-10-09 18:24:58 +0300

    Removed unused utils code.

--- components/utils/src/lib.rs
+++ components/utils/src/lib.rs
@@ -15,9 +15,6 @@ extern crate async_mutex;
 
 extern crate serde;
 
-mod close_handle; 
-mod service_client;
-mod stream_mediator;
 
 pub mod int_convert;
 pub mod safe_arithmetic;
@@ -29,8 +26,4 @@ pub mod async_adapter;
 pub mod frame_codec;
 pub mod async_test_utils;
 
-pub use self::close_handle::CloseHandle;
-pub use self::service_client::{ServiceClient, ServiceClientError};
-pub use self::stream_mediator::{StreamMediator, StreamMediatorError};
-// pub use self::macros::TryFromBytesError;
 
