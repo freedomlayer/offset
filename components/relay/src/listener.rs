ddc0ad345bc88b27cde1b563f8c5eb5f1c3893ed
real <real@freedomlayer.org>
2018-10-17 16:28:52 +0300

    Changed test_listener_keep_alive_basic() to be deterministic.

--- components/relay/src/listener.rs
+++ components/relay/src/listener.rs
@@ -18,7 +18,7 @@ enum ListenerLoopError {
     IncomingMessagesFailure,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum ListenerEvent {
     ListenerReceiver(RelayListenIn),
     OutgoingMessage(IncomingConnection),
@@ -33,7 +33,8 @@ async fn listener_loop<M,K,MO,KI,TS>(listener_receiver: M,
                       outgoing_messages: MO,
                       mut incoming_messages: KI,
                       timer_stream: TS, 
-                      keepalive_ticks: usize) 
+                      keepalive_ticks: usize,
+                      mut opt_event_sender: Option<mpsc::Sender<ListenerEvent>>)
     -> Result<(), ListenerLoopError> 
 where
     M: Stream<Item=RelayListenIn> + Unpin + 'static,
@@ -67,6 +68,9 @@ where
 
 
     while let Some(listener_event) = await!(listener_events.next()) {
+        if let Some(ref mut event_sender) = opt_event_sender {
+            await!(event_sender.send(listener_event.clone())).unwrap();
+        }
         match listener_event {
             ListenerEvent::ListenerReceiver(relay_listen_in) => {
                 match relay_listen_in {
@@ -128,7 +132,8 @@ where
                   outgoing_messages,
                   incoming_messages.sink_map_err(|_| ()),
                   timer_stream,
-                  keepalive_ticks);
+                  keepalive_ticks,
+                  None);
 
     let spawn_fut = listener_fut.map_err(|e| {
         error!("listener_loop() error: {:?}", e);
@@ -154,11 +159,13 @@ mod tests {
         mut wsender: mpsc::Sender<IncomingConnection>,
         mut receiver: mpsc::Receiver<RelayListenOut>, 
         mut sender: mpsc::Sender<RelayListenIn>,
-        mut tick_sender: mpsc::Sender<()>) -> Result<(),()> {
+        mut tick_sender: mpsc::Sender<()>,
+        mut event_receiver: mpsc::Receiver<ListenerEvent>) {
 
         // Make sure that keepalives are sent on time:
         for i in 0 .. 8usize {
             await!(tick_sender.send(())).unwrap();
+            let _ = await!(event_receiver.next()).unwrap();
         }
 
         let msg = await!(receiver.next()).unwrap();
@@ -167,12 +174,14 @@ mod tests {
         // Check send/receive messages:
         let public_key = PublicKey::from(&[0xee; PUBLIC_KEY_LEN]);
         await!(wsender.send(IncomingConnection(public_key.clone())));
+        let _ = await!(event_receiver.next()).unwrap();
 
         let msg = await!(receiver.next()).unwrap();
         assert_eq!(msg, RelayListenOut::IncomingConnection(IncomingConnection(public_key)));
 
         let public_key = PublicKey::from(&[0xaa; PUBLIC_KEY_LEN]);
         await!(sender.send(RelayListenIn::RejectConnection(RejectConnection(public_key.clone())))).unwrap();
+        let _ = await!(event_receiver.next()).unwrap();
 
         let msg = await!(wreceiver.next()).unwrap();
         assert_eq!(msg, RejectConnection(public_key));
@@ -180,18 +189,22 @@ mod tests {
         // Check that lack of keepalives causes disconnection:
         for i in 0 .. 7usize {
             await!(tick_sender.send(())).unwrap();
+            let _ = await!(event_receiver.next()).unwrap();
         }
 
         let public_key = PublicKey::from(&[0xaa; PUBLIC_KEY_LEN]);
         await!(sender.send(RelayListenIn::KeepAlive)).unwrap();
+        let _ = await!(event_receiver.next()).unwrap();
 
         await!(tick_sender.send(())).unwrap();
+        let _ = await!(event_receiver.next()).unwrap();
 
         let msg = await!(receiver.next()).unwrap();
         assert_eq!(msg, RelayListenOut::KeepAlive);
 
         for i in 0 .. 15usize {
             await!(tick_sender.send(())).unwrap();
+            let _ = await!(event_receiver.next()).unwrap();
         }
 
         let msg = await!(receiver.next()).unwrap();
@@ -207,8 +220,6 @@ mod tests {
         drop(wsender);
         // drop(receiver);
         drop(sender);
-
-        Ok(())
     }
 
     #[test]
@@ -231,17 +242,32 @@ mod tests {
         let timer_stream = thread_pool.run(timer_client.request_timer_stream()).unwrap();
 
         let keepalive_ticks = 16;
-        let (w_a_ca, w_a_ac) = listener_keepalive(a_ca, 
-                                  a_ac,
-                                  timer_stream,
-                                  keepalive_ticks,
-                                  thread_pool.clone());
+        let (event_sender, event_receiver) = mpsc::channel(0);
+
+        let (new_listener_sender, outgoing_messages) = mpsc::channel::<IncomingConnection>(0);
+        let (incoming_messages, new_listener_receiver) = mpsc::channel::<RejectConnection>(0);
+        let listener_fut = listener_loop(a_ca,
+                      a_ac.sink_map_err(|_| ()),
+                      outgoing_messages,
+                      incoming_messages.sink_map_err(|_| ()),
+                      timer_stream,
+                      keepalive_ticks,
+                      Some(event_sender));
+
+        let spawn_fut = listener_fut.map_err(|e| {
+            error!("listener_loop() error: {:?}", e);
+            ()
+        }).then(|_| future::ready(()));
+        thread_pool.spawn(spawn_fut);
+    
+        let (w_a_ca, w_a_ac) = (new_listener_receiver, new_listener_sender);
 
         thread_pool.run(
             run_listener_keepalive_basic(
                 w_a_ca, w_a_ac,
                 c_ac, c_ca,
-                tick_sender)
-        ).unwrap();
+                tick_sender,
+                event_receiver)
+        );
     }
 }
