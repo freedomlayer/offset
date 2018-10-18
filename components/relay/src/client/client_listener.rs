8ee459f374a11ef3e8485bcb0994a167c79edc0b
real <real@freedomlayer.org>
2018-10-18 16:07:25 +0300

    Added serialization for sender and receiver at client_listener().

--- components/relay/src/client/client_listener.rs
+++ components/relay/src/client/client_listener.rs
@@ -1,8 +1,13 @@
 use std::collections::HashSet;
+use futures::{future, Stream, StreamExt, Sink, SinkExt};
+
 use crypto::identity::PublicKey;
+use proto::relay::messages::{InitConnection, RelayListenOut, IncomingConnection};
+use proto::relay::serialize::{serialize_init_connection,
+    serialize_relay_listen_in, deserialize_relay_listen_out};
 
 use timer::TimerClient;
-use super::connector::Connector;
+use super::connector::{Connector, ConnPair};
 
 enum AllowedOp {
     Clear,
@@ -55,18 +60,63 @@ impl Allowed {
     }
 }
 
+#[derive(Debug)]
 pub enum ClientListenerError {
+    RequestTimerStreamError,
+    SendInitConnectionError,
+    ConnectionFailure,
+    ConnectionTimedOut,
 }
 
-pub async fn client_listener<C>(mut connector: C,
+enum ClientListenerEvent {
+    TimerTick,
+
+}
+
+pub async fn client_listener<C, IAO>(mut connector: C,
+                                incoming_allowed: IAO,
                                 timer_client: TimerClient) -> Result<(), ClientListenerError>
 where
     C: Connector<Address=(), SendItem=Vec<u8>, RecvItem=Vec<u8>>,
+    IAO: Stream<Item=AllowedOp>,
 {
+    let timer_stream = await!(timer_client.request_timer_stream())
+        .map_err(|_|  ClientListenerError::RequestTimerStreamError)?;
+
     let conn_pair = match await!(connector.connect(())) {
         Some(conn_pair) => conn_pair,
-        None => return Ok(()),
+        None => return Err(ClientListenerError::ConnectionFailure),
     };
+
+    let ConnPair {mut sender, receiver} = conn_pair;
+    let ser_init_connection = serialize_init_connection(&InitConnection::Listen);
+
+    await!(sender.send(ser_init_connection))
+        .map_err(|_| ClientListenerError::SendInitConnectionError)?;
+
+    // Add serialization for sender:
+    let mut sender = sender
+        .sink_map_err(|_| ())
+        .with(|vec| -> future::Ready<Result<_,()>> {
+            future::ready(Ok(serialize_relay_listen_in(&vec)))
+        });
+
+    // Add deserialization for receiver:
+    let mut receiver = receiver.map(|relay_listen_in| {
+        deserialize_relay_listen_out(&relay_listen_in).ok()
+    }).take_while(|opt_relay_listen_in| {
+        future::ready(opt_relay_listen_in.is_some())
+    }).map(|opt| opt.unwrap());
+
+    let mut allowed = Allowed::new();
+
+    while let Some(relay_listen_out) = await!(receiver.next()) {
+        match relay_listen_out {
+            RelayListenOut::KeepAlive => {},
+            RelayListenOut::IncomingConnection(IncomingConnection(public_key)) => {},
+        }
+    }
+
     Ok(())
 
     // TODO: 
