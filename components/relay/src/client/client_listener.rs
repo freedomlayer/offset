7283a77d33978ac14f2f62b40dd63e7281013bd1
real <real@freedomlayer.org>
2018-10-18 16:12:04 +0300

    Renamed Allowed -> AccessControl.

--- components/relay/src/client/client_listener.rs
+++ components/relay/src/client/client_listener.rs
@@ -9,52 +9,52 @@ use proto::relay::serialize::{serialize_init_connection,
 use timer::TimerClient;
 use super::connector::{Connector, ConnPair};
 
-enum AllowedOp {
+enum AccessControlOp {
     Clear,
     Add(PublicKey),
     Remove(PublicKey),
     AllowAll,
 }
 
-enum InnerAllowed {
+enum InnerAccessControl {
     Only(HashSet<PublicKey>),
     All,
 }
 
-struct Allowed {
-    inner: InnerAllowed,
+struct AccessControl {
+    inner: InnerAccessControl,
 }
 
 #[derive(Debug)]
 struct ApplyOpError;
 
-impl Allowed {
-    pub fn new() -> Allowed {
-        Allowed {
-            inner: InnerAllowed::Only(HashSet::new()) 
+impl AccessControl {
+    pub fn new() -> AccessControl {
+        AccessControl {
+            inner: InnerAccessControl::Only(HashSet::new()) 
         }
     }
 
-    pub fn apply_op(&mut self, allowed_op: AllowedOp) -> Result<(), ApplyOpError> {
+    pub fn apply_op(&mut self, allowed_op: AccessControlOp) -> Result<(), ApplyOpError> {
         match allowed_op {
-            AllowedOp::Clear => self.inner = InnerAllowed::Only(HashSet::new()),
-            AllowedOp::Add(public_key) => {
+            AccessControlOp::Clear => self.inner = InnerAccessControl::Only(HashSet::new()),
+            AccessControlOp::Add(public_key) => {
                 match self.inner {
-                    InnerAllowed::Only(ref mut only_set) => {
+                    InnerAccessControl::Only(ref mut only_set) => {
                         only_set.insert(public_key);
                     },
-                    InnerAllowed::All => return Err(ApplyOpError),
+                    InnerAccessControl::All => return Err(ApplyOpError),
                 }
             }
-            AllowedOp::Remove(public_key) => {
+            AccessControlOp::Remove(public_key) => {
                 match self.inner {
-                    InnerAllowed::Only(ref mut only_set) => {
+                    InnerAccessControl::Only(ref mut only_set) => {
                         only_set.remove(&public_key);
                     },
-                    InnerAllowed::All => return Err(ApplyOpError),
+                    InnerAccessControl::All => return Err(ApplyOpError),
                 }
             }
-            AllowedOp::AllowAll => self.inner = InnerAllowed::All,
+            AccessControlOp::AllowAll => self.inner = InnerAccessControl::All,
         }
         Ok(())
     }
@@ -73,12 +73,12 @@ enum ClientListenerEvent {
 
 }
 
-pub async fn client_listener<C, IAO>(mut connector: C,
-                                incoming_allowed: IAO,
-                                timer_client: TimerClient) -> Result<(), ClientListenerError>
+pub async fn client_listener<C, IAC>(mut connector: C,
+                                incoming_access_control: IAC,
+                                mut timer_client: TimerClient) -> Result<(), ClientListenerError>
 where
     C: Connector<Address=(), SendItem=Vec<u8>, RecvItem=Vec<u8>>,
-    IAO: Stream<Item=AllowedOp>,
+    IAC: Stream<Item=AccessControlOp>,
 {
     let timer_stream = await!(timer_client.request_timer_stream())
         .map_err(|_|  ClientListenerError::RequestTimerStreamError)?;
@@ -108,7 +108,7 @@ where
         future::ready(opt_relay_listen_in.is_some())
     }).map(|opt| opt.unwrap());
 
-    let mut allowed = Allowed::new();
+    let mut access_control = AccessControl::new();
 
     while let Some(relay_listen_out) = await!(receiver.next()) {
         match relay_listen_out {
