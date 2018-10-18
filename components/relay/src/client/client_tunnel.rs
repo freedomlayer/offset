3f322134208b9c4555df607bfff4df094b07d47b
real <real@freedomlayer.org>
2018-10-18 13:10:29 +0300

    Added timer_stream argument to client_tunnel().

--- components/relay/src/client/client_tunnel.rs
+++ components/relay/src/client/client_tunnel.rs
@@ -1,16 +1,19 @@
 use futures::{Stream, Sink};
 use proto::relay::messages::{InitConnection, TunnelMessage};
+use timer::TimerTick;
 
 /// Run the keepalive maintenance, exposing to the user the ability to send and receive Vec<u8>
 /// frames.
-pub async fn client_tunnel<TTS,TTSE,FTR,UFTS,UFTSE,UTTR>(to_tunnel_sender: TTS, from_tunnel_receiver: FTR, 
+pub async fn client_tunnel<TTS,TTSE,FTR,UFTS,UFTSE,UTTR,TS>(to_tunnel_sender: TTS, from_tunnel_receiver: FTR, 
                            user_from_tunnel_sender: UFTS, user_to_tunnel_receiver: UTTR,
+                           timer_stream: TS,
                            keepalive_ticks: usize) -> Result<(),()> 
 where
     TTS: Sink<SinkItem=TunnelMessage, SinkError=TTSE>,
     FTR: Stream<Item=TunnelMessage>,
     UFTS: Sink<SinkItem=Vec<u8>, SinkError=UFTSE>,
     UTTR: Stream<Item=Vec<u8>>,
+    TS: Stream<Item=TimerTick>,
 {
     // TODO:
     // - Forwared TunnelMessages.
--- components/relay/src/client/connector.rs
+++ components/relay/src/client/connector.rs
@@ -9,6 +9,8 @@ use proto::relay::messages::{InitConnection};
 use proto::relay::serialize::{serialize_init_connection, serialize_tunnel_message,
     deserialize_tunnel_message};
 
+use timer::TimerClient;
+
 use super::client_tunnel::client_tunnel;
 
 pub struct ConnPair<Item> {
@@ -22,37 +24,46 @@ pub trait Connector {
     fn connect(&mut self, address: Self::Address) -> FutureObj<Result<ConnPair<Self::Item>,()>>;
 }
 
-pub struct RelayConnector<A,C,S> {
-    connector: C,
-    spawner: S,
-    keepalive_ticks: usize,
-    phantom_address: PhantomData<A>,
+#[derive(Debug)]
+pub enum RelayConnectorError {
+    InnerConnectorError,
+    SendInitConnectionError,
+    RequestTimerStreamError,
+    SpawnClientTunnelError,
 }
 
-impl<A,C,S> RelayConnector<A,C,S> 
+pub struct RelayConnector<C,S> {
+    connector: C,
+    spawner: S,
+    timer_client: TimerClient,
+    keepalive_ticks: usize,
+}
+
+impl<A: 'static,C,S> RelayConnector<C,S> 
 where
     C: Connector<Address=A, Item=Vec<u8>>,
     S: Spawn,
 {
-    pub fn new(connector: C, spawner: S, keepalive_ticks: usize) -> RelayConnector<A,C,S> {
+    pub fn new(connector: C, spawner: S, timer_client: TimerClient, keepalive_ticks: usize) -> RelayConnector<C,S> {
         RelayConnector {
             connector,
             spawner,
+            timer_client,
             keepalive_ticks,
-            phantom_address: PhantomData,
         }
     }
 
     async fn relay_connect(&mut self, relay_address: A, remote_public_key: PublicKey) 
-        -> Result<ConnPair<Vec<u8>>,()> {
+        -> Result<ConnPair<Vec<u8>>, RelayConnectorError> {
 
-        let mut conn_pair = await!(self.connector.connect(relay_address))?;
+        let mut conn_pair = await!(self.connector.connect(relay_address))
+            .map_err(|_| RelayConnectorError::InnerConnectorError)?;
 
         // Send an InitConnection::Connect(PublicKey) message to remote side:
         let init_connection = InitConnection::Connect(remote_public_key);
         let ser_init_connection = serialize_init_connection(&init_connection);
         await!(conn_pair.sender.send(ser_init_connection))
-            .map_err(|_| ())?;
+            .map_err(|_| RelayConnectorError::SendInitConnectionError)?;
 
         let ConnPair {sender, receiver} = conn_pair;
 
@@ -73,15 +84,19 @@ where
         let (user_from_tunnel_sender, user_from_tunnel) = mpsc::channel(0);
         let (user_to_tunnel, user_to_tunnel_receiver) = mpsc::channel(0);
 
+        let timer_stream = await!(self.timer_client.request_timer_stream())
+            .map_err(|_| RelayConnectorError::RequestTimerStreamError)?;
+
         let client_tunnel = client_tunnel(to_tunnel_sender, from_tunnel_receiver,
                                           user_from_tunnel_sender, user_to_tunnel_receiver,
+                                          timer_stream,
                                           self.keepalive_ticks)
             .map_err(|e| {
                 error!("client_tunnel error: {:?}", e);
             }).then(|_| future::ready(()));
 
         self.spawner.spawn(client_tunnel)
-            .map_err(|_| ())?;
+            .map_err(|_| RelayConnectorError::SpawnClientTunnelError)?;
 
         Ok(ConnPair {
             sender: user_to_tunnel,
@@ -90,9 +105,9 @@ where
     }
 }
 
-impl<A,C,S> Connector for RelayConnector<A,C,S> 
+impl<A,C,S> Connector for RelayConnector<C,S> 
 where
-    A: Sync + Send,
+    A: Sync + Send + 'static,
     C: Connector<Address=A, Item=Vec<u8>> + Sync + Send,
     S: Spawn + Sync + Send,
 {
@@ -101,7 +116,9 @@ where
 
     fn connect(&mut self, address: Self::Address) -> FutureObj<Result<ConnPair<Self::Item>,()>> {
         let (relay_address, remote_public_key) = address;
-        FutureObj::new(self.relay_connect(relay_address, remote_public_key).boxed())
+        let relay_connect = self.relay_connect(relay_address, remote_public_key)
+            .map_err(|_| ());
+        FutureObj::new(relay_connect.boxed())
     }
 }
 
