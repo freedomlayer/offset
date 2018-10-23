5e9209c959d6fe753d7564d6434d2e49960ea0c8
real <real@freedomlayer.org>
2018-10-23 12:18:55 +0300

    Simplified access_control to only have add/remove operations.

--- components/relay/src/client/access_control.rs
+++ components/relay/src/client/access_control.rs
@@ -3,19 +3,12 @@ use std::collections::HashSet;
 
 #[derive(Clone, Debug)]
 pub enum AccessControlOp {
-    Clear,
     Add(PublicKey),
     Remove(PublicKey),
-    AllowAll,
-}
-
-enum InnerAccessControl {
-    Only(HashSet<PublicKey>),
-    All,
 }
 
 pub struct AccessControl {
-    inner: InnerAccessControl,
+    allowed: HashSet<PublicKey>,
 }
 
 #[derive(Debug)]
@@ -24,40 +17,25 @@ pub struct ApplyOpError;
 impl AccessControl {
     pub fn new() -> AccessControl {
         AccessControl {
-            inner: InnerAccessControl::Only(HashSet::new()) 
+            allowed: HashSet::new(),
         }
     }
 
     pub fn apply_op(&mut self, allowed_op: AccessControlOp) -> Result<(), ApplyOpError> {
         match allowed_op {
-            AccessControlOp::Clear => self.inner = InnerAccessControl::Only(HashSet::new()),
             AccessControlOp::Add(public_key) => {
-                match self.inner {
-                    InnerAccessControl::Only(ref mut only_set) => {
-                        only_set.insert(public_key);
-                    },
-                    InnerAccessControl::All => return Err(ApplyOpError),
-                }
+                self.allowed.insert(public_key);
             }
             AccessControlOp::Remove(public_key) => {
-                match self.inner {
-                    InnerAccessControl::Only(ref mut only_set) => {
-                        only_set.remove(&public_key);
-                    },
-                    InnerAccessControl::All => return Err(ApplyOpError),
-                }
+                self.allowed.remove(&public_key);
             }
-            AccessControlOp::AllowAll => self.inner = InnerAccessControl::All,
         }
         Ok(())
     }
 
     /// Check if a certain public key is allowed.
     pub fn is_allowed(&self, public_key: &PublicKey) -> bool {
-        match &self.inner {
-            InnerAccessControl::All => true,
-            InnerAccessControl::Only(only) => only.contains(public_key)
-        }
+        self.allowed.contains(public_key)
     }
 }
 
