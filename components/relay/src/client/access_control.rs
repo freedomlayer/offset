2289d0776e5390db33b971d029268ee69f65b91b
real <real@freedomlayer.org>
2018-10-23 14:26:41 +0300

    Added a basic test for AccessControl.

--- components/relay/src/client/access_control.rs
+++ components/relay/src/client/access_control.rs
@@ -40,4 +40,44 @@ impl AccessControl {
 }
 
 
-// TODO: Add tests.
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crypto::identity::{PublicKey, PUBLIC_KEY_LEN};
+
+    #[test]
+    fn test_access_control_basic() {
+        let a_public_key = PublicKey::from(&[0xaa; PUBLIC_KEY_LEN]);
+        let b_public_key = PublicKey::from(&[0xbb; PUBLIC_KEY_LEN]);
+
+        let mut ac = AccessControl::new();
+        assert!(!ac.is_allowed(&a_public_key));
+        assert!(!ac.is_allowed(&b_public_key));
+
+        // Add a:
+        ac.apply_op(AccessControlOp::Add(a_public_key.clone()));
+        assert!(ac.is_allowed(&a_public_key));
+        assert!(!ac.is_allowed(&b_public_key));
+
+        // Add b:
+        ac.apply_op(AccessControlOp::Add(b_public_key.clone()));
+        assert!(ac.is_allowed(&a_public_key));
+        assert!(ac.is_allowed(&b_public_key));
+
+        // Remove a:
+        ac.apply_op(AccessControlOp::Remove(a_public_key.clone()));
+        assert!(!ac.is_allowed(&a_public_key));
+        assert!(ac.is_allowed(&b_public_key));
+
+        // Remove b:
+        ac.apply_op(AccessControlOp::Remove(b_public_key.clone()));
+        assert!(!ac.is_allowed(&a_public_key));
+        assert!(!ac.is_allowed(&b_public_key));
+
+        // Remove b again:
+        ac.apply_op(AccessControlOp::Remove(b_public_key.clone()));
+        assert!(!ac.is_allowed(&a_public_key));
+        assert!(!ac.is_allowed(&b_public_key));
+    }
+}
+
