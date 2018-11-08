0aee32c1ccecf5a35b957fef0f74920e75192df0
real <real@freedomlayer.org>
2018-11-08 13:51:11 +0200

    Began writing a handle_init test. Stack overflow occurs.

--- components/funder/src/handler/handle_init.rs
+++ components/funder/src/handler/handle_init.rs
@@ -31,3 +31,65 @@ impl<A: Clone + 'static, R: CryptoRandom> MutableFunderHandler<A,R> {
         }
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::rc::Rc;
+
+    use crate::state::{FunderState, FunderMutation};
+    use crate::ephemeral::FunderEphemeral;
+
+    use futures::executor::ThreadPool;
+    use futures::{future, FutureExt};
+    use futures::task::SpawnExt;
+    use identity::{create_identity, IdentityClient};
+
+    use crypto::test_utils::DummyRandom;
+    use crypto::identity::{SoftwareEd25519Identity,
+                            generate_pkcs8_key_pair, PUBLIC_KEY_LEN,
+                            PublicKey};
+
+
+    async fn task_handle_init_basic(identity_client: IdentityClient) {
+
+        let local_pk = await!(identity_client.request_public_key()).unwrap();
+        let pk_b = PublicKey::from(&[0xbb; PUBLIC_KEY_LEN]);
+
+        let mut state = FunderState::new(&local_pk);
+        let f_mutation = FunderMutation::AddFriend((pk_b, 3u32)); // second arg is address
+        state.mutate(&f_mutation);
+        /*
+
+        let ephemeral = FunderEphemeral::new(&state);
+        let rng = DummyRandom::new(&[2u8]);
+
+        let mutable_funder_handler = MutableFunderHandler {
+            state,
+            ephemeral,
+            identity_client: identity_client.clone(),
+            rng: Rc::new(rng),
+            mutations: Vec::new(),
+            outgoing_comms: Vec::new(),
+            responses_received: Vec::new(),
+        };
+        */
+    }
+
+    #[test]
+    fn test_handle_init_basic() {
+        // Start identity service:
+        let mut thread_pool = ThreadPool::new().unwrap();
+
+        let rng = DummyRandom::new(&[1u8]);
+        let pkcs8 = generate_pkcs8_key_pair(&rng);
+        let identity = SoftwareEd25519Identity::from_pkcs8(&pkcs8).unwrap();
+        let (requests_sender, identity_server) = create_identity(identity);
+        let identity_client = IdentityClient::new(requests_sender);
+        thread_pool.spawn(identity_server.then(|_| future::ready(()))).unwrap();
+
+        thread_pool.run(task_handle_init_basic(identity_client));
+    }
+}
