833796231afec05d87533b85eb926de4f8c6ae55
real <real@freedomlayer.org>
2018-11-21 12:30:32 +0200

    Completed long route payment test.

--- components/funder/src/tests/tests.rs
+++ components/funder/src/tests/tests.rs
@@ -4,13 +4,11 @@ use futures::task::Spawn;
 use crypto::identity::PublicKey;
 use crypto::uid::{Uid, UID_LEN};
 
-use crate::types::{IncomingControlMessage,
-    AddFriend, FriendStatus,
-    SetFriendStatus, RequestsStatus, SetRequestsStatus,
-    SetFriendRemoteMaxDebt, FriendsRoute, UserRequestSendFunds,
+use crate::types::{IncomingControlMessage, FriendStatus,
+    RequestsStatus, FriendsRoute, UserRequestSendFunds,
     InvoiceId, INVOICE_ID_LEN, ResponseSendFundsResult,
     ReceiptAck};
-use crate::report::{FunderReport, FriendLivenessReport,
+use crate::report::{FunderReport,
                     ChannelStatusReport};
 use super::utils::create_node_controls;
 
@@ -42,19 +40,8 @@ async fn task_funder_basic(spawner: impl Spawn + Clone + Send + 'static) {
     await!(node_controls[1].set_requests_status(&public_keys[0], RequestsStatus::Open));
 
     // Wait for liveness:
-    let pk1 = node_controls[1].public_key.clone();
-    let pred = |report: &FunderReport<_>| {
-       let friend = report.friends.get(&pk1).unwrap();
-       friend.liveness == FriendLivenessReport::Online
-    };
-    await!(node_controls[0].recv_until(pred));
-
-    let pk0 = node_controls[0].public_key.clone();
-    let pred = |report: &FunderReport<_>| {
-       let friend = report.friends.get(&pk0).unwrap();
-       friend.liveness == FriendLivenessReport::Online
-    };
-    await!(node_controls[1].recv_until(pred));
+    await!(node_controls[0].wait_until_alive(&public_keys[1]));
+    await!(node_controls[1].wait_until_alive(&public_keys[0]));
 
     // Send credits 0 --> 1
     let user_request_send_funds = UserRequestSendFunds {
@@ -87,7 +74,7 @@ async fn task_funder_basic(spawner: impl Spawn + Clone + Send + 'static) {
     await!(node_controls[0].recv_until(pred));
 
     // Verify expected balances:
-    let friend = node_controls[0].report.friends.get(&pk1).unwrap();
+    let friend = node_controls[0].report.friends.get(&public_keys[1]).unwrap();
     let tc_report = match &friend.channel_status {
        ChannelStatusReport::Consistent(tc_report) => tc_report,
        _ => unreachable!(),
@@ -96,7 +83,7 @@ async fn task_funder_basic(spawner: impl Spawn + Clone + Send + 'static) {
 
 
     let pred = |report: &FunderReport<_>| {
-       let friend = report.friends.get(&pk0).unwrap();
+       let friend = report.friends.get(&public_keys[0]).unwrap();
        let tc_report = match &friend.channel_status {
            ChannelStatusReport::Consistent(tc_report) => tc_report,
            _ => unreachable!(),
@@ -121,47 +108,87 @@ async fn task_funder_forward_payment(spawner: impl Spawn + Clone + Send + 'stati
     let mut node_controls = await!(create_node_controls(num_nodes, spawner));
 
     // Create topology:
-    //-------------------------
-    // Add friends to 0: (1)
-    let add_friend = AddFriend {
-        friend_public_key: node_controls[1].public_key.clone(),
-        address: 1u32,
-        name: "node1".into(),
-        balance: 8, 
-    };
-    await!(node_controls[0].send(IncomingControlMessage::AddFriend(add_friend))).unwrap();
-    await!(node_controls[0].recv_until(|report| report.friends.len() == 1));
+    // ----------------
+    let public_keys = node_controls
+        .iter()
+        .map(|nc| nc.public_key.clone())
+        .collect::<Vec<PublicKey>>();
 
-    // Add friends to 1: (0 and 2)
-    let add_friend = AddFriend {
-        friend_public_key: node_controls[0].public_key.clone(),
-        address: 0u32,
-        name: "node0".into(),
-        balance: -8, 
-    };
-    await!(node_controls[1].send(IncomingControlMessage::AddFriend(add_friend))).unwrap();
-    let add_friend = AddFriend {
-        friend_public_key: node_controls[2].public_key.clone(),
-        address: 2u32,
-        name: "node0".into(),
-        balance: 6, 
-    };
-    await!(node_controls[1].send(IncomingControlMessage::AddFriend(add_friend))).unwrap();
-    await!(node_controls[1].recv_until(|report| report.friends.len() == 2));
+    for i in 0 .. 3 {
+        println!("public_key{}: {}", i, public_keys[i][0]);
+    }
+    println!();
 
-    // Add friends to 2: (1)
-    let add_friend = AddFriend {
-        friend_public_key: node_controls[1].public_key.clone(),
-        address: 1u32,
-        name: "node1".into(),
-        balance: -6, 
+    // Add friends:
+    await!(node_controls[0].add_friend(&public_keys[1], 1u32, "node1", 8));
+    await!(node_controls[1].add_friend(&public_keys[0], 0u32, "node0", -8));
+    await!(node_controls[1].add_friend(&public_keys[2], 2u32, "node2", 6));
+    await!(node_controls[2].add_friend(&public_keys[1], 0u32, "node0", -6));
+
+    // Enable friends:
+    await!(node_controls[0].set_friend_status(&public_keys[1], FriendStatus::Enable));
+    await!(node_controls[1].set_friend_status(&public_keys[0], FriendStatus::Enable));
+    await!(node_controls[1].set_friend_status(&public_keys[2], FriendStatus::Enable));
+    await!(node_controls[2].set_friend_status(&public_keys[1], FriendStatus::Enable));
+
+    // Set remote max debt:
+    await!(node_controls[0].set_remote_max_debt(&public_keys[1], 200));
+    await!(node_controls[1].set_remote_max_debt(&public_keys[0], 100));
+    await!(node_controls[1].set_remote_max_debt(&public_keys[2], 300));
+    await!(node_controls[2].set_remote_max_debt(&public_keys[1], 400));
+
+    // Open requests, allowing this route: 0 --> 1 --> 2
+    await!(node_controls[1].set_requests_status(&public_keys[0], RequestsStatus::Open));
+    await!(node_controls[2].set_requests_status(&public_keys[1], RequestsStatus::Open));
+
+    // Wait until alive:
+    await!(node_controls[0].wait_until_alive(&public_keys[1]));
+    await!(node_controls[1].wait_until_alive(&public_keys[0]));
+    await!(node_controls[1].wait_until_alive(&public_keys[2]));
+    await!(node_controls[2].wait_until_alive(&public_keys[1]));
+
+
+    // Send credits 0 --> 2
+    let user_request_send_funds = UserRequestSendFunds {
+        request_id: Uid::from(&[3; UID_LEN]),
+        route: FriendsRoute { public_keys: vec![
+            public_keys[0].clone(), 
+            public_keys[1].clone(), 
+            public_keys[2].clone()] },
+        invoice_id: InvoiceId::from(&[1; INVOICE_ID_LEN]),
+        dest_payment: 20,
     };
-    await!(node_controls[2].send(IncomingControlMessage::AddFriend(add_friend))).unwrap();
-    await!(node_controls[2].recv_until(|report| report.friends.len() == 1));
+    await!(node_controls[0].send(IncomingControlMessage::RequestSendFunds(user_request_send_funds))).unwrap();
+    let response_received = await!(node_controls[0].recv_until_response()).unwrap();
+    assert_eq!(response_received.request_id, Uid::from(&[3; UID_LEN]));
+    let receipt = match response_received.result {
+        ResponseSendFundsResult::Failure(_) => unreachable!(),
+        ResponseSendFundsResult::Success(send_funds_receipt) => send_funds_receipt,
+    };
 
-    // Enable friends and open requests
-    // --------------------------------
+    // Send ReceiptAck:
+    let receipt_ack = ReceiptAck {
+        request_id: Uid::from(&[3; UID_LEN]),
+        receipt_signature: receipt.signature.clone(),
+    };
+    await!(node_controls[0].send(IncomingControlMessage::ReceiptAck(receipt_ack))).unwrap();
 
+    let pred = |report: &FunderReport<_>| report.num_ready_receipts == 0;
+    await!(node_controls[0].recv_until(pred));
+
+    // Make sure that node2 got the credits:
+    let pred = |report: &FunderReport<_>| {
+        let friend = match report.friends.get(&public_keys[1]) {
+            None => return false,
+            Some(friend) => friend,
+        };
+        let tc_report = match &friend.channel_status {
+           ChannelStatusReport::Consistent(tc_report) => tc_report,
+           _ => return false,
+        };
+        tc_report.balance.balance == -6 + 20
+    };
+    await!(node_controls[2].recv_until(pred));
 }
 
 #[test]
