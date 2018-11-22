ce15793a8a1db71f183bdf1ef544bfce7ca63e02
real <real@freedomlayer.org>
2018-11-22 17:15:11 +0200

    Raw custom int types are now private.

--- components/proto/src/capnp_common.rs
+++ components/proto/src/capnp_common.rs
@@ -15,7 +15,7 @@ use crypto::crypto_rand::RandValue;
 
 
 /// Read the underlying bytes from given `CustomUInt128` reader.
-pub fn read_custom_u_int128(from: &custom_u_int128::Reader) -> Vec<u8> {
+fn read_custom_u_int128(from: &custom_u_int128::Reader) -> Vec<u8> {
     let mut vec = Vec::new();
     vec.write_u64::<BigEndian>(from.get_x0()).unwrap();
     vec.write_u64::<BigEndian>(from.get_x1()).unwrap();
@@ -23,14 +23,14 @@ pub fn read_custom_u_int128(from: &custom_u_int128::Reader) -> Vec<u8> {
 }
 
 /// Fill the components of `CustomUInt128` from given bytes.
-pub fn write_custom_u_int128(from: impl AsRef<[u8]>, to: &mut custom_u_int128::Builder) {
+fn write_custom_u_int128(from: impl AsRef<[u8]>, to: &mut custom_u_int128::Builder) {
     let mut reader = io::Cursor::new(from.as_ref());
     to.set_x0(reader.read_u64::<BigEndian>().unwrap());
     to.set_x1(reader.read_u64::<BigEndian>().unwrap());
 }
 
 /// Read the underlying bytes from given `CustomUInt256` reader.
-pub fn read_custom_u_int256(from: &custom_u_int256::Reader) -> Vec<u8> {
+fn read_custom_u_int256(from: &custom_u_int256::Reader) -> Vec<u8> {
     let mut vec = Vec::new();
     vec.write_u64::<BigEndian>(from.get_x0()).unwrap();
     vec.write_u64::<BigEndian>(from.get_x1()).unwrap();
@@ -40,7 +40,7 @@ pub fn read_custom_u_int256(from: &custom_u_int256::Reader) -> Vec<u8> {
 }
 
 /// Fill the components of `CustomUInt256` from given bytes.
-pub fn write_custom_u_int256(from: impl AsRef<[u8]>, to: &mut custom_u_int256::Builder) {
+fn write_custom_u_int256(from: impl AsRef<[u8]>, to: &mut custom_u_int256::Builder) {
     let mut reader = io::Cursor::new(from.as_ref());
     to.set_x0(reader.read_u64::<BigEndian>().unwrap());
     to.set_x1(reader.read_u64::<BigEndian>().unwrap());
@@ -50,7 +50,7 @@ pub fn write_custom_u_int256(from: impl AsRef<[u8]>, to: &mut custom_u_int256::B
 
 
 /// Read the underlying bytes from given `CustomUInt512` reader.
-pub fn read_custom_u_int512(from: &custom_u_int512::Reader) -> Vec<u8> {
+fn read_custom_u_int512(from: &custom_u_int512::Reader) -> Vec<u8> {
     let mut vec = Vec::new();
     vec.write_u64::<BigEndian>(from.get_x0()).unwrap();
     vec.write_u64::<BigEndian>(from.get_x1()).unwrap();
@@ -64,7 +64,7 @@ pub fn read_custom_u_int512(from: &custom_u_int512::Reader) -> Vec<u8> {
 }
 
 /// Fill the components of `CustomUInt512` from given bytes.
-pub fn write_custom_u_int512(from: impl AsRef<[u8]>, to: &mut custom_u_int512::Builder) {
+fn write_custom_u_int512(from: impl AsRef<[u8]>, to: &mut custom_u_int512::Builder) {
     let mut reader = io::Cursor::new(from.as_ref());
     to.set_x0(reader.read_u64::<BigEndian>().unwrap());
     to.set_x1(reader.read_u64::<BigEndian>().unwrap());
