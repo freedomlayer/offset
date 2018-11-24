49c7e6895cb27abe07c10f7d42f3f167df77d046
real <real@freedomlayer.org>
2018-11-24 18:11:32 +0200

    Added some skeleton code for inner_channeler_loop().

--- components/channeler/src/channeler.rs
+++ components/channeler/src/channeler.rs
@@ -1,6 +1,7 @@
 use std::marker::Unpin;
-use futures::{future, Stream, StreamExt, Sink};
-use futures::task::Spawn;
+use futures::{future, FutureExt, TryFutureExt, Stream, StreamExt, Sink};
+use futures::task::{Spawn, SpawnExt};
+use futures::channel::mpsc;
 
 use proto::funder::messages::{FunderToChanneler, ChannelerToFunder};
 use crypto::identity::PublicKey;
@@ -13,19 +14,54 @@ use relay::client::client_listener::{client_listener, ClientListenerError};
 
 use crate::listener::listener_loop;
 
+pub enum ChannelerEvent<A> {
+    FromFunder(FunderToChanneler<A>),
+    IncomingConnection(()),
+    ConnectionEstablished(()),
+}
 
-fn inner_channeler_loop<FF,TF,C,A>(address: A,
+pub enum ChannelerError {
+    SpawnError,
+}
+
+fn inner_channeler_loop<FF,TF,C,A,S>(address: A,
                         from_funder: FF, 
                         to_funder: TF,
+                        conn_timeout_ticks: usize,
+                        keepalive_ticks: usize,
+                        backoff_ticks: usize,
                         timer_client: TimerClient,
                         connector: C,
-                        mut spawner: impl Spawn + Clone + Send + 'static)
+                        mut spawner: S) -> Result<(), ChannelerError>
 where
-    A: Clone,
-    C: Connector<Address=A, SendItem=Vec<u8>, RecvItem=Vec<u8>>,
+    A: Clone + Send + Sync + 'static,
+    C: Connector<Address=A, SendItem=Vec<u8>, RecvItem=Vec<u8>> + Clone + Send + Sync + 'static,
     FF: Stream<Item=FunderToChanneler<A>>,
     TF: Sink<SinkItem=ChannelerToFunder>,
+    S: Spawn + Clone + Send + Sync + 'static,
 {
+
+    let (addresses_sender, addresses_receiver) = mpsc::channel(0);
+    let (access_control_sender, access_control_receiver) = mpsc::channel(0);
+    let (connections_sender, connections_receiver) = mpsc::channel(0);
+
+    let listener_loop_fut = listener_loop(connector,
+                      address,
+                      addresses_receiver,
+                      access_control_receiver,
+                      connections_sender,
+                      conn_timeout_ticks,
+                      keepalive_ticks,
+                      backoff_ticks,
+                      timer_client.clone(),
+                      spawner.clone())
+        .map_err(|e| {
+            error!("[Channeler] listener_loop error: {:?}", e);
+        }).then(|_| future::ready(()));
+
+    spawner.spawn(listener_loop_fut)
+        .map_err(|_| ChannelerError::SpawnError)?;
+
     unimplemented!();
     // TODO:
     // Handle events in a loop:
