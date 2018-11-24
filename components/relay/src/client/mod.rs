ba33199886552bc66483b45d527ac0024e760d56
real <real@freedomlayer.org>
2018-11-24 12:29:06 +0200

    Changed some modules to be public.

--- components/relay/src/client/mod.rs
+++ components/relay/src/client/mod.rs
@@ -1,10 +1,10 @@
-mod connector;
+pub mod connector;
 mod client_connector;
 mod client_tunnel;
 #[allow(unused)]
 mod client_listener;
 #[allow(unused)]
-mod access_control;
+pub mod access_control;
 
 #[cfg(test)]
 mod test_utils;
