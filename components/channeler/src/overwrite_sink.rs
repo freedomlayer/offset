50a5e6b879ad5bc3d3e92ed976b8e3cbea5a2558
real <real@freedomlayer.org>
2018-11-27 15:02:46 +0200

    Using spawned Future implementation for OverwriteSink.

--- components/channeler/src/overwrite_sink.rs
+++ components/channeler/src/overwrite_sink.rs
@@ -1,127 +1,14 @@
 use std::marker::Unpin;
 use core::pin::Pin;
-use futures::task::{Poll, LocalWaker};
-use futures::Sink;
+use futures::task::{Poll, LocalWaker, Spawn, SpawnExt};
+use futures::channel::mpsc;
+use futures::{future, Future, FutureExt, TryFutureExt, StreamExt};
 
-
-struct OverwriteSink<S,T> {
-    sink: S,
-    opt_item: Option<T>,
+#[derive(Debug)]
+enum OverwriteChannelError {
+    SendError,
 }
 
-impl<S,T,E> OverwriteSink<S,T> 
-where
-    S: Sink<SinkItem=T, SinkError=E> + Unpin,
-    T: Unpin,
-{
-    fn new(sink: S) -> OverwriteSink<S,T> {
-        OverwriteSink { sink, opt_item: None }
-    }
-}
-
-impl<S,T,E> Sink for OverwriteSink<S,T> 
-where
-    S: Sink<SinkItem=T, SinkError=E> + Unpin,
-    T: Unpin,
-{
-    type SinkItem = T;
-    type SinkError = E;
-
-    fn poll_ready(self: Pin<&mut Self>, _lw: &LocalWaker) -> Poll<Result<(), E>> {
-        Poll::Ready(Ok(()))
-    }
-
-    fn start_send(mut self: Pin<&mut Self>, item: T) -> Result<(), E> {
-        println!("start_send");
-        self.opt_item = Some(item);
-        Ok(())
-    }
-
-    fn poll_flush(mut self: Pin<&mut Self>, lw: &LocalWaker
-    ) -> Poll<Result<(), E>> {
-        println!("poll_flush");
-        let item = if let Some(item) = self.opt_item.take() {
-            item
-        } else {
-            return Poll::Ready(Ok(()));
-        };
-
-        match Pin::new(&mut self.sink).poll_ready(lw) {
-            Poll::Pending => {
-                self.opt_item = Some(item);
-                return Poll::Pending;
-            },
-            Poll::Ready(Ok(())) => Pin::new(&mut self.sink).start_send(item)?,
-            Poll::Ready(Err(e)) => return Poll::Ready(Err(e)),
-        };
-        Pin::new(&mut self.sink).poll_flush(lw)
-    }
-
-    fn poll_close(mut self: Pin<&mut Self>, lw: &LocalWaker) -> Poll<Result<(), E>> {
-        println!("poll_close");
-        match Pin::new(&mut self).poll_flush(lw) {
-            Poll::Ready(Ok(())) => {},
-            Poll::Ready(Err(e)) => return Poll::Ready(Err(e)),
-            Poll::Pending => return Poll::Pending,
-        };
-        Pin::new(&mut self.sink).poll_close(lw)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use futures::{stream, StreamExt, SinkExt};
-    use futures::executor::ThreadPool;
-    use futures::task::{Spawn, SpawnExt};
-    use futures::channel::mpsc;
-
-    async fn task_overwrite_sink_send_all() {
-        let (sender, mut receiver) = mpsc::channel::<u32>(0);
-        let mut overwrite_sender = OverwriteSink::new(sender);
-
-        let mut st = stream::iter(3u32 ..= 5);
-        await!(overwrite_sender.send_all(&mut st)).unwrap();
-        assert_eq!(await!(receiver.next()), Some(5));
-        let mut st = stream::iter(6u32 ..= 7);
-        await!(overwrite_sender.send_all(&mut st)).unwrap();
-        assert_eq!(await!(receiver.next()), Some(7));
-        drop(overwrite_sender);
-        assert_eq!(await!(receiver.next()), None);
-    }
-
-    #[test]
-    fn test_overwrite_sink_send_all() {
-        let mut thread_pool = ThreadPool::new().unwrap();
-        thread_pool.run(task_overwrite_sink_send_all());
-    }
-
-    async fn task_overwrite_sink_single_send() {
-        let (sender, mut receiver) = mpsc::channel::<u32>(0);
-        let mut overwrite_sender = OverwriteSink::new(sender);
-
-        await!(overwrite_sender.send(3)).unwrap();
-        await!(overwrite_sender.send(4)).unwrap();
-        await!(overwrite_sender.send(5)).unwrap();
-        assert_eq!(await!(receiver.next()), Some(5));
-        await!(overwrite_sender.send(6)).unwrap();
-        await!(overwrite_sender.send(7)).unwrap();
-        assert_eq!(await!(receiver.next()), Some(7));
-        drop(overwrite_sender);
-        assert_eq!(await!(receiver.next()), None);
-    }
-
-    #[test]
-    fn test_overwrite_sink_single_send() {
-        let mut thread_pool = ThreadPool::new().unwrap();
-        thread_pool.run(task_overwrite_sink_single_send());
-    }
-}
-
-
-// ===================================== 
-
-/*
 struct OverwriteChannel<T> {
     opt_item: Option<T>,
     sender: mpsc::Sender<T>,
@@ -209,16 +96,65 @@ where
         .map_err(|e| { error!("[Channeler] OverwriteChannel error: {:?}", e); })
         .then(|_| future::ready(()));
 
-    spawner.spawn(overwrite_fut);
+    spawner.spawn(overwrite_fut).unwrap();
 
     (sender, receiver)
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use futures::{stream, StreamExt, SinkExt};
+    use futures::executor::ThreadPool;
+    use futures::task::{Spawn, SpawnExt};
 
-*/
+    async fn task_overwrite_sink_send_all(spawner: impl Spawn) {
+        // let (sender, mut receiver) = mpsc::channel::<u32>(0);
+        // let mut overwrite_sender = OverwriteSink::new(sender);
+        let (mut sender, mut receiver) = overwrite_channel::<u32,_>(spawner);
 
-// TODO: 
-// - Implement this code as a Stream instead of a spawned future. 
-// We should be the receiver. Is this possible/reasonable?
-//
-// - How to test this code?
+        let mut st = stream::iter(3u32 ..= 7);
+        await!(sender.send_all(&mut st)).unwrap();
+        drop(sender);
+        let mut last_item = None;
+        while let Some(item) = await!(receiver.next()) {
+            last_item = Some(item);
+        }
+        assert_eq!(last_item, Some(7));
+    }
+
+    #[test]
+    fn test_overwrite_sink_send_all() {
+        let mut thread_pool = ThreadPool::new().unwrap();
+        thread_pool.run(task_overwrite_sink_send_all(thread_pool.clone()));
+    }
+
+    async fn task_overwrite_sink_single_send(spawner: impl Spawn) {
+        // let (sender, mut receiver) = mpsc::channel::<u32>(0);
+        // let mut overwrite_sender = OverwriteSink::new(sender);
+        let (mut sender, mut receiver) = overwrite_channel::<u32,_>(spawner);
+
+        await!(sender.send(3)).unwrap();
+        await!(sender.send(4)).unwrap();
+        await!(sender.send(5)).unwrap();
+        await!(sender.send(6)).unwrap();
+        await!(sender.send(7)).unwrap();
+        drop(sender);
+        let mut last_item = None;
+        while let Some(item) = await!(receiver.next()) {
+            println!("item = {:?}", item);
+            last_item = Some(item);
+        }
+        assert_eq!(last_item, Some(7));
+
+    }
+
+    #[test]
+    fn test_overwrite_sink_single_send() {
+        let mut thread_pool = ThreadPool::new().unwrap();
+        thread_pool.run(task_overwrite_sink_single_send(thread_pool.clone()));
+    }
+}
+
+// TODO: Better tests for this code?
+
