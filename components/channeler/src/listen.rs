fddd59332d5de0828c09687e5ee8b32baea384ee
real <real@freedomlayer.org>
2018-12-05 16:11:28 +0200

    Implemented Listener for Channeler's listen, relying on ClientListener.

--- components/channeler/src/listen.rs
+++ components/channeler/src/listen.rs
@@ -1,7 +1,7 @@
 use std::marker::Unpin;
 use futures::{select, FutureExt, TryFutureExt, Stream, StreamExt, Sink, SinkExt};
 use futures::task::{Spawn, SpawnExt};
-use futures::channel::{mpsc, oneshot};
+use futures::channel::mpsc;
 
 use proto::consts::TICKS_TO_REKEY;
 
@@ -12,50 +12,16 @@ use timer::utils::sleep_ticks;
 
 use identity::IdentityClient;
 
-use common::connector::{Connector, ConnPair};
 use common::listener::Listener;
-use relay::client::client_listener::{client_listener, ClientListenerError};
+use common::connector::ConnPair;
 use relay::client::access_control::{AccessControlOp, AccessControl};
 
 use secure_channel::create_secure_channel;
 
-use crate::connector_utils::ConstAddressConnector;
-
 #[derive(Debug)]
 pub enum ListenError {
-    RequestTimerStreamError,
     SleepTicksError,
-    TimerClosed,
-    AccessControlError,
     SpawnError,
-    IncomingAddressClosed,
-    Canceled,
-}
-
-enum ListenSelect {
-    ListenError(ListenError),
-    Canceled,
-}
-
-/// Distinguish between fatal and non-fatal errors
-fn convert_client_listener_result(client_listener_result: Result<(), ClientListenerError>) -> Result<(), ListenError> {
-    match client_listener_result {
-        Ok(()) => unreachable!(),
-        Err(ClientListenerError::RequestTimerStreamError) => 
-            Err(ListenError::RequestTimerStreamError),
-        Err(ClientListenerError::TimerClosed) => 
-            Err(ListenError::TimerClosed),
-        Err(ClientListenerError::AccessControlError) =>
-            Err(ListenError::AccessControlError),
-        Err(ClientListenerError::SpawnError) =>
-            Err(ListenError::SpawnError),
-        Err(ClientListenerError::AccessControlClosed) =>
-            Err(ListenError::Canceled),
-        Err(ClientListenerError::SendInitConnectionError) |
-        Err(ClientListenerError::ConnectionFailure) |
-        Err(ClientListenerError::SendToServerError) |
-        Err(ClientListenerError::ServerClosed) => Ok(()),
-    }
 }
 
 /// Encrypt incoming plain connections
@@ -99,85 +65,9 @@ where
 }
 
 
-/// Connect to relay and keep listening for incoming connections.
-/// Aborts in one of the following cases:
-/// - Canceled through close_receiver.
-/// - A fatal error occured.
-pub async fn listen_loop<A,C,IAC,CS,R,S>(connector: C,
-                 address: A,
-                 mut incoming_access_control: IAC,
-                 connections_sender: CS,
-                 close_receiver: oneshot::Receiver<()>,
-                 mut access_control: AccessControl,
-                 conn_timeout_ticks: usize,
-                 keepalive_ticks: usize,
-                 backoff_ticks: usize,
-                 timer_client: TimerClient,
-                 identity_client: IdentityClient,
-                 rng: R,
-                 mut spawner: S) -> Result<(), ListenError> 
-where
-    A: Clone + Send + Sync + 'static,
-    C: Connector<Address=A, SendItem=Vec<u8>, RecvItem=Vec<u8>> + Sync + Send + Clone + 'static, 
-    IAC: Stream<Item=AccessControlOp> + Unpin + 'static,
-    CS: Sink<SinkItem=(PublicKey, ConnPair<Vec<u8>, Vec<u8>>)> + Unpin + Clone + Send + 'static,
-    R: CryptoRandom + 'static,
-    S: Spawn + Clone + Send + 'static,
-{
-    let (plain_connections_sender, plain_connections_receiver) = mpsc::channel(0);
-    spawner.spawn(conn_encryptor(plain_connections_receiver, 
-                                 connections_sender, // Sends encrypted connections
-                                 timer_client.clone(),
-                                 identity_client,
-                                 rng,
-                                 spawner.clone()))
-        .map_err(|_| ListenError::SpawnError)?;
-
-    // TODO: get rid of Box::pinned() later ?
-    let mut listener_fut = Box::pinned(async {
-        loop {
-            let const_address_connector = ConstAddressConnector::new(connector.clone(), address.clone());
-            let res = await!(client_listener(const_address_connector,
-                                        &mut access_control,
-                                        &mut incoming_access_control,
-                                        plain_connections_sender.clone(),
-                                        conn_timeout_ticks,
-                                        keepalive_ticks,
-                                        timer_client.clone(),
-                                        spawner.clone()));
-
-            // Exit the loop if the error is fatal:
-            // TODO: Should we unwrap here? We need to make sure that everything stops if a fatal error
-            // occurs here.
-            convert_client_listener_result(res)?;
-
-            // Wait for a while before attempting to connect again:
-            // TODO: Possibly wait here in a smart way? Exponential backoff?
-            await!(sleep_ticks(backoff_ticks, timer_client.clone()))
-                .map_err(|_| ListenError::SleepTicksError)?;
-        }
-        // This is a hack to help the compiler know that the return type
-        // here is Result<(),_>
-        #[allow(unreachable_code)]
-        Ok(())
-    }).fuse();
-
-    let mut close_fut = close_receiver.fuse();
-
-    let listener_select = select! {
-        listener_fut = listener_fut => ListenSelect::ListenError(listener_fut.err().unwrap()),
-        _close_fut = close_fut => ListenSelect::Canceled,
-    };
-
-    match listener_select {
-        ListenSelect::ListenError(listener_error) => Err(listener_error),
-        ListenSelect::Canceled => Err(ListenError::Canceled),
-    }
-}
-
 #[derive(Clone)]
-pub struct ChannelerListener<A,C,R,S> {
-    connector: C,
+pub struct ChannelerListener<A,L,R,S> {
+    client_listener: L,
     address: A,
     conn_timeout_ticks: usize,
     keepalive_ticks: usize,
@@ -188,14 +78,16 @@ pub struct ChannelerListener<A,C,R,S> {
     spawner: S,
 }
 
-impl<A,C,R,S> ChannelerListener<A,C,R,S>
+impl<A,L,R,S> ChannelerListener<A,L,R,S>
 where
     A: Clone + Send + Sync + 'static,
-    C: Connector<Address=A, SendItem=Vec<u8>, RecvItem=Vec<u8>> + Sync + Send + Clone + 'static, 
+    L: Listener<Connection=(PublicKey, ConnPair<Vec<u8>,Vec<u8>>), 
+        Config=AccessControlOp, Arg=(A, AccessControl)> + Clone + 'static,
     R: CryptoRandom + 'static,
     S: Spawn + Clone + Send + 'static,
 {
-    fn new() -> ChannelerListener<A,C,R,S> {
+
+    fn new() -> ChannelerListener<A,L,R,S> {
         unimplemented!();
     }
 
@@ -203,52 +95,53 @@ where
                    mut access_control_receiver: mpsc::Receiver<AccessControlOp>,
                    connections_sender: mpsc::Sender<(PublicKey, ConnPair<Vec<u8>,Vec<u8>>)>,
                    mut access_control: AccessControl)
-                    -> Result<(), ListenError> {
+                    -> Result<!, ListenError> {
 
-        let (plain_connections_sender, plain_connections_receiver) = mpsc::channel(0);
-        self.spawner.spawn(conn_encryptor(plain_connections_receiver, 
-                                     connections_sender, // Sends encrypted connections
-                                     self.timer_client.clone(),
-                                     self.identity_client.clone(),
-                                     self.rng.clone(),
-                                     self.spawner.clone()))
-            .map_err(|_| ListenError::SpawnError)?;
-
-        // TODO: get rid of Box::pinned() later ?
         loop {
-            let const_address_connector = ConstAddressConnector::new(self.connector.clone(), 
-                                                                     relay_address.clone());
-            let res = await!(client_listener(const_address_connector,
-                                        &mut access_control,
-                                        &mut access_control_receiver,
-                                        plain_connections_sender.clone(),
-                                        self.conn_timeout_ticks,
-                                        self.keepalive_ticks,
-                                        self.timer_client.clone(),
-                                        self.spawner.clone()));
+            let (mut plain_connections_sender, plain_connections_receiver) = mpsc::channel(0);
+            self.spawner.spawn(conn_encryptor(plain_connections_receiver, 
+                                         connections_sender.clone(), // Sends encrypted connections
+                                         self.timer_client.clone(),
+                                         self.identity_client.clone(),
+                                         self.rng.clone(),
+                                         self.spawner.clone()))
+                .map_err(|_| ListenError::SpawnError)?;
 
-            // Exit the loop if the error is fatal:
-            // TODO: Should we unwrap here? We need to make sure that everything stops if a fatal error
-            // occurs here.
-            convert_client_listener_result(res)?;
+            let (mut access_control_sender, mut connections_receiver) = 
+                self.client_listener.clone().listen((relay_address.clone(), access_control.clone()));
+
+            let fut_access_control_send_all = self.spawner.spawn_with_handle(async move {
+                while let Some(access_control_op) = await!(access_control_receiver.next()) {
+                    access_control.apply_op(access_control_op.clone());
+                    if let Err(_) = await!(access_control_sender.send(access_control_op)) {
+                        break;
+                    }
+                }
+                (access_control, access_control_receiver)
+            }).unwrap();
+
+            let fut_connections_send_all = 
+                plain_connections_sender.send_all(&mut connections_receiver);
+
+            let ((res_access_control, res_access_control_receiver), _ ) = 
+                await!(fut_access_control_send_all.join(fut_connections_send_all));
+
+            access_control = res_access_control;
+            access_control_receiver = res_access_control_receiver;
 
             // Wait for a while before attempting to connect again:
             // TODO: Possibly wait here in a smart way? Exponential backoff?
             await!(sleep_ticks(self.backoff_ticks, self.timer_client.clone()))
                 .map_err(|_| ListenError::SleepTicksError)?;
         }
-        // This is a hack to help the compiler know that the return type
-        // here is Result<(),_>
-        #[allow(unreachable_code)]
-        Ok(())
-
     }
 }
 
-impl<A,C,R,S> Listener for ChannelerListener<A,C,R,S> 
+impl<A,L,R,S> Listener for ChannelerListener<A,L,R,S> 
 where
     A: Clone + Send + Sync + 'static,
-    C: Connector<Address=A, SendItem=Vec<u8>, RecvItem=Vec<u8>> + Sync + Send + Clone + 'static, 
+    L: Listener<Connection=(PublicKey, ConnPair<Vec<u8>,Vec<u8>>), 
+        Config=AccessControlOp, Arg=(A, AccessControl)> + Clone + Send + 'static,
     R: CryptoRandom + 'static,
     S: Spawn + Clone + Send + 'static,
 {
