c53252e9474501eb70b43d4ad4dd16b967537120
real <real@freedomlayer.org>
2018-12-06 12:24:21 +0200

    Added a connection retry test for Channeler's connect.rs.

--- components/channeler/src/connect.rs
+++ components/channeler/src/connect.rs
@@ -100,6 +100,7 @@ mod tests {
     use common::conn::IdentityConnTransform;
 
 
+    /// Check basic connection using the ChannelerConnector.
     async fn task_channeler_connector_basic<S>(mut spawner: S) 
     where
         S: Spawn + Clone + Sync + Send,
@@ -147,5 +148,65 @@ mod tests {
     fn test_channeler_connector_basic() {
         let mut thread_pool = ThreadPool::new().unwrap();
         thread_pool.run(task_channeler_connector_basic(thread_pool.clone()));
+    }
+
+
+    /// Check connection retry in case of connection failure
+    async fn task_channeler_connector_retry<S>(mut spawner: S) 
+    where
+        S: Spawn + Clone + Sync + Send,
+    {
+
+        // Create a mock time service:
+        let (mut tick_sender, tick_receiver) = mpsc::channel::<()>(0);
+        let timer_client = create_timer_incoming(tick_receiver, spawner.clone()).unwrap();
+
+        let backoff_ticks = 2;
+
+        let (conn_request_sender, mut conn_request_receiver) = mpsc::channel::<ConnRequest<Vec<u8>,Vec<u8>,(u32, PublicKey)>>(0);
+        let client_connector = DummyConnector::new(conn_request_sender);
+
+        // We don't need encryption for this test:
+        let encrypt_transform = IdentityConnTransform::<Vec<u8>,Vec<u8>,Option<PublicKey>>::new();
+
+        let mut channeler_connector = ChannelerConnector::new(
+            client_connector,
+            encrypt_transform,
+            backoff_ticks,
+            timer_client,
+            spawner);
+
+        let public_key_b = PublicKey::from(&[0xaa; PUBLIC_KEY_LEN]);
+        let connect_fut = async {
+            let (mut sender, mut receiver) = await!(channeler_connector.connect((0x1337, public_key_b))).unwrap();
+            await!(sender.send(vec![1,2,3])).unwrap();
+            assert_eq!(await!(receiver.next()).unwrap(), vec![3,2,1]);
+        };
+
+        let server_fut = async {
+            // Fail in the first time:
+            let request = await!(conn_request_receiver.next()).unwrap();
+            request.reply(None);
+            // Wait enough time to let the client try again:
+            for _ in 0 .. backoff_ticks + 3 { // TODO: Fix this. Not exact!
+                await!(tick_sender.send(())).unwrap();
+            }
+
+            // This time return a connection:
+            let request = await!(conn_request_receiver.next()).unwrap();
+            let (mut local_sender, remote_receiver) = mpsc::channel(0);
+            let (remote_sender, mut local_receiver) = mpsc::channel(0);
+            request.reply(Some((remote_sender, remote_receiver)));
+            assert_eq!(await!(local_receiver.next()).unwrap(), vec![1,2,3]);
+            await!(local_sender.send(vec![3,2,1])).unwrap();
+        };
+
+        let _ = await!(connect_fut.join(server_fut));
+    }
+
+    #[test]
+    fn test_channeler_connector_retry() {
+        let mut thread_pool = ThreadPool::new().unwrap();
+        thread_pool.run(task_channeler_connector_retry(thread_pool.clone()));
     }
 }
