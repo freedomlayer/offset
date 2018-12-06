263a633e2620447ce1313fa1effe78d5dab04dd8
real <real@freedomlayer.org>
2018-12-06 18:52:02 +0200

    Typo in comment.

--- components/channeler/src/listen.rs
+++ components/channeler/src/listen.rs
@@ -120,7 +120,7 @@ where
                     // We select over .next() invocations instead of selecting over streams because
                     // we can't afford to lose access_control_receiver. access_control_receiver lives for the entire outer loop,
                     // and as a receiver it can not be cloned.
-                    // TODO: Find out if we can accidentally lose a access_control_op here.
+                    // TODO: Find out if we can accidentally lose an access_control_op here.
                     let event = select! {
                         opt_access_control_op = access_control_receiver.next().fuse() 
                             => ListenLoopEvent::AccessControlOp(opt_access_control_op),
