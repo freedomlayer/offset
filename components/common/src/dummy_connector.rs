931615ea0b4d694e1df4c82013c0ec1e4db966ec
real <real@freedomlayer.org>
2018-12-08 14:51:13 +0200

    DummyConnector is now more generic (Maybe take any input/output).

--- components/common/src/dummy_connector.rs
+++ components/common/src/dummy_connector.rs
@@ -1,41 +1,50 @@
 use futures::channel::{mpsc, oneshot};
 use futures::SinkExt;
-use crate::conn::{FutTransform, ConnPair, BoxFuture};
+use crate::conn::{FutTransform, BoxFuture};
 
 
-pub struct ConnRequest<SI,RI,A> {
+pub struct ConnRequest<A,O> {
     pub address: A,
-    response_sender: oneshot::Sender<Option<ConnPair<SI,RI>>>,
+    response_sender: oneshot::Sender<O>,
 }
 
-impl<SI,RI,A> ConnRequest<SI,RI,A> {
-    pub fn reply(self, opt_conn_pair: Option<ConnPair<SI,RI>>) {
-        self.response_sender.send(opt_conn_pair).ok().unwrap();
+impl<A,O> ConnRequest<A,O> {
+    pub fn reply(self, response: O) {
+        self.response_sender.send(response).ok().unwrap();
     }
 }
 
 /// A connector that contains only one pre-created connection.
-#[derive(Clone)]
-pub struct DummyConnector<SI,RI,A> {
-    req_sender: mpsc::Sender<ConnRequest<SI,RI,A>>,
+pub struct DummyConnector<A,O> {
+    req_sender: mpsc::Sender<ConnRequest<A,O>>,
 }
 
-impl<SI,RI,A> DummyConnector<SI,RI,A> {
-    pub fn new(req_sender: mpsc::Sender<ConnRequest<SI,RI,A>>) -> Self {
+impl<A,O> DummyConnector<A,O> {
+    pub fn new(req_sender: mpsc::Sender<ConnRequest<A,O>>) -> Self {
         DummyConnector { 
             req_sender,
         }
     }
 }
 
-impl<SI,RI,A> FutTransform for DummyConnector<SI,RI,A> 
+// TODO: Why didn't the automatic #[derive(Clone)] works for DummyListener?
+// Seemed like it had a problem with having config_receiver inside ListenRequest.
+// This is a workaround for this issue:
+impl<A,O> Clone for DummyConnector<A,O> {
+    fn clone(&self) -> DummyConnector<A,O> {
+        DummyConnector {
+            req_sender: self.req_sender.clone(),
+        }
+    }
+}
+
+impl<A,O> FutTransform for DummyConnector<A,O> 
 where
-    SI: Send,
-    RI: Send,
+    O: Send,
     A: Send + Sync,
 {
     type Input = A;
-    type Output = Option<ConnPair<SI,RI>>;
+    type Output = O;
 
     fn transform<'a>(&'a mut self, address: A) -> BoxFuture<'_, Self::Output> {
         let (response_sender, response_receiver) = oneshot::channel();
@@ -46,7 +55,7 @@ where
 
         let fut_conn_pair = async move {
             await!(self.req_sender.send(conn_request)).unwrap();
-            await!(response_receiver).ok()?
+            await!(response_receiver).unwrap()
         };
         Box::pinned(fut_conn_pair)
     }
