8030e25b1ccdc82412ff5402a2d24b4191ea0890
real <real@freedomlayer.org>
2018-12-14 19:12:33 +0200

    Using OptionIterator instead of returning Option<impl Iterator>. Added opt_exclude to get_route().

--- components/index_server/src/graph.rs
+++ components/index_server/src/graph.rs
@@ -21,10 +21,43 @@ struct CapacityGraph<N> {
     nodes: HashMap<N,HashMap<N,CapacityEdge>>,
 }
 
+/// An iterator that wraps an optional iterator.
+/// If the optional iterator exists, OptionIterator will behave exactly like the underlying
+/// iterator. Otherwise, it will immediately return None.
+///
+/// This iterator is useful for cases where we want a function to be able to either return an
+/// Iterator, or an empty Iterator, and those two iterators must be of the same type. Useful for
+/// functions that return `impl Iterator<...>`.
+struct OptionIterator<I> {
+    opt_iterator: Option<I>,
+}
+
+impl<I,T> Iterator for OptionIterator<I> 
+where
+    I: Iterator<Item=T>,
+{
+    type Item = T;
+    fn next(&mut self) -> Option<T> {
+        match &mut self.opt_iterator {
+            Some(iterator) => iterator.next(),
+            None => None,
+        }
+    }
+}
+
+impl<I> OptionIterator<I> {
+    fn new(opt_iterator: Option<I>) -> OptionIterator<I> {
+        OptionIterator {
+            opt_iterator,
+        }
+    }
+}
+
+
 
 impl<N> CapacityGraph<N> 
 where
-    N: cmp::Eq + hash::Hash + Clone,
+    N: cmp::Eq + hash::Hash + Clone + std::fmt::Debug,
 {
 
     pub fn new() -> CapacityGraph<N> {
@@ -99,10 +132,13 @@ where
         cmp::min(a_send, b_recv)
     }
 
-    fn neighbors_with_send_capacity(&self, a: N, capacity: u128) -> Option<impl Iterator<Item=&N>> {
-        let a_map = self.nodes.get(&a)?;
+    fn neighbors_with_send_capacity(&self, a: N, capacity: u128) -> OptionIterator<impl Iterator<Item=&N>> {
+        let a_map = match self.nodes.get(&a) {
+            Some(a_map) => a_map,
+            None => return OptionIterator::new(None),
+        };
         let iter = a_map.keys().filter(move |b| self.get_send_capacity(&a,b) >= capacity);
-        Some(iter)
+        OptionIterator::new(Some(iter))
     }
 
     /// Calculate the amount of capacity we can send through a route.
@@ -115,52 +151,24 @@ where
 
     /// Get a route with capacity at least `capacity`. 
     /// Returns the route together with the capacity it is possible to send through the route.
-    pub fn get_route(&self, a: &N, b: &N, capacity: u128) -> Option<(Vec<N>, u128)> {
-        let get_neighbors = |b: &N| self.neighbors_with_send_capacity(b.clone(), capacity).unwrap();
+    ///
+    /// opt_exclude is an optional edge to exclude (The returned route must not go through this
+    /// edge). This can be useful for finding non trivial loops.
+    pub fn get_route(&self, a: &N, b: &N, capacity: u128, opt_exclude: Option<(&N, &N)>) -> Option<(Vec<N>, u128)> {
+        let (opt_e_start, opt_e_end) = match opt_exclude {
+            Some((e_start, e_end)) => (Some(e_start), Some(e_end)),
+            None => (None, None),
+        };
+        let get_neighbors = |cur_node: &N| {
+            let cur_node_is_e_start = Some(cur_node) == opt_e_start;
+            println!("b = {:?}", b);
+            self.neighbors_with_send_capacity(b.clone(), capacity)
+                .filter(move |&next_node| !cur_node_is_e_start || Some(next_node) != opt_e_end)
+        };
         let route = bfs(a, b, get_neighbors)?;
         // We assert that we will always have valid capacity here:
         let capacity = self.get_route_capacity(&route).unwrap();
 
-        Some((route, capacity))
-    }
-
-    /// A loop from myself through given neighbor, back to myself.
-    /// a -> neighbor -> ... -> ... -> a
-    pub fn get_loop_from(&self, a: &N, neighbor: &N, capacity: u128) -> Option<(Vec<N>, u128)> {
-        let get_neighbors = move |cur_node: &N| {
-            // The following directed edge is not allowed: (neighbor -> a)
-            let cur_node_is_neighbor = cur_node == neighbor;
-            self.neighbors_with_send_capacity(cur_node.clone(), capacity)
-                .unwrap()
-                .filter(move |&next_node| !cur_node_is_neighbor || (next_node != a))
-        };
-
-        let route = bfs(a, a, get_neighbors)?;
-        // We expect that bfs does not return the trivial route (a)
-        assert!(route.len() >= 2);
-
-        // We assert that we will always have valid capacity here:
-        let capacity = self.get_route_capacity(&route).unwrap();
-        Some((route, capacity))
-    }
-
-    /// A loop from myself back to myself through given neighbor.
-    /// a -> ... -> ... -> neighbor -> a
-    pub fn get_loop_to(&self, a: &N, neighbor: &N, capacity: u128) -> Option<(Vec<N>, u128)> {
-        let get_neighbors = move |cur_node: &N| {
-            // The following directed edge is not allowed: (a -> neighbor)
-            let cur_node_is_a = cur_node == a;
-            self.neighbors_with_send_capacity(cur_node.clone(), capacity)
-                .unwrap()
-                .filter(move |&next_node| !cur_node_is_a || (next_node != neighbor))
-        };
-
-        let route = bfs(a, a, get_neighbors)?;
-        // We expect that bfs does not return the trivial route (a)
-        assert!(route.len() >= 2);
-
-        // We assert that we will always have valid capacity here:
-        let capacity = self.get_route_capacity(&route).unwrap();
         Some((route, capacity))
     }
 }
@@ -204,8 +212,7 @@ mod tests {
         assert_eq!(cg.nodes.len(), 1);
     }
 
-    #[test]
-    fn test_get_route() {
+    fn example_capacity_graph() -> CapacityGraph<u32> {
         /*
          * Example graph:
          *
@@ -236,7 +243,18 @@ mod tests {
         cg.update_edge(4, 2, (30, 18));
         cg.update_edge(2, 4, (18, 30));
 
-        assert_eq!(cg.get_route(&0, &5, 25), Some((vec![0,1,3,4,2,5], 30)));
+        cg
+    }
+
+    #[test]
+    fn test_get_route() {
+        let mut cg = example_capacity_graph();
+
+        assert_eq!(cg.get_route(&2, &5, 29, None), Some((vec![2,5], 30)));
+        // assert_eq!(cg.get_route(&2, &5, 30, None), Some((vec![2,5], 30)));
+        // assert_eq!(cg.get_route(&2, &5, 31, None), None);
+
+        // assert_eq!(cg.get_route(&0, &5, 25), Some((vec![0,1,3,4,2,5], 30)));
     }
 }
 
