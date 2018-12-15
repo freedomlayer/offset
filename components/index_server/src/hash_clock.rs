47acb4d27f820e5fa327774d4fe2cd69a18f0792
real <real@freedomlayer.org>
2018-12-15 19:25:05 +0200

    Refactoring + added a basic HashClock test.

--- components/index_server/src/hash_clock.rs
+++ components/index_server/src/hash_clock.rs
@@ -6,21 +6,13 @@ use std::collections::{HashMap, VecDeque};
 /// Prefix put before hashing a list of hashes:
 const HASH_CLOCK_PREFIX: &[u8] = b"HASH_CLOCK";
 
-
-/// Information about how a hash value was created.
-/// A vector of all hashes (in the correct order), together with an optional corresponding neighbor id.
-///
-/// Some hashes might not have a corresponding neighbor id. For example: `hash(rand_value)`
-/// produced locally.
-type HashInfo<N> = Vec<(Option<N>, HashResult)>;
-
 struct HashClock<N> {
     /// Last hash we received from each neighbor
     neighbor_hashes: HashMap<N, HashResult>,
     /// Maximum length of last_ticks:
     last_ticks_max_len: usize,
     last_ticks: VecDeque<HashResult>,
-    last_ticks_map: HashMap<HashResult, HashInfo<N>>,
+    last_ticks_map: HashMap<HashResult, Vec<HashResult>>,
 }
 
 /// Combine a list of hashes into one hash result:
@@ -54,7 +46,7 @@ where
     }
 
     /// Insert a new pair of (hash, hash_info)
-    fn insert_tick_hash(&mut self, tick_hash: HashResult, hash_info: HashInfo<N>) {
+    fn insert_tick_hash(&mut self, tick_hash: HashResult, expansion: Vec<HashResult>) {
         assert!(self.last_ticks_max_len > 0);
         self.last_ticks.push_back(tick_hash.clone());
 
@@ -64,7 +56,7 @@ where
         }
 
         assert!(self.last_ticks.len() <= self.last_ticks_max_len);
-        self.last_ticks_map.insert(tick_hash.clone(), hash_info);
+        self.last_ticks_map.insert(tick_hash.clone(), expansion);
     }
 
     /// Should be called when a new hash is received from a neighbor.
@@ -80,46 +72,32 @@ where
     /// Advance the time in the clock by one tick.
     /// The resulting tick_hash should be sent to all the neighbors.
     pub fn tick(&mut self, rand_value: RandValue) -> HashResult {
-        let mut hash_info = Vec::new();
-
-        let mut hashes = Vec::new();
+        let mut expansion = Vec::new();
 
         let hashed_rand_value = sha_512_256(&rand_value);
-        hashes.push(hashed_rand_value.clone());
-        hash_info.push((None, hashed_rand_value));
+        expansion.push(hashed_rand_value);
 
         // Concatenate all hashes, and update hash_info accordingly:
-        for (neighbor, hash) in &self.neighbor_hashes {
-            hashes.push(hash.clone());
-            hash_info.push((Some(neighbor.clone()), hash.clone()));
+        for (_, hash) in &self.neighbor_hashes {
+            expansion.push(hash.clone());
         }
 
-        let tick_hash = hash_hashes(&hashes);
-        self.insert_tick_hash(tick_hash.clone(), hash_info);
+        let tick_hash = hash_hashes(&expansion);
+        self.insert_tick_hash(tick_hash.clone(), expansion);
 
         tick_hash
     }
 
     /// Given a tick hash (that was created in this HashClock), create a hash proof for a neighbor.
-    pub fn create_hash_proof(&mut self, tick_hash: HashResult) -> Option<Vec<HashResult>> {
-        // Make sure that we have the given tick_hash:
-        let hash_info = self.last_ticks_map.get(&tick_hash)?;
-
-        // Prepare a full list of hashes:
-        let hashes = hash_info
-            .iter()
-            .map(|(_opt_neighbor, hash_result)| hash_result)
-            .cloned()
-            .collect::<Vec<HashResult>>();
-
-        Some(hashes)
+    pub fn get_expansion(&self, tick_hash: &HashResult) -> Option<&Vec<HashResult>> {
+        self.last_ticks_map.get(tick_hash)
     }
 
     /// Verify a chain of hash proof links.
     /// Each link shows that a certain hash is composed from a list of hashes. 
     /// Eventually one of those hashes is a tick_hash created at this HashClock. 
     /// This proves that the `origin_tick_hash` is recent.
-    pub fn verify_hash_proof_chain(&self, origin_tick_hash: &HashResult, hash_proof_chain: &[Vec<HashResult>]) 
+    pub fn verify_expansion_chain(&self, origin_tick_hash: &HashResult, expansion_chain: &[Vec<HashResult>]) 
         -> Option<HashResult> {
 
         /*                       +-/hash0    +-/hash0    +-/hash0
@@ -131,32 +109,88 @@ where
 
         // Add origin_tick_hash as a list of 1 hashes in the beginning of hash_proof_chain.
         // This allows dealing with some edge cases more smoothly.
-        let mut proof_chain: Vec<&[HashResult]> = Vec::new();
+        let mut ex_expansion_chain: Vec<&[HashResult]> = Vec::new();
         let first_hashes = &[origin_tick_hash.clone()];
-        proof_chain.push(&first_hashes[..]);
+        ex_expansion_chain.push(&first_hashes[..]);
 
-        for hashes in hash_proof_chain {
-            proof_chain.push(hashes);
+        for hashes in expansion_chain {
+            ex_expansion_chain.push(hashes);
         }
 
         // Calculate hashes of all lists of hashes
-        let hash_res = hash_proof_chain
+        let hash_res = expansion_chain
             .iter()
             .map(|hashes| hash_hashes(hashes))
             .collect::<Vec<_>>();
 
         for i in 0 .. hash_res.len() {
-            if !proof_chain[i].contains(&hash_res[i]) {
+            if !ex_expansion_chain[i].contains(&hash_res[i]) {
                 return None;
             }
         }
 
-        for hash in proof_chain.last().unwrap().iter() {
+        for hash in ex_expansion_chain.last().unwrap().iter() {
             if self.last_ticks_map.contains_key(hash) {
                 return Some(hash.clone());
             }
         }
         None
+    }
+
+    pub fn get_neighbor_hash(&self, neighbor: &N) -> Option<&HashResult> {
+        self.neighbor_hashes.get(neighbor)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crypto::crypto_rand::RAND_VALUE_LEN;
+
+    #[test]
+    fn test_hash_clock_basic() {
+
+        let last_ticks_max_len = 4;
+
+        let mut hash_clocks = Vec::new();
+        for _ in 0 .. 4 {
+            hash_clocks.push(HashClock::new(last_ticks_max_len));
+        }
+
+        // Some iterations of communication between the participants:
+        for iter in 0 .. 8 {
+            for j in 0 .. hash_clocks.len() {
+                let rand_value = RandValue::from(&[iter as u8; RAND_VALUE_LEN]);
+                let tick_hash = hash_clocks[j].tick(rand_value);
+                for k in 0 .. hash_clocks.len() {
+                    if k == j {
+                        continue;
+                    }
+                    hash_clocks[k].update_neighbor_hash(j, tick_hash.clone());
+                }
+            }
+        }
+
+        let origin_tick_hash = hash_clocks[0].get_neighbor_hash(&1).unwrap().clone();
+        let tick_hash1 = hash_clocks[1].verify_expansion_chain(&origin_tick_hash, &[]).unwrap();
+
+        let expansion1 = hash_clocks[1].get_expansion(&tick_hash1).unwrap().clone();
+        let tick_hash2 = hash_clocks[2].verify_expansion_chain(&origin_tick_hash, &[expansion1.clone()]).unwrap();
+
+        let expansion2 = hash_clocks[2].get_expansion(&tick_hash2).unwrap().clone();
+        let tick_hash3 = hash_clocks[3].verify_expansion_chain(&origin_tick_hash, &[expansion1.clone(), expansion2.clone()]).unwrap();
+
+        let expansion3 = hash_clocks[3].get_expansion(&tick_hash3).unwrap().clone();
+        let tick_hash3 = hash_clocks[0].verify_expansion_chain(&origin_tick_hash, &[expansion1.clone(), expansion2.clone(), expansion3.clone()]).unwrap();
+
+
+        // Everything is forgotten after last_ticks_max_len ticks:
+        for iter in 0 .. last_ticks_max_len {
+            let rand_value = RandValue::from(&[iter as u8; RAND_VALUE_LEN]);
+            hash_clocks[0].tick(rand_value);
+        }
+
+        assert!(hash_clocks[0].verify_expansion_chain(&origin_tick_hash, &[expansion1, expansion2, expansion3]).is_none());
     }
 }
 
