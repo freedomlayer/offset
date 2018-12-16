b62f14208dc8aa7049f2c44e8d979e9a024c0242
real <real@freedomlayer.org>
2018-12-16 12:37:39 +0200

    Initial implementation for RatchetPool.

--- components/index_server/src/verifier.rs
+++ components/index_server/src/verifier.rs
@@ -5,37 +5,98 @@ use crypto::hash::HashResult;
 use proto::index::messages::{ForwardMutationsUpdate, TimeProofLink};
 use crate::hash_clock::HashClock;
 
-struct Verifier {
-    hash_clock: HashClock<PublicKey>,
-    node_states: HashMap<PublicKey, NodeState>, 
-    node_time_to_live: usize,
-}
 
-struct NodeState {
+struct Ratchet<U> {
     /// A randomly generated sessionId. The counter is related to this session Id.
-    pub session_id: Uid,
+    pub session_id: U,
     /// Incrementing counter, making sure that mutations are received in the correct order.
     /// For a new session, the counter should begin from 0 and increment by 1 for every MutationsUpdate message.
     /// When a new connection is established, a new sesionId should be randomly generated.
     pub counter: u64,
     /// This number decreases as time progresses.
     /// If it reaches 0, NodeState is removed.
-    pub time_to_live: usize,
+    pub ticks_to_live: usize,
 }
 
-impl NodeState {
-    fn new(session_id: Uid,
+impl<U> Ratchet<U> 
+where
+    U: std::cmp::Eq + Clone,
+{
+    fn new(session_id: U,
            counter: u64,
-           time_to_live: usize) -> Self {
+           ticks_to_live: usize) -> Self {
 
-        NodeState {
+        Ratchet {
             session_id,
             counter,
-            time_to_live,
+            ticks_to_live,
         }
+    }
+
+    fn update(&mut self, session_id: &U, counter: u64) -> bool {
+        if &self.session_id != session_id {
+            self.session_id = session_id.clone();
+            self.counter = counter;
+            return true;
+        } 
+        // self.session_id == session_id:
+        if self.counter < counter {
+            self.counter = counter;
+            return true;
+        }
+        false
     }
 }
 
+struct RatchetPool<N,U> {
+    ratchets: HashMap<N, Ratchet<U>>,
+    ratchet_ticks_to_live: usize,
+}
+
+impl<N,U> RatchetPool<N,U> 
+where
+    N: std::cmp::Eq + std::hash::Hash + Clone,
+    U: std::cmp::Eq + Clone,
+{
+    pub fn new(ratchet_ticks_to_live: usize) -> Self {
+        RatchetPool {
+            ratchets: HashMap::new(),
+            ratchet_ticks_to_live,
+        }
+    }
+
+    pub fn tick(&mut self) {
+        self.ratchets.retain(|node, ratchet| {
+            ratchet.ticks_to_live = ratchet.ticks_to_live.saturating_sub(1);
+            ratchet.ticks_to_live > 0
+        });
+    }
+
+    /// Try to update a certain ratchet.
+    /// Returns true if ratchet moved forward (Or created)
+    pub fn update(&mut self, node: &N, session_id: &U, counter: u64) -> bool {
+        let ratchet = match self.ratchets.get_mut(node) {
+            None => {
+                let ratchet = Ratchet::new(session_id.clone(), 
+                                                counter,
+                                                self.ratchet_ticks_to_live);
+                self.ratchets.insert(node.clone(), ratchet);
+                return true;
+            },
+            Some(ratchet) => ratchet,
+        };
+        ratchet.update(session_id, counter)
+    }
+}
+
+
+/*
+
+struct Verifier {
+    hash_clock: HashClock<PublicKey>,
+    node_states: HashMap<PublicKey, NodeState>, 
+    node_time_to_live: usize,
+}
 
 impl Verifier {
     pub fn new(last_ticks_max_len: usize,
@@ -107,4 +168,5 @@ impl Verifier {
         unimplemented!();
     }
 }
+*/
 
