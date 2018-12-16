bbfe69e3d391c3ee4d85d9ce9159dbb1eed04cb4
real <real@freedomlayer.org>
2018-12-16 16:46:21 +0200

    Removed RandValue from Verifier's interface. Using internal rng instead.

--- components/index_server/src/verifier/simple_verifier.rs
+++ components/index_server/src/verifier/simple_verifier.rs
@@ -1,23 +1,25 @@
 use crypto::hash::HashResult;
-use crypto::crypto_rand::RandValue;
+use crypto::crypto_rand::{RandValue, CryptoRandom};
 
 use super::hash_clock::HashClock;
 use super::ratchet::RatchetPool;
 use super::verifier::Verifier;
 
 
-struct SimpleVerifier<N,U> {
+struct SimpleVerifier<N,U,R> {
     hash_clock: HashClock<N>,
     ratchet_pool: RatchetPool<N,U>,
+    rng: R,
 }
 
-impl<N,U> SimpleVerifier<N,U> 
+impl<N,U,R> SimpleVerifier<N,U,R> 
 where
     N: std::cmp::Eq + std::hash::Hash + Clone,
     U: std::cmp::Eq + Clone,
+    R: CryptoRandom,
 {
 
-    pub fn new(ticks_to_live: usize) -> Self {
+    pub fn new(ticks_to_live: usize, rng: R) -> Self {
         // TODO(Security): Make sure that we don't have an off-by-one here with the decision to have
         // one ticks_to_live value for both `hash_clock` and `ratchet_pool`.
        
@@ -26,15 +28,17 @@ where
         SimpleVerifier {
             hash_clock: HashClock::new(ticks_to_live),
             ratchet_pool: RatchetPool::new(ticks_to_live),
+            rng,
         }
     }
 
 }
 
-impl<N,U> Verifier for SimpleVerifier<N,U>
+impl<N,U,R> Verifier for SimpleVerifier<N,U,R>
 where
     N: std::cmp::Eq + std::hash::Hash + Clone,
     U: std::cmp::Eq + Clone,
+    R: CryptoRandom,
 {
     type Node = N;
     type SessionId = U;
@@ -60,7 +64,8 @@ where
         Some(hashes)
     }
 
-    fn tick(&mut self, rand_value: RandValue) -> (HashResult, Vec<N>) {
+    fn tick(&mut self) -> (HashResult, Vec<N>) {
+        let rand_value = RandValue::new(&self.rng);
         (self.hash_clock.tick(rand_value), self.ratchet_pool.tick())
     }
 
@@ -73,4 +78,3 @@ where
     }
 }
 
-// TODO: Tests
--- components/index_server/src/verifier/verifier.rs
+++ components/index_server/src/verifier/verifier.rs
@@ -1,6 +1,4 @@
 use crypto::hash::HashResult;
-use crypto::crypto_rand::RandValue;
-
 
 pub trait Verifier {
     type Node;
@@ -18,7 +16,7 @@ pub trait Verifier {
 
     /// One time tick. Returns a `tick_hash` representing the local current time,
     /// and a vector of all the nodes removed due to timeout
-    fn tick(&mut self, rand_value: RandValue) -> (HashResult, Vec<Self::Node>);
+    fn tick(&mut self) -> (HashResult, Vec<Self::Node>);
 
     /// Process a time tick from a neighbor. This information is used when producing a `tick_hash`.
     fn neighbor_tick(&mut self, neighbor: Self::Node, tick_hash: HashResult) -> Option<HashResult>;
