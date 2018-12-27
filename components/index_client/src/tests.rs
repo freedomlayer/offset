1ef9f71b3da37372fd4f22be595e53ea18f82b23
real <real@freedomlayer.org>
2018-12-27 16:41:48 +0200

    Some work on index_client first test. Not done yet.

--- components/index_client/src/tests.rs
+++ components/index_client/src/tests.rs
@@ -1,11 +1,16 @@
 use futures::task::{Spawn, SpawnExt};
 use futures::executor::ThreadPool;
-use futures::channel::mpsc;
-use futures::{FutureExt, TryFutureExt};
+use futures::channel::{mpsc, oneshot};
+use futures::{FutureExt, TryFutureExt, StreamExt, SinkExt};
 
 use common::dummy_connector::DummyConnector;
 
-use crate::index_client::{index_client_loop, IndexClientConfig};
+use crypto::hash::{HashResult, HASH_RESULT_LEN};
+use proto::index_client::messages::{AppServerToIndexClient, IndexClientToAppServer,
+                                    IndexClientReportMutation};
+
+use crate::index_client::{index_client_loop, IndexClientConfig,
+                        IndexClientConfigMutation};
 use crate::seq_friends::SeqFriendsClient;
 
 
@@ -14,8 +19,8 @@ where
     S: Spawn + Clone + Send + 'static,
 {
 
-    let (app_server_sender, from_app_server) = mpsc::channel(0);
-    let (to_app_server, app_server_receiver) = mpsc::channel(0);
+    let (mut app_server_sender, from_app_server) = mpsc::channel(0);
+    let (to_app_server, mut app_server_receiver) = mpsc::channel(0);
 
     let index_client_config = IndexClientConfig {
         index_servers: vec![0x1337u32],
@@ -24,15 +29,15 @@ where
     let (seq_friends_sender, seq_friends_receiver) = mpsc::channel(0);
     let seq_friends_client = SeqFriendsClient::new(seq_friends_sender);
 
-
-    let (session_sender, session_receiver) = mpsc::channel(0);
+    let (session_sender, mut session_receiver) = mpsc::channel(0);
     let index_client_session = DummyConnector::new(session_sender);
 
-    let (database_sender, database_receiver) = mpsc::channel(0);
+    let (database_sender, mut database_receiver) = mpsc::channel(0);
     let database = DummyConnector::new(database_sender);
 
     let max_open_requests = 2;
     let keepalive_ticks = 8;
+    let backoff_ticks = 4;
 
     let (tick_sender, timer_stream) = mpsc::channel::<()>(0);
 
@@ -43,6 +48,7 @@ where
                                index_client_session,
                                max_open_requests,
                                keepalive_ticks,
+                               backoff_ticks,
                                database,
                                timer_stream,
                                spawner.clone())
@@ -51,6 +57,46 @@ where
 
     spawner.spawn(loop_fut).unwrap();
 
+    // Wait for a connection request:
+    let session_conn_request = await!(session_receiver.next()).unwrap();
+    assert_eq!(session_conn_request.address, 0x1337);
+
+    // Send a SessionHandle back to the index client:
+    let (control_sender, control_receiver) = mpsc::channel(0);
+    let (close_sender, close_receiver) = oneshot::channel();
+    session_conn_request.reply(Some((control_sender, close_receiver)));
+
+    // We should be notified that a connection to a server was established:
+    match await!(app_server_receiver.next()).unwrap() {
+        IndexClientToAppServer::ReportMutations(mut mutations) => {
+            assert_eq!(mutations.len(), 1);
+            match mutations.pop().unwrap() {
+                IndexClientReportMutation::SetConnectedServer(Some(address)) => 
+                    assert_eq!(address, 0x1337),
+                _ => unreachable!(),
+            };
+        },
+        _ => unreachable!(),
+    };
+
+    // Add an index server:
+    await!(app_server_sender.send(AppServerToIndexClient::AddIndexServer(0x1338))).unwrap();
+
+    let request = await!(database_receiver.next()).unwrap();
+    assert_eq!(request.address, IndexClientConfigMutation::AddIndexServer(0x1338));
+    request.reply(Some(()));
+
+    match await!(app_server_receiver.next()).unwrap() {
+        IndexClientToAppServer::ReportMutations(mut mutations) => {
+            assert_eq!(mutations.len(), 1);
+            match mutations.pop().unwrap() {
+                IndexClientReportMutation::AddIndexServer(address) => 
+                    assert_eq!(address, 0x1338),
+                _ => unreachable!(),
+            };
+        },
+        _ => unreachable!(),
+    };
 
     // TODO: Continue test here.
 }
