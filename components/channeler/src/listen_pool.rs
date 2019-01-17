1e8179b1d015e0ecc263309ea754c4be4513138d
real <real@freedomlayer.org>
2019-01-17 10:50:49 +0200

    Moved AccessControlPk and AccessControlOpPk to types.rs.

--- components/channeler/src/listen_pool.rs
+++ components/channeler/src/listen_pool.rs
@@ -1,12 +1,21 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+use futures::{Stream, StreamExt, SinkExt};
 use futures::channel::mpsc;
+use futures::task::{Spawn, SpawnExt};
+
+use common::conn::{Listener, FutTransform};
+
 use crypto::identity::PublicKey;
+use crate::types::{RawConn, AccessControlPk, AccessControlOpPk};
 
 pub enum LpConfig<B> {
     SetLocalAddress(Vec<B>),
     UpdateFriend((PublicKey, Vec<B>)),
     RemoveFriend(PublicKey),
 }
+
+#[derive(Debug)]
+struct ListenPoolClientError;
 
 
 pub struct LpConfigClient<B> {
@@ -19,13 +28,50 @@ impl<B> LpConfigClient<B> {
             request_sender,
         }
     }
+
+    pub async fn config(&mut self, config: LpConfig<B>) -> Result<(), ListenPoolClientError> {
+        await!(self.request_sender.send(config))
+            .map_err(|_| ListenPoolClientError)?;
+        Ok(())
+    }
+}
+
+enum RelayStatus {
+    Connecting,
+    Connected,
+}
+
+struct Relay {
+    friends: HashSet<PublicKey>,
+    status: RelayStatus,
 }
 
 struct ListenPool<B> {
     local_address: Vec<B>,
-    friends: HashMap<PublicKey, Vec<B>>,
+    // friends: HashMap<PublicKey, Vec<B>>,
+    relays: HashMap<B, Relay>,
 }
 
-async fn listen_pool_loop<B>(incoming_config: mpsc::Receiver<LpConfig<B>>) {
+#[derive(Debug)]
+enum ListenPoolError {
+}
+
+enum ConnectPoolEvent {
+
+}
+
+async fn listen_pool_loop<B,L,TS,S>(incoming_config: mpsc::Receiver<LpConfig<B>>,
+                             listener: L,
+                             backoff_ticks: usize,
+                             timer_stream: TS,
+                             spawner: S) -> Result<(), ListenPoolError>
+where
+    L: Listener<Connection=(PublicKey, RawConn), 
+        Config=AccessControlOpPk, Arg=(B, AccessControlPk)> + Clone + 'static,
+    TS: Stream,
+    S: Spawn,
+{
+
+    unimplemented!();
 }
 
