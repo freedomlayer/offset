34316bed1354e7f6db8e8a432fc94c2323cc58ae
real <real@freedomlayer.org>
2019-01-17 16:21:50 +0200

    Some work on listen_pool. Not done yet.

--- components/channeler/src/listen_pool.rs
+++ components/channeler/src/listen_pool.rs
@@ -1,6 +1,6 @@
 use std::hash::Hash;
 use std::collections::{HashMap, HashSet};
-use futures::{future, stream, Stream, StreamExt, Sink, SinkExt};
+use futures::{future, FutureExt, TryFutureExt, stream, Stream, StreamExt, Sink, SinkExt};
 use futures::channel::mpsc;
 use futures::task::{Spawn, SpawnExt};
 
@@ -9,7 +9,7 @@ use common::access_control::{AccessControlOp, AccessControl};
 
 use crypto::identity::PublicKey;
 use crate::types::{RawConn, AccessControlPk, AccessControlOpPk};
-use crate::transform_pool::create_transform_pool;
+use crate::transform_pool::transform_pool_loop;
 
 pub enum LpConfig<B> {
     SetLocalAddresses(Vec<B>),
@@ -50,7 +50,6 @@ enum ListenPoolError {
 enum LpEvent<B> {
     Config(LpConfig<B>),
     ConfigClosed,
-    IncomingEncryptedConn((PublicKey, RawConn)), // Incoming encrypted connection
     RelayClosed(B),
     TimerTick,
     TimerClosed,
@@ -68,7 +67,6 @@ struct Relay {
 
 struct ListenPool<B,L,S> {
     local_addresses: HashSet<B>,
-    // friends: HashMap<PublicKey, HashSet<B>>,
     relays: HashMap<B, Relay>,
     plain_conn_sender: mpsc::Sender<(PublicKey, RawConn)>,
     listener: L,
@@ -81,7 +79,7 @@ where
     B: Hash + Eq + Clone,
     L: Listener<Connection=(PublicKey, RawConn), 
         Config=AccessControlOpPk, Arg=(B, AccessControlPk)> + Clone + 'static,
-    S: Spawn,
+    S: Spawn + Clone,
 {
     pub fn new(plain_conn_sender: mpsc::Sender<(PublicKey, RawConn)>,
            listener: L,
@@ -98,46 +96,31 @@ where
         }
     }
 
-    fn start_listen(&mut self, address: B, opt_friend: Option<PublicKey>) -> Result<(), ListenPoolError> {
-        // TODO: Assert that we are not already listening to relay address?
-        
-        assert!(!self.relays.contains_key(&address));
-        if let None = opt_friend {
-            // No friend will try to connect on this address, but we defined it
-            // as one of our local addresses:
-            assert!(self.local_addresses.contains(&address));
-        }
-        
-        let relay_friends = opt_friend
-            .into_iter()
-            .collect::<HashSet<_>>();
+    fn spawn_listen(&self, address: B, relay_friends: &HashSet<PublicKey>) 
+        -> Result<mpsc::Sender<AccessControlOpPk>, ListenPoolError> {
 
         // Fill in access_control:
         let mut access_control = AccessControlPk::new();
 
-        for friend_public_key in &relay_friends {
+        for friend_public_key in relay_friends {
             access_control.apply_op(AccessControlOp::Add(friend_public_key.clone()));
         }
 
         let (access_control_sender, mut connections_receiver) = 
-            self.listener.clone().listen((address.clone(), access_control));
+            self.listener.clone().listen((address, access_control));
         // TODO: Do we need the listener.clone() here? Maybe Listen doesn't need to take ownership
         // over self?
 
         let mut c_plain_conn_sender = self.plain_conn_sender.clone();
         let send_fut = async move {
             let _ = await!(c_plain_conn_sender.send_all(&mut connections_receiver));
+            // TODO: We should somehow send LpEvent::RelayClosed to the main loop
+            unimplemented!();
         };
-        self.spawner.spawn(send_fut)
+        self.spawner.clone().spawn(send_fut)
             .map_err(|_| ListenPoolError::SpawnError)?;
 
-        let relay = Relay {
-            friends: relay_friends,
-            status: RelayStatus::Connected(access_control_sender),
-        };
-
-        self.relays.insert(address, relay);
-        Ok(())
+        Ok(access_control_sender)
     }
 
     pub fn handle_config(&mut self, config: LpConfig<B>) -> Result<(), ListenPoolError> {
@@ -166,7 +149,13 @@ where
                 }
 
                 for address in new_addresses {
-                    self.start_listen(address.clone(), None)?;
+                    let relay_friends = HashSet::new();
+                    let access_control_sender = self.spawn_listen(address.clone(), &relay_friends)?;
+                    let relay = Relay {
+                        friends: relay_friends,
+                        status: RelayStatus::Connected(access_control_sender),
+                    };
+                    self.relays.insert(address, relay);
                 }
             },
             LpConfig::UpdateFriend((friend_public_key, addresses)) => {
@@ -176,7 +165,14 @@ where
                             relay.friends.insert(friend_public_key.clone());
                         },
                         None => {
-                            self.start_listen(address.clone(), Some(friend_public_key.clone()))?;
+                            let mut relay_friends = HashSet::new();
+                            relay_friends.insert(friend_public_key.clone());
+                            let access_control_sender = self.spawn_listen(address.clone(), &relay_friends)?;
+                            let relay = Relay {
+                                friends: relay_friends,
+                                status: RelayStatus::Connected(access_control_sender),
+                            };
+                            self.relays.insert(address.clone(), relay);
                         },
                     }
                 }
@@ -198,31 +194,55 @@ where
         Ok(())
     }
 
-    pub fn handle_incoming_encrypted_conn(&mut self,
-                                      remote_public_key: PublicKey,
-                                      enc_conn: RawConn) -> Result<(), ListenPoolError> {
-        unimplemented!();
-    }
-
     pub fn handle_relay_closed(&mut self,
                            address: B) -> Result<(), ListenPoolError> {
 
-        unimplemented!();
+        let relay = match self.relays.get_mut(&address) {
+            Some(relay) => relay,
+            None => return Ok(()), // TODO: Could this happen?
+        };
+
+        relay.status = RelayStatus::Waiting(self.backoff_ticks);
+        Ok(())
     }
 
     pub fn handle_timer_tick(&mut self) -> Result<(), ListenPoolError> {
-        unimplemented!();
+        let mut spawn_addresses = Vec::new();
+        for (address, relay) in &mut self.relays {
+            match &mut relay.status {
+                RelayStatus::Waiting(ref mut remaining_ticks) => {
+                    *remaining_ticks = (*remaining_ticks).saturating_sub(1);
+                    if *remaining_ticks > 0 {
+                        continue;
+                    }
+                    spawn_addresses.push(address.clone());
+                },
+                RelayStatus::Connected(_access_control_sender) => {}, // Nothing to do
+            }
+        }
+
+        // Reconnect to relays for which enough time has passed:
+        for address in spawn_addresses {
+            let relay = self.relays.get(&address).unwrap();
+            let access_control_sender = self.spawn_listen(address.clone(), &relay.friends)?;
+
+            let relay = self.relays.get_mut(&address).unwrap();
+            relay.status = RelayStatus::Connected(access_control_sender);
+        }
+        Ok(())
     }
 }
 
 
+#[allow(unused)]
 async fn listen_pool_loop<B,L,ET,TS,S>(incoming_config: mpsc::Receiver<LpConfig<B>>,
-                                 listener: L,
-                                 encrypt_transform: ET,
-                                 max_concurrent_encrypt: usize,
-                                 backoff_ticks: usize,
-                                 timer_stream: TS,
-                                 spawner: S) -> Result<(), ListenPoolError>
+                                       outgoing_conns: mpsc::Sender<(PublicKey, RawConn)>,
+                                       listener: L,
+                                       encrypt_transform: ET,
+                                       max_concurrent_encrypt: usize,
+                                       backoff_ticks: usize,
+                                       timer_stream: TS,
+                                       mut spawner: S) -> Result<(), ListenPoolError>
 where
     B: Clone + Eq + Hash,
     L: Listener<Connection=(PublicKey, RawConn), 
@@ -231,20 +251,24 @@ where
     TS: Stream + Unpin,
     S: Spawn + Clone + Send + 'static,
 {
+    
+    let (plain_conn_sender, incoming_plain_conn) = mpsc::channel(0);
+    let enc_loop_fut = transform_pool_loop(incoming_plain_conn,
+                        outgoing_conns,
+                        encrypt_transform,
+                        max_concurrent_encrypt,
+                        spawner.clone())
+        .map_err(|e| error!("transform_pool_loop: {:?}", e))
+        .map(|_| ());
 
-    let (plain_conn_sender, incoming_encrypted_conn) = create_transform_pool(
-                          encrypt_transform, 
-                          max_concurrent_encrypt,
-                          spawner.clone())
-        .map_err(|_| ListenPoolError::CreateEncryptPoolError)?;
+    spawner.spawn(enc_loop_fut)
+        .map_err(|_| ListenPoolError::SpawnError)?;
+    
 
     let mut listen_pool = ListenPool::<B,L,S>::new(plain_conn_sender,
                                       listener,
                                       backoff_ticks,
                                       spawner);
-
-    let incoming_encrypted_conn = incoming_encrypted_conn
-        .map(|pk_enc_conn| LpEvent::IncomingEncryptedConn(pk_enc_conn));
 
     let incoming_config = incoming_config
         .map(|config| LpEvent::Config(config))
@@ -254,17 +278,13 @@ where
         .map(|_| LpEvent::<B>::TimerTick)
         .chain(stream::once(future::ready(LpEvent::TimerClosed)));
 
-    let mut incoming_events = incoming_encrypted_conn
-        .select(incoming_config)
-        .select(timer_stream);
+    let mut incoming_events = incoming_config.select(timer_stream);
 
     while let Some(event) = await!(incoming_events.next()) {
         match event {
             LpEvent::Config(config) => 
                 listen_pool.handle_config(config)?,
             LpEvent::ConfigClosed => return Err(ListenPoolError::ConfigClosed),
-            LpEvent::IncomingEncryptedConn((remote_public_key, enc_conn)) => 
-                listen_pool.handle_incoming_encrypted_conn(remote_public_key, enc_conn)?,
             LpEvent::RelayClosed(address) => 
                 listen_pool.handle_relay_closed(address)?,
             LpEvent::TimerTick => 
