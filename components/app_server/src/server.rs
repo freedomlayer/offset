1bff77ff3a09de172f9d913c4ad4f457181ac84b
real <real@freedomlayer.org>
2019-01-22 17:40:48 +0200

    app_server: Added a tests for forwarding a funder command.

--- components/app_server/src/server.rs
+++ components/app_server/src/server.rs
@@ -467,21 +467,20 @@ mod tests {
     use im::hashmap::HashMap as ImHashMap;
 
     use crypto::identity::{PublicKey, PUBLIC_KEY_LEN};
-    use proto::funder::report::FunderReport;
+    use proto::funder::report::{FunderReport, FunderReportMutation};
     use proto::index_client::messages::IndexClientReport;
 
-
-    async fn task_app_server_loop_basic<S>(mut spawner: S) 
+    async fn task_app_server_loop_funder_command<S>(mut spawner: S) 
     where
         S: Spawn + Clone + Send + 'static,
     {
-        let (funder_sender, from_funder) = mpsc::channel(0);
-        let (to_funder, funder_receiver) = mpsc::channel(0);
+        let (mut funder_sender, from_funder) = mpsc::channel(0);
+        let (to_funder, mut funder_receiver) = mpsc::channel(0);
 
         let (index_client_sender, from_index_client) = mpsc::channel(0);
         let (to_index_client, index_client_receiver) = mpsc::channel(0);
 
-        let (connections_sender, incoming_connections) = mpsc::channel(0);
+        let (mut connections_sender, incoming_connections) = mpsc::channel(0);
 
         // Create a dummy initial_node_report:
         let funder_report = FunderReport {
@@ -506,19 +505,68 @@ mod tests {
                         from_index_client,
                         to_index_client,
                         incoming_connections,
-                        initial_node_report,
+                        initial_node_report.clone(),
                         spawner.clone())
             .map_err(|e| error!("app_server_loop() error: {:?}", e))
             .map(|_| ());
 
         spawner.spawn(fut_loop);
 
-        // TODO: Continue here
+        let (mut app_sender, app_server_receiver) = mpsc::channel(0);
+        let (app_server_sender, mut app_receiver) = mpsc::channel(0);
+        let app_server_conn_pair = (app_server_sender, app_server_receiver);
+
+        let app_permissions = AppPermissions {
+            reports: true,
+            routes: true,
+            send_funds: true,
+            config: true,
+        };
+
+        await!(connections_sender.send((app_permissions, app_server_conn_pair))).unwrap();
+
+        // The app should receive the current node report as the first message:
+        let to_app_message = await!(app_receiver.next()).unwrap();
+        match to_app_message {
+            AppServerToApp::Report(report) => assert_eq!(report, initial_node_report),
+            _ => unreachable!(),
+        };
+
+        // Send a command through the app:
+        let new_address = vec![0u32, 1u32, 2u32];
+        await!(app_sender.send(AppToAppServer::SetRelays(new_address.clone()))).unwrap();
+
+        // SetRelays command should be forwarded to the Funder:
+        let to_funder_message = await!(funder_receiver.next()).unwrap();
+        match to_funder_message {
+            FunderIncomingControl::SetAddress(address) => assert_eq!(address, new_address),
+            _ => unreachable!(),
+        };
+
+        let funder_report_mutation = FunderReportMutation::SetAddress(new_address.clone());
+        let funder_report_mutations = vec![funder_report_mutation.clone()];
+        await!(funder_sender.send(FunderOutgoingControl::ReportMutations(funder_report_mutations))).unwrap();
+
+        let to_app_message = await!(app_receiver.next()).unwrap();
+        match to_app_message {
+            AppServerToApp::ReportMutations(report_mutations) => {
+                assert_eq!(report_mutations.len(), 1);
+                let report_mutation = &report_mutations[0];
+                match report_mutation {
+                    NodeReportMutation::Funder(received_funder_report_mutation) => {
+                        assert_eq!(received_funder_report_mutation, 
+                                   &funder_report_mutation);
+                    },
+                    _ => unreachable!(),
+                }
+            },
+            _ => unreachable!(),
+        }
     }
 
     #[test]
-    fn test_app_server_loop_basic() {
+    fn test_app_server_loop_funder_command() {
         let mut thread_pool = ThreadPool::new().unwrap();
-        thread_pool.run(task_app_server_loop_basic(thread_pool.clone()));
+        thread_pool.run(task_app_server_loop_funder_command(thread_pool.clone()));
     }
 }
