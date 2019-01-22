f029174321a0581c9b19ab236394a4c273a91688
real <real@freedomlayer.org>
2019-01-22 12:03:46 +0200

    Some work on outline of app server loop.

--- components/app_server/src/server.rs
+++ components/app_server/src/server.rs
@@ -10,6 +10,7 @@ use crypto::identity::PublicKey;
 
 use proto::funder::messages::{FunderOutgoingControl, FunderIncomingControl};
 use proto::app_server::messages::{AppServerToApp, AppToAppServer};
+use proto::index_client::messages::{IndexClientToAppServer, AppServerToIndexClient};
 
 use crate::config::AppPermissions;
 
@@ -19,17 +20,21 @@ type IncomingAppConnection<B,ISA> = (PublicKey, AppPermissions, ConnPair<AppServ
 pub enum AppServerError {
     FunderClosed,
     SpawnError,
+    IndexClientClosed,
 }
 
 pub enum AppServerEvent<B: Clone,ISA> {
     IncomingConnection(IncomingAppConnection<B,ISA>),
-    FromFunder(FunderOutgoingControl<B>),
+    FromFunder(FunderOutgoingControl<Vec<B>>),
     FunderClosed,
+    FromIndexClient(IndexClientToAppServer<ISA>),
+    IndexClientClosed,
     FromApp((u128, Option<AppToAppServer<B,ISA>>)), // None means that app was closed
 }
 
 pub struct App<B: Clone,ISA> {
     public_key: PublicKey,
+    permissions: AppPermissions,
     opt_sender: Option<mpsc::Sender<AppServerToApp<B,ISA>>>,
 }
 
@@ -67,7 +72,7 @@ where
     where
         S: Spawn,
     {
-        let (public_key, app_permissions, (sender, mut receiver)) = incoming_app_connection;
+        let (public_key, permissions, (sender, mut receiver)) = incoming_app_connection;
 
         let app_counter = self.app_counter;
         let mut receiver = receiver.map(move |app_to_app_server| (app_counter.clone(), Some(app_to_app_server)))
@@ -80,6 +85,7 @@ where
 
         let app = App {
             public_key,
+            permissions,
             opt_sender: Some(sender),
         };
 
@@ -87,18 +93,42 @@ where
         self.app_counter = self.app_counter.wrapping_add(1);
         Ok(())
     }
+
+    fn handle_from_funder(&mut self, funder_message: FunderOutgoingControl<Vec<B>>)
+        -> Result<(), AppServerError> {
+
+        unimplemented!();
+    }
+
+    fn handle_from_index_client(&mut self, index_client_message: IndexClientToAppServer<ISA>) 
+        -> Result<(), AppServerError> {
+
+        unimplemented!();
+    }
+
+    fn handle_from_app(&mut self, app_id: u128, opt_app_message: Option<AppToAppServer<B,ISA>>)
+        -> Result<(), AppServerError> {
+        // Old code:
+        // App connection closed:
+        // app_server.apps.remove(&conn_id).unwrap();
+        unimplemented!();
+    }
 }
 
 
-pub async fn app_server_loop<B,ISA,FF,TF,IC,S>(from_funder: FF, 
-                                               to_funder: TF, 
-                                               incoming_connections: IC,
-                                               mut spawner: S) -> Result<(), AppServerError>
+pub async fn app_server_loop<B,ISA,FF,TF,FIC,TIC,IC,S>(from_funder: FF, 
+                                                       to_funder: TF, 
+                                                       from_index_client: FIC,
+                                                       to_index_client: TIC,
+                                                       incoming_connections: IC,
+                                                       mut spawner: S) -> Result<(), AppServerError>
 where
     B: Clone + Send + 'static,
     ISA: Send + 'static,
-    FF: Stream<Item=FunderOutgoingControl<B>> + Unpin,
-    TF: Sink<SinkItem=FunderIncomingControl<B>> + Unpin + Sync + Send,
+    FF: Stream<Item=FunderOutgoingControl<Vec<B>>> + Unpin,
+    TF: Sink<SinkItem=FunderIncomingControl<Vec<B>>> + Unpin + Sync + Send,
+    FIC: Stream<Item=IndexClientToAppServer<ISA>> + Unpin,
+    TIC: Sink<SinkItem=AppServerToIndexClient<ISA>> + Unpin,
     IC: Stream<Item=IncomingAppConnection<B,ISA>> + Unpin,
     S: Spawn,
 {
@@ -110,6 +140,10 @@ where
         .map(|funder_outgoing_control| AppServerEvent::FromFunder(funder_outgoing_control))
         .chain(stream::once(future::ready(AppServerEvent::FunderClosed)));
 
+    let from_index_client = from_index_client
+        .map(|index_client_msg| AppServerEvent::FromIndexClient(index_client_msg))
+        .chain(stream::once(future::ready(AppServerEvent::IndexClientClosed)));
+
     let from_app_receiver = from_app_receiver
         .map(|from_app: (u128, Option<AppToAppServer<B,ISA>>)| AppServerEvent::FromApp(from_app));
 
@@ -117,6 +151,7 @@ where
         .map(|incoming_connection| AppServerEvent::IncomingConnection(incoming_connection));
 
     let mut events = from_funder
+                    .select(from_index_client)
                     .select(from_app_receiver)
                     .select(incoming_connections);
 
@@ -125,16 +160,15 @@ where
             AppServerEvent::IncomingConnection(incoming_app_connection) => {
                 app_server.add_app_connection(incoming_app_connection, &mut spawner)?;
             },
-            AppServerEvent::FromFunder(funder_outgoing_control) => unimplemented!(),
+            AppServerEvent::FromFunder(funder_outgoing_control) => 
+                app_server.handle_from_funder(funder_outgoing_control)?,
             AppServerEvent::FunderClosed => return Err(AppServerError::FunderClosed),
-            AppServerEvent::FromApp((conn_id, Some(funder_incoming_control))) => unimplemented!(),
-            AppServerEvent::FromApp((conn_id, None)) => {
-                // App connection closed:
-                app_server.apps.remove(&conn_id).unwrap();
-            }
+            AppServerEvent::FromIndexClient(from_index_client) => 
+                app_server.handle_from_index_client(from_index_client)?,
+            AppServerEvent::IndexClientClosed => return Err(AppServerError::IndexClientClosed),
+            AppServerEvent::FromApp((app_id, opt_app_message)) => 
+                app_server.handle_from_app(app_id, opt_app_message)?,
         }
-        // TODO
     }
-    
-    unimplemented!();
+    Ok(())
 }
