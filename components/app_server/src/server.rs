d4aa71441d809a36345fd0d10a61663f8e025e92
real <real@freedomlayer.org>
2019-01-22 18:33:16 +0200

    Added some app_server_loop() tests.

--- components/app_server/src/server.rs
+++ components/app_server/src/server.rs
@@ -467,10 +467,23 @@ mod tests {
     use im::hashmap::HashMap as ImHashMap;
 
     use crypto::identity::{PublicKey, PUBLIC_KEY_LEN};
+    use crypto::uid::UID_LEN;
     use proto::funder::report::{FunderReport, FunderReportMutation};
-    use proto::index_client::messages::IndexClientReport;
+    use proto::index_client::messages::{IndexClientReport, IndexClientReportMutation, 
+        ClientResponseRoutes, ResponseRoutesResult};
+    use proto::index_server::messages::RequestRoutes;
 
-    async fn task_app_server_loop_funder_command<S>(mut spawner: S) 
+    /// A test util function.
+    /// Spawns an app server loop and returns all relevant channels
+    /// used for control or communication.
+    fn spawn_dummy_app_server<S>(mut spawner: S) -> 
+        (mpsc::Sender<FunderOutgoingControl<Vec<u32>>>,
+         mpsc::Receiver<FunderIncomingControl<Vec<u32>>>,
+         mpsc::Sender<IndexClientToAppServer<u64>>,
+         mpsc::Receiver<AppServerToIndexClient<u64>>,
+         mpsc::Sender<IncomingAppConnection<u32,u64>>,
+         NodeReport<u32,u64>)
+
     where
         S: Spawn + Clone + Send + 'static,
     {
@@ -510,7 +523,23 @@ mod tests {
             .map_err(|e| error!("app_server_loop() error: {:?}", e))
             .map(|_| ());
 
-        spawner.spawn(fut_loop);
+        spawner.spawn(fut_loop).unwrap();
+
+        (funder_sender, funder_receiver,
+         index_client_sender, index_client_receiver,
+         connections_sender, initial_node_report)
+
+
+    }
+
+    async fn task_app_server_loop_funder_command<S>(mut spawner: S) 
+    where
+        S: Spawn + Clone + Send + 'static,
+    {
+
+        let (mut funder_sender, mut funder_receiver,
+             index_client_sender, index_client_receiver,
+             mut connections_sender, initial_node_report) = spawn_dummy_app_server(spawner.clone());
 
         let (mut app_sender, app_server_receiver) = mpsc::channel(0);
         let (app_server_sender, mut app_receiver) = mpsc::channel(0);
@@ -568,5 +597,335 @@ mod tests {
     fn test_app_server_loop_funder_command() {
         let mut thread_pool = ThreadPool::new().unwrap();
         thread_pool.run(task_app_server_loop_funder_command(thread_pool.clone()));
+    }
+
+    // -------------------------------------------------
+    // -------------------------------------------------
+
+    async fn task_app_server_loop_index_client_command<S>(mut spawner: S) 
+    where
+        S: Spawn + Clone + Send + 'static,
+    {
+
+        let (mut funder_sender, mut funder_receiver,
+             mut index_client_sender, mut index_client_receiver,
+             mut connections_sender, initial_node_report) = spawn_dummy_app_server(spawner.clone());
+
+        let (mut app_sender, app_server_receiver) = mpsc::channel(0);
+        let (app_server_sender, mut app_receiver) = mpsc::channel(0);
+        let app_server_conn_pair = (app_server_sender, app_server_receiver);
+
+        let app_permissions = AppPermissions {
+            reports: true,
+            routes: true,
+            send_funds: true,
+            config: true,
+        };
+
+        await!(connections_sender.send((app_permissions, app_server_conn_pair))).unwrap();
+
+        // The app should receive the current node report as the first message:
+        let to_app_message = await!(app_receiver.next()).unwrap();
+        match to_app_message {
+            AppServerToApp::Report(report) => assert_eq!(report, initial_node_report),
+            _ => unreachable!(),
+        };
+
+        // Send a command through the app:
+        let new_index_server_address = 300u64;
+        await!(app_sender.send(AppToAppServer::AddIndexServer(new_index_server_address))).unwrap();
+
+        // AddIndexServer command should be forwarded to IndexClient:
+        let to_index_client_message = await!(index_client_receiver.next()).unwrap();
+        match to_index_client_message {
+            AppServerToIndexClient::AddIndexServer(address) => assert_eq!(address, new_index_server_address),
+            _ => unreachable!(),
+        };
+
+        let index_client_report_mutation = IndexClientReportMutation::AddIndexServer(new_index_server_address.clone());
+        let index_client_report_mutations = vec![index_client_report_mutation.clone()];
+        await!(index_client_sender.send(IndexClientToAppServer::ReportMutations(index_client_report_mutations))).unwrap();
+
+        let to_app_message = await!(app_receiver.next()).unwrap();
+        match to_app_message {
+            AppServerToApp::ReportMutations(report_mutations) => {
+                assert_eq!(report_mutations.len(), 1);
+                let report_mutation = &report_mutations[0];
+                match report_mutation {
+                    NodeReportMutation::IndexClient(received_index_client_report_mutation) => {
+                        assert_eq!(received_index_client_report_mutation, 
+                                   &index_client_report_mutation);
+                    },
+                    _ => unreachable!(),
+                }
+            },
+            _ => unreachable!(),
+        }
+    }
+
+    #[test]
+    fn test_app_server_loop_index_client_command() {
+        let mut thread_pool = ThreadPool::new().unwrap();
+        thread_pool.run(task_app_server_loop_index_client_command(thread_pool.clone()));
+    }
+
+
+    // -------------------------------------------------
+    // -------------------------------------------------
+
+    async fn task_app_server_loop_all_apps_closed<S>(mut spawner: S) 
+    where
+        S: Spawn + Clone + Send + 'static,
+    {
+
+        let (mut funder_sender, mut funder_receiver,
+             mut index_client_sender, mut index_client_receiver,
+             mut connections_sender, initial_node_report) = spawn_dummy_app_server(spawner.clone());
+
+        let (mut app_sender, app_server_receiver) = mpsc::channel(0);
+        let (app_server_sender, mut app_receiver) = mpsc::channel(0);
+        let app_server_conn_pair = (app_server_sender, app_server_receiver);
+
+        let app_permissions = AppPermissions {
+            reports: true,
+            routes: true,
+            send_funds: true,
+            config: true,
+        };
+
+        await!(connections_sender.send((app_permissions, app_server_conn_pair))).unwrap();
+
+        // The app should receive the current node report as the first message:
+        let to_app_message = await!(app_receiver.next()).unwrap();
+        match to_app_message {
+            AppServerToApp::Report(report) => assert_eq!(report, initial_node_report),
+            _ => unreachable!(),
+        };
+
+        drop(connections_sender);
+
+        // Communication with the last connected app should still work:
+        let new_index_server_address = 300u64;
+        let index_client_report_mutation = IndexClientReportMutation::AddIndexServer(new_index_server_address.clone());
+        let index_client_report_mutations = vec![index_client_report_mutation.clone()];
+        await!(index_client_sender.send(IndexClientToAppServer::ReportMutations(index_client_report_mutations))).unwrap();
+
+        let to_app_message = await!(app_receiver.next()).unwrap(); 
+
+        // Last app disconnects:
+        drop(app_sender);
+
+        // At this point the app_server loop should close.
+
+        assert!(await!(funder_receiver.next()).is_none());
+        assert!(await!(index_client_receiver.next()).is_none());
+    }
+
+    #[test]
+    fn test_app_server_loop_index_all_apps_closed() {
+        let mut thread_pool = ThreadPool::new().unwrap();
+        thread_pool.run(task_app_server_loop_all_apps_closed(thread_pool.clone()));
+    }
+
+
+    // -------------------------------------------------
+    // -------------------------------------------------
+
+    async fn task_app_server_loop_two_apps<S>(mut spawner: S) 
+    where
+        S: Spawn + Clone + Send + 'static,
+    {
+
+        let (mut funder_sender, mut funder_receiver,
+             mut index_client_sender, mut index_client_receiver,
+             mut connections_sender, initial_node_report) = spawn_dummy_app_server(spawner.clone());
+
+        let (mut app_sender0, app_server_receiver) = mpsc::channel(0);
+        let (app_server_sender, mut app_receiver0) = mpsc::channel(0);
+        let app_server_conn_pair = (app_server_sender, app_server_receiver);
+        let app_permissions = AppPermissions {
+            reports: true,
+            routes: true,
+            send_funds: true,
+            config: true,
+        };
+        await!(connections_sender.send((app_permissions, app_server_conn_pair))).unwrap();
+
+        let (mut app_sender1, app_server_receiver) = mpsc::channel(0);
+        let (app_server_sender, mut app_receiver1) = mpsc::channel(0);
+        let app_server_conn_pair = (app_server_sender, app_server_receiver);
+        let app_permissions = AppPermissions {
+            reports: true,
+            routes: true,
+            send_funds: true,
+            config: true,
+        };
+        await!(connections_sender.send((app_permissions, app_server_conn_pair))).unwrap();
+
+        // The apps should receive the current node report as the first message:
+        // Send a report 
+        let to_app_message = await!(app_receiver0.next()).unwrap();
+        match to_app_message {
+            AppServerToApp::Report(report) => assert_eq!(report, initial_node_report),
+            _ => unreachable!(),
+        };
+        let to_app_message = await!(app_receiver1.next()).unwrap();
+        match to_app_message {
+            AppServerToApp::Report(report) => assert_eq!(report, initial_node_report),
+            _ => unreachable!(),
+        };
+
+        // Send a dummy report message from IndexClient:
+        let new_index_server_address = 300u64;
+        let index_client_report_mutation = IndexClientReportMutation::AddIndexServer(new_index_server_address.clone());
+        let index_client_report_mutations = vec![index_client_report_mutation.clone()];
+        await!(index_client_sender.send(IndexClientToAppServer::ReportMutations(index_client_report_mutations))).unwrap();
+
+        // Both apps should get the report:
+        let to_app_message = await!(app_receiver0.next()).unwrap(); 
+        match to_app_message {
+            AppServerToApp::ReportMutations(report_mutations) => {
+                assert_eq!(report_mutations.len(), 1);
+                let report_mutation = &report_mutations[0];
+                match report_mutation {
+                    NodeReportMutation::IndexClient(received_index_client_report_mutation) => {
+                        assert_eq!(received_index_client_report_mutation, 
+                                   &index_client_report_mutation);
+                    },
+                    _ => unreachable!(),
+                }
+            },
+            _ => unreachable!(),
+        }
+        let to_app_message = await!(app_receiver1.next()).unwrap(); 
+        match to_app_message {
+            AppServerToApp::ReportMutations(report_mutations) => {
+                assert_eq!(report_mutations.len(), 1);
+                let report_mutation = &report_mutations[0];
+                match report_mutation {
+                    NodeReportMutation::IndexClient(received_index_client_report_mutation) => {
+                        assert_eq!(received_index_client_report_mutation, 
+                                   &index_client_report_mutation);
+                    },
+                    _ => unreachable!(),
+                }
+            },
+            _ => unreachable!(),
+        }
+    }
+
+    #[test]
+    fn test_app_server_loop_index_two_apps() {
+        let mut thread_pool = ThreadPool::new().unwrap();
+        thread_pool.run(task_app_server_loop_two_apps(thread_pool.clone()));
+    }
+
+    // ----------------------------------------------
+    // ----------------------------------------------
+
+
+    async fn task_app_server_loop_request_routes<S>(mut spawner: S) 
+    where
+        S: Spawn + Clone + Send + 'static,
+    {
+
+        let (mut funder_sender, mut funder_receiver,
+             mut index_client_sender, mut index_client_receiver,
+             mut connections_sender, initial_node_report) = spawn_dummy_app_server(spawner.clone());
+
+        // Connect two apps:
+        let (mut app_sender0, app_server_receiver) = mpsc::channel(0);
+        let (app_server_sender, mut app_receiver0) = mpsc::channel(0);
+        let app_server_conn_pair = (app_server_sender, app_server_receiver);
+        let app_permissions = AppPermissions {
+            reports: true,
+            routes: true,
+            send_funds: true,
+            config: true,
+        };
+        await!(connections_sender.send((app_permissions, app_server_conn_pair))).unwrap();
+
+        let (mut app_sender1, app_server_receiver) = mpsc::channel(0);
+        let (app_server_sender, mut app_receiver1) = mpsc::channel(0);
+        let app_server_conn_pair = (app_server_sender, app_server_receiver);
+        let app_permissions = AppPermissions {
+            reports: true,
+            routes: true,
+            send_funds: true,
+            config: true,
+        };
+        await!(connections_sender.send((app_permissions, app_server_conn_pair))).unwrap();
+
+
+        // The apps should receive the current node report as the first message:
+        let _to_app_message = await!(app_receiver0.next()).unwrap();
+        let _to_app_message = await!(app_receiver1.next()).unwrap();
+
+        // Send a request routes message through app0:
+        let request_routes = RequestRoutes {
+            request_id: Uid::from(&[3; UID_LEN]),
+            capacity: 250,
+            source: PublicKey::from(PublicKey::from(&[0xee; PUBLIC_KEY_LEN])),
+            destination: PublicKey::from(PublicKey::from(&[0xff; PUBLIC_KEY_LEN])),
+            opt_exclude: None,
+        };
+
+        await!(app_sender0.send(AppToAppServer::RequestRoutes(request_routes.clone()))).unwrap();
+
+        // RequestRoutes command should be forwarded to IndexClient:
+        let to_index_client_message = await!(index_client_receiver.next()).unwrap();
+        match to_index_client_message {
+            AppServerToIndexClient::RequestRoutes(received_request_routes) => 
+                assert_eq!(received_request_routes, request_routes),
+            _ => unreachable!(),
+        };
+
+        // IndexClient returns a response that is not related to any open request.
+        // This response will be discarded.
+        let client_response_routes = ClientResponseRoutes {
+            request_id: Uid::from(&[2; UID_LEN]),
+            result: ResponseRoutesResult::Failure,
+        };
+        await!(index_client_sender.send(IndexClientToAppServer::ResponseRoutes(client_response_routes))).unwrap();
+
+        // We shouldn't get an message at any of the apps:
+        assert!(app_receiver0.try_next().is_err());
+        assert!(app_receiver1.try_next().is_err());
+
+        // IndexClient returns a response corresponding to an open request:
+        let client_response_routes = ClientResponseRoutes {
+            request_id: Uid::from(&[3; UID_LEN]),
+            result: ResponseRoutesResult::Failure,
+        };
+        await!(index_client_sender.send(IndexClientToAppServer::ResponseRoutes(client_response_routes))).unwrap();
+
+        let to_app_message = await!(app_receiver0.next()).unwrap();
+        match to_app_message {
+            AppServerToApp::ResponseRoutes(response_routes) => {
+                assert_eq!(response_routes.request_id, Uid::from(&[3; UID_LEN]));
+                assert_eq!(response_routes.result, ResponseRoutesResult::Failure);
+            },
+            _ => unreachable!(),
+        }
+        // We shouldn't get an incoming message at app1:
+        assert!(app_receiver1.try_next().is_err());
+
+        // IndexClient again returns the same response.
+        // This time the response should be discarded, 
+        // because it does not correspond to any open request.
+        let client_response_routes = ClientResponseRoutes {
+            request_id: Uid::from(&[3; UID_LEN]),
+            result: ResponseRoutesResult::Failure,
+        };
+
+        // We shouldn't get an message at any of the apps:
+        assert!(app_receiver0.try_next().is_err());
+        assert!(app_receiver1.try_next().is_err());
+    }
+
+    #[test]
+    fn test_app_server_loop_index_request_routes() {
+        let mut thread_pool = ThreadPool::new().unwrap();
+        thread_pool.run(task_app_server_loop_request_routes(thread_pool.clone()));
     }
 }
