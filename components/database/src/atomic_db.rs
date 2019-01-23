82dbe68a3610de1c6d2861aff69edaf626b19fc0
real <real@freedomlayer.org>
2019-01-23 14:50:18 +0200

    Began implementing a generic database crate.

---
+++ components/database/src/atomic_db.rs
@@ -0,0 +1,18 @@
+
+pub trait MutableState {
+    type Mutation;
+    type MutateError;
+
+    fn mutate(&mut self, mutation: &Self::Mutation) -> Result<(), Self::MutateError>;
+}
+
+/// An atomic database. Allows to batch a list of mutations, and guarantees to apply them to the
+/// database in an atomic manner.
+pub trait AtomicDb {
+    type State;
+    type Mutation;
+    type Error;
+
+    fn get_state(&self) -> &Self::State;
+    fn mutate(&mut self, mutations: &[Self::Mutation]) -> Result<(), Self::Error>;
+}
---
+++ components/database/src/file_db.rs
@@ -0,0 +1,110 @@
+use std::io;
+use std::io::prelude::*;
+use std::string::String;
+use std::fs::File;
+
+use std::fmt::Debug;
+
+use serde::Serialize;
+use serde::de::DeserializeOwned;
+
+use serde_json;
+use atomicwrites;
+
+use crate::atomic_db::{AtomicDb, MutableState};
+
+#[derive(Debug)]
+pub enum FileDbError<ME> {
+    ReadError(io::Error),
+    WriteError(atomicwrites::Error<io::Error>),
+    DeserializeError(serde_json::error::Error),
+    SerializeError(serde_json::error::Error),
+    MutateError(ME),
+}
+
+pub struct FileDbConn {
+    /// DbCore file path
+    db_path: String,
+}
+
+impl FileDbConn {
+    pub fn new(db_path: &str) -> FileDbConn {
+        FileDbConn {
+            db_path: String::from(db_path),
+        }
+    }
+}
+
+#[allow(unused)]
+pub struct FileDb<S> {
+    /// Connection to the database
+    db_conn: FileDbConn,
+    /// Current state represented by the database:
+    state: S,
+}
+
+
+#[allow(unused)]
+impl<S> FileDb<S>
+where
+    S: MutableState + Clone + Serialize + DeserializeOwned,
+{
+    pub fn new(db_conn: FileDbConn) -> Result<Self, FileDbError<S::MutateError>> {
+
+        // TODO: Should create a new funder_state here if does not exist?
+        let mut fr = File::open(&db_conn.db_path)
+            .map_err(FileDbError::ReadError)?;
+
+        let mut serialized_str = String::new();
+        fr.read_to_string(&mut serialized_str)
+            .map_err(FileDbError::ReadError)?;
+
+        let state: S = serde_json::from_str(&serialized_str)
+            .map_err(FileDbError::DeserializeError)?;
+
+        Ok(FileDb {
+            db_conn,
+            state,
+        })
+    }
+}
+
+
+#[allow(unused)]
+impl<S> AtomicDb for FileDb<S> 
+where 
+    S: Clone + Serialize + DeserializeOwned + MutableState,
+    S::Mutation: Clone + Serialize + DeserializeOwned,
+    S::MutateError: Debug,
+{
+    type State = S;
+    type Mutation = S::Mutation;
+    type Error = FileDbError<S::MutateError>;
+
+    /// Get current FunderState represented by the database
+    fn get_state(&self) -> &Self::State {
+        &self.state
+    }
+
+    /// Apply a set of mutations atomically the database, and save it.
+    fn mutate(&mut self, mutations: &[Self::Mutation]) -> Result<(), Self::Error> {
+        // Apply all mutations to state:
+        for mutation in mutations.iter() {
+            self.state.mutate(mutation)
+                .map_err(|mutate_error| FileDbError::MutateError(mutate_error))?;
+        }
+
+        // Serialize the state:
+        let serialized_str = serde_json::to_string(&self.state)
+            .map_err(FileDbError::SerializeError)?;
+
+        // Save the new state to file, atomically:
+        let af = atomicwrites::AtomicFile::new(
+            &self.db_conn.db_path, atomicwrites::AllowOverwrite);
+        af.write(|fw| {
+            fw.write_all(serialized_str.as_bytes())
+        }).map_err(FileDbError::WriteError)?;
+        
+        Ok(())
+    }
+}
