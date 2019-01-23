3e2d00dc567c14a24c3c952ee26bf7ff9b1b9425
real <real@freedomlayer.org>
2019-01-23 19:28:01 +0200

    Removed old database code from offst-funder.

--- components/funder/src/database/mod.rs
+++
@@ -1,9 +0,0 @@
-mod atomic_db;
-mod file_db;
-mod runner;
-// mod client;
-
-pub use self::atomic_db::{AtomicDb};
-pub use self::file_db::{FileDb, FileDbError};
-// pub use self::client::{DbClient, DbClientError};
-pub use self::runner::{DbRunner, DbRunnerError};
