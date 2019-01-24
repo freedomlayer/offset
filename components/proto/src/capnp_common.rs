2db8186e2ce88bdf694a273fe5b9827aac539972
real <real@freedomlayer.org>
2019-01-24 16:07:28 +0200

    Implemented write_relay_address().

--- components/proto/src/capnp_common.rs
+++ components/proto/src/capnp_common.rs
@@ -179,5 +179,22 @@ pub fn read_relay_address(from: &relay_address::Reader) -> Result<RelayAddress, 
 }
 
 pub fn write_relay_address(from: RelayAddress, to: &mut relay_address::Builder) {
-    unimplemented!();
+
+    write_public_key(&from.public_key, &mut to.reborrow().init_public_key());
+    let tcp_address_builder = to.reborrow().init_address();
+
+    match from.address {
+        TcpAddress::V4(tcp_address_v4) => {
+            let mut tcp_address_v4_writer = tcp_address_builder.init_v4();
+            let address_u32 = BigEndian::read_u32(&tcp_address_v4.address);
+            tcp_address_v4_writer.set_port(tcp_address_v4.port);
+            tcp_address_v4_writer.set_address(address_u32);
+        },
+        TcpAddress::V6(tcp_address_v6) => {
+            let mut tcp_address_v6_writer = tcp_address_builder.init_v6();
+            tcp_address_v6_writer.set_port(tcp_address_v6.port);
+            let mut address_builder = tcp_address_v6_writer.init_address();
+            write_buffer128(&tcp_address_v6.address, &mut address_builder);
+        },
+    }
 }
