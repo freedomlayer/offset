fb5e98e3393219a81161afd0e3867ba40426942f
real <real@freedomlayer.org>
2019-01-30 17:33:53 +0200

    Fixed imports. Not done yet.

--- components/net/src/tcp_connector.rs
+++ components/net/src/tcp_connector.rs
@@ -1,15 +1,17 @@
+use common::conn::{FutTransform, BoxFuture, ConnPairVec};
+use proto::funder::messages::TcpAddress;
 
+pub struct TcpConnector;
 
-/*
-use crate::conn::{FutTransform, BoxFuture, ConnPairVec};
-
-
-pub trait FutTransform {
-    type Input;
-    type Output;
+impl FutTransform  for TcpConnector {
+    type Input = TcpAddress;
+    type Output = ConnPairVec;
 
     fn transform(&mut self, input: Self::Input)
-        -> BoxFuture<'_, Self::Output>;
+        -> BoxFuture<'_, Self::Output> {
+
+        // TODO:
+        unimplemented!();
+    }
 }
 
-*/
