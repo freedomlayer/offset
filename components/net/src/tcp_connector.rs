64b3e730bf32b4d6a868cb2ce3298575120eeab9
real <real@freedomlayer.org>
2019-01-30 17:23:58 +0200

    Added offst-net crate.

---
+++ components/net/src/tcp_connector.rs
@@ -0,0 +1,15 @@
+
+
+/*
+use crate::conn::{FutTransform, BoxFuture, ConnPairVec};
+
+
+pub trait FutTransform {
+    type Input;
+    type Output;
+
+    fn transform(&mut self, input: Self::Input)
+        -> BoxFuture<'_, Self::Output>;
+}
+
+*/
