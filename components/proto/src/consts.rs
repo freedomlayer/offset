201f8464242a5b52317c7220c413f065625040ca
real <real@freedomlayer.org>
2019-02-01 17:00:28 +0200

    Added some constants to proto/src/consts.rs.

--- components/proto/src/consts.rs
+++ components/proto/src/consts.rs
@@ -1,3 +1,6 @@
+/// The current protocol version
+pub const PROTOCOL_VERSION: u32 = 0;
+
 /// Maximum amount of friend operations sent in one move token message.
 pub const MAX_OPERATIONS_IN_BATCH: usize = 16;
 
@@ -16,3 +19,7 @@ pub const KEEPALIVE_TICKS: usize = 8;
 /// Relay server: The amount of ticks to wait before a relay connection from a client
 /// sends identification of which type of connection it is.
 pub const CONN_TIMEOUT_TICKS: usize = 4;
+
+/// The stream TCP connection is split into prefix length frames. This is the maximum allowed
+/// length for such frame, measured in bytes.
+pub const MAX_FRAME_LENGTH: usize = 1 << 20; // 1[MB]
