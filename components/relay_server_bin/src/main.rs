95f483b7499eaa34ef6c6600cf22148336236e50
real <real@freedomlayer.org>
2019-02-01 15:21:54 +0200

    Added some code to relay-server-bin's main. Not done yet.

--- components/relay_server_bin/src/main.rs
+++ components/relay_server_bin/src/main.rs
@@ -4,11 +4,27 @@
 #![feature(generators)]
 #![feature(never_type)]
 
-
-// #[macro_use]
+#[macro_use]
 extern crate log;
 
+use futures::executor::ThreadPool;
+use relay::{relay_server, RelayServerError};
 
 fn main() {
+    let thread_pool = match ThreadPool::new() {
+        Ok(thread_pool) => thread_pool,
+        Err(_) => {
+            error!("Could not create a ThreadPool! Aborting.");
+            return;
+        },
+    };
+
+    /*
+    relay_server(mut timer_client: TimerClient, 
+                incoming_conns: S,
+                keepalive_ticks: usize,
+                mut spawner: impl Spawn + Clone) -> Result<(), RelayServerError> 
+    */
+
     println!("Hello world!");
 }
