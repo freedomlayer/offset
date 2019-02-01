a8fd36679159f7ec2f3b1476ee542ee0f74f0f98
real <real@freedomlayer.org>
2019-02-01 14:58:47 +0200

    Added an empty relay_server_bin crate.

---
+++ components/relay_server_bin/src/lib.rs
@@ -0,0 +1,11 @@
+#![crate_type = "lib"] 
+#![feature(futures_api, async_await, await_macro, arbitrary_self_types)]
+#![feature(nll)]
+#![feature(try_from)]
+#![feature(generators)]
+#![feature(never_type)]
+
+
+// #[macro_use]
+extern crate log;
+
