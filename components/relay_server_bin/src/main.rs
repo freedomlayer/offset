dccd65be34a5fb6fe616a5571eb60ee645f69c07
real <real@freedomlayer.org>
2019-02-01 16:06:51 +0200

    Some work on relay-server-bin. Not done yet.

--- components/relay_server_bin/src/main.rs
+++ components/relay_server_bin/src/main.rs
@@ -7,8 +7,15 @@
 #[macro_use]
 extern crate log;
 
+use std::time::{Duration, Instant};
 use futures::executor::ThreadPool;
+
+use common::int_convert::usize_to_u64;
+use timer::create_timer;
 use relay::{relay_server, RelayServerError};
+use net::TcpListener;
+use proto::consts::{TICK_MS, KEEPALIVE_TICKS};
+
 
 fn main() {
     let thread_pool = match ThreadPool::new() {
@@ -19,12 +26,24 @@ fn main() {
         },
     };
 
+    let dur = Duration::from_millis(usize_to_u64(TICK_MS).unwrap()); 
+    let timer_client = match create_timer(dur, thread_pool.clone()) {
+        Ok(timer_client) => timer_client,
+        Err(_) => {
+            error!("Failed to create timer! Aborting.");
+            return;
+        }
+    };
+
+
     /*
-    relay_server(mut timer_client: TimerClient, 
-                incoming_conns: S,
-                keepalive_ticks: usize,
-                mut spawner: impl Spawn + Clone) -> Result<(), RelayServerError> 
+    relay_server(incoming_conns,
+                timer_client,
+                conn_timeout_ticks,
+                keepalive_ticks,
+                thread_pool);
     */
+
 
     println!("Hello world!");
 }
