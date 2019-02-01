7578737f5d78187181b33b6e72a5803017978373
real <real@freedomlayer.org>
2019-02-01 16:06:32 +0200

    Added some protocol constants.

--- components/proto/src/consts.rs
+++ components/proto/src/consts.rs
@@ -4,5 +4,15 @@ pub const MAX_OPERATIONS_IN_BATCH: usize = 16;
 /// Maximum length of route used to pass credit.
 pub const MAX_ROUTE_LEN: usize = 32;
 
+/// Amount of milliseconds in one tick:
+pub const TICK_MS: usize = 1000;
+
 /// Amount of ticks to wait before rekeying a secure channel.
-pub const TICKS_TO_REKEY: usize = 60 * 60 * 2; // 1 hour given that TICK is half a second.
+pub const TICKS_TO_REKEY: usize = 60 * 60 * (1000 / TICK_MS); // 1 hour
+
+/// If no message was sent for this amount of ticks, the connection will be closed
+pub const KEEPALIVE_TICKS: usize = 8;
+
+/// Relay server: The amount of ticks to wait before a relay connection from a client
+/// sends identification of which type of connection it is.
+pub const CONN_TIMEOUT_TICKS: usize = 4;
