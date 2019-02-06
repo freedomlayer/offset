4eda97d58a722c44a3e6943ad9e3a75a3be373f8
real <real@freedomlayer.org>
2019-02-06 15:09:39 +0200

    Some work on AppConnTransform. Not done yet.

--- components/bin/src/bin/node.rs
+++ components/bin/src/bin/node.rs
@@ -7,12 +7,14 @@
 #[macro_use]
 extern crate log;
 
+use std::collections::HashMap;
+
 use std::net::SocketAddr;
 use std::path::Path;
 use std::time::Duration;
 
 use futures::executor::ThreadPool;
-use futures::task::SpawnExt;
+use futures::task::{Spawn, SpawnExt};
 use futures::channel::mpsc;
 use futures::{FutureExt, TryFutureExt};
 
@@ -20,14 +22,22 @@ use clap::{Arg, App};
 use log::Level;
 
 use common::int_convert::usize_to_u64;
-use common::conn::{FutTransform, FuncFutTransform, Listener};
+use common::conn::{FutTransform, FuncFutTransform, 
+    Listener, BoxFuture, ConnPairVec};
+use common::transform_pool::transform_pool_loop;
+
 use crypto::identity::{Identity, PublicKey};
-use identity::{create_identity, IdentityClient};
-use timer::create_timer;
-use version::VersionPrefix;
 use crypto::crypto_rand::system_random;
 
-use node::{node, NodeConfig, NodeState, NodeError};
+use identity::{create_identity, IdentityClient};
+use timer::create_timer;
+
+use version::VersionPrefix;
+use secure_channel::SecureChannel;
+use keepalive::KeepAliveChannel;
+
+use node::{node, NodeConfig, NodeState, 
+    NodeError, IncomingAppConnection, AppPermissions};
 
 use database::file_db::FileDb;
 use database::{database_loop, DatabaseClient, AtomicDb};
@@ -38,7 +48,8 @@ use proto::index_server::messages::IndexServerAddress;
 use proto::funder::messages::RelayAddress;
 use net::{TcpConnector, TcpListener, socket_addr_to_tcp_address};
 
-use bin::load_identity_from_file;
+
+use bin::{load_identity_from_file, load_trusted_apps};
 
 /// Memory allocated to a channel in memory (Used to connect two components)
 const CHANNEL_LEN: usize = 0x20;
@@ -46,7 +57,7 @@ const CHANNEL_LEN: usize = 0x20;
 const BACKOFF_TICKS: usize = 0x8;
 /// Maximum amount of encryption set ups (diffie hellman) that we allow to occur at the same
 /// time.
-const MAX_CONCURREN_ENCRYPT: usize = 0x8;
+const MAX_CONCURRENT_ENCRYPT: usize = 0x8;
 /// The size we allocate for the user send funds requests queue.
 const MAX_PENDING_USER_REQUESTS: usize = 0x20;
 /// Maximum amount of concurrent index client requests:
@@ -54,12 +65,16 @@ const MAX_OPEN_INDEX_CLIENT_REQUESTS: usize = 0x8;
 /// The amount of ticks we are willing to wait until a connection is established (Through
 /// the relay)
 const CONN_TIMEOUT_TICKS: usize = 0x8;
+/// Maximum amount of concurrent applications
+/// going through the incoming connection transform at the same time
+const MAX_CONCURRENT_INCOMING_APP: usize = 0x8;
 
 
 #[derive(Debug)]
 enum NodeBinError {
     ParseListenAddressError,
     LoadIdentityError,
+    LoadTrustedAppsError,
     CreateThreadPoolError,
     CreateIdentityError,
     CreateTimerError,
@@ -68,6 +83,90 @@ enum NodeBinError {
     DatabaseIdentityMismatch,
     NodeError(NodeError),
 }
+
+#[derive(Clone)]
+struct AppConnTransform<VT,ET,KT,S,GT> {
+    version_transform: VT, 
+    encrypt_transform: ET, 
+    keepalive_transform: KT,
+    spawner: S,
+    get_trusted_apps: GT,
+    thread_pool: ThreadPool,
+}
+
+impl<VT,ET,KT,S,GT> AppConnTransform<VT,ET,KT,S,GT> {
+    pub fn new(version_transform: VT,
+               encrypt_transform: ET,
+               keepalive_transform: KT,
+               spawner: S,
+               get_trusted_apps: GT) -> Result<Self, NodeBinError> {
+
+        // Create a extra inner thread pool.
+        // We use another thread pool because we don't want to block
+        // the main thread pool when we are reading files from the filesystem.
+        // TODO: What is the idiomatic way to do this without having an extra ThreadPool?
+        let thread_pool = ThreadPool::new()
+            .map_err(|_| NodeBinError::CreateThreadPoolError)?;
+
+        Ok(AppConnTransform {
+            version_transform, 
+            encrypt_transform, 
+            keepalive_transform,
+            spawner,
+            get_trusted_apps,
+            thread_pool,
+        })
+    }
+}
+
+impl<VT,ET,KT,S,GT> FutTransform for AppConnTransform<VT,ET,KT,S,GT> 
+where
+    VT: FutTransform<Input=ConnPairVec, Output=ConnPairVec> + Clone + Send,
+    ET: FutTransform<Input=(Option<PublicKey>, ConnPairVec),
+                     Output=Option<(PublicKey, ConnPairVec)>> + Clone + Send,
+    KT: FutTransform<Input=ConnPairVec, Output=ConnPairVec> + Clone + Send,
+    S: Spawn + Clone + Send,
+    GT: Fn() -> Result<HashMap<PublicKey, AppPermissions>, NodeBinError> + Clone + Send,
+{
+    type Input = ConnPairVec;
+    type Output = Option<IncomingAppConnection<RelayAddress,IndexServerAddress>>;
+
+    fn transform(&mut self, conn_pair: Self::Input)
+        -> BoxFuture<'_, Self::Output> {
+        
+        // TODO: 
+        // - TcpListener to get incoming apps
+        // - Configuration directory for trusted apps? For each app:
+        //      - Public key
+        //      - permissions
+        //
+        // - For each incoming connection:
+        //      - version prefix
+        //      - encrypt (Checking against trusted apps list 
+        //              + Obtain permissions)
+        //      - keepalive
+        //      - serialization
+
+        Box::pin(async move {
+            // Version perfix:
+            let ver_conn = await!(self.version_transform.transform(conn_pair));
+            // Encrypt:
+            let (public_key, enc_conn) = await!(self.encrypt_transform.transform((None, ver_conn)))?;
+
+            // Obtain permissions for app (Or reject it if not trusted):
+            let trusted_apps = (self.get_trusted_apps)().ok()?;
+            let app_permissions = trusted_apps.get(&public_key)?;
+
+            // Keepalive wrapper:
+            let ka_conn = await!(self.keepalive_transform.transform(enc_conn));
+
+            // TODO: serializationn
+
+            unimplemented!();
+        })
+    }
+}
+
 
 fn run() -> Result<(), NodeBinError> {
     simple_logger::init_with_level(Level::Warn).unwrap();
@@ -93,7 +192,14 @@ fn run() -> Result<(), NodeBinError> {
                                .value_name("laddr")
                                .help("Listening address. \nExamples:\n- 0.0.0.0:1337\n- fe80::14c2:3048:b1ac:85fb:1337")
                                .required(true))
+                          .arg(Arg::with_name("trusted")
+                               .short("t")
+                               .long("trusted")
+                               .value_name("trusted")
+                               .help("Directory path of trusted applications")
+                               .required(true))
                           .get_matches();
+
 
     // Parse listening address
     let listen_address_str = matches.value_of("laddr").unwrap();
@@ -134,7 +240,7 @@ fn run() -> Result<(), NodeBinError> {
         ticks_to_rekey: TICKS_TO_REKEY,
         /// Maximum amount of encryption set ups (diffie hellman) that we allow to occur at the same
         /// time.
-        max_concurrent_encrypt: MAX_CONCURREN_ENCRYPT,
+        max_concurrent_encrypt: MAX_CONCURRENT_ENCRYPT,
         /// The amount of ticks we are willing to wait until a connection is established (Through
         /// the relay)
         conn_timeout_ticks: CONN_TIMEOUT_TICKS,
@@ -152,9 +258,10 @@ fn run() -> Result<(), NodeBinError> {
     // Wrap net connector with a version prefix:
     let version_transform = VersionPrefix::new(PROTOCOL_VERSION, 
                                                    thread_pool.clone());
+    let c_version_transform = version_transform.clone();
     let version_connector = FuncFutTransform::new(move |address| {
         let mut c_net_connector = net_connector.clone();
-        let mut c_version_transform = version_transform.clone();
+        let mut c_version_transform = c_version_transform.clone();
         Box::pin(async move {
             let conn_pair = await!(c_net_connector.transform(address))?;
             Some(await!(c_version_transform.transform(conn_pair)))
@@ -193,21 +300,53 @@ fn run() -> Result<(), NodeBinError> {
     let app_tcp_listener = TcpListener::new(MAX_FRAME_LENGTH, thread_pool.clone());
     let (_config_sender, incoming_app_raw_conns) = app_tcp_listener.listen(listen_tcp_address);
 
+    let mut encrypt_transform = SecureChannel::new(
+        identity_client.clone(),
+        rng.clone(),
+        timer_client.clone(),
+        TICKS_TO_REKEY,
+        thread_pool.clone());
 
-    // TODO: 
-    // - TcpListener to get incoming apps
-    // - Configuration directory for trusted apps? For each app:
-    //      - Public key
-    //      - permissions
-    //
-    // - For each incoming connection:
-    //      - version prefix
-    //      - encrypt (Checking against trusted apps list 
-    //              + Obtain permissions)
-    //      - keepalive
-    //      - serialization
+    let mut keepalive_transform = KeepAliveChannel::new(
+        timer_client.clone(),
+        KEEPALIVE_TICKS,
+        thread_pool.clone());
+
+    // Create a closure for loading trusted apps map:
+    let trusted_dir_path = Path::new(matches.value_of("trusted").unwrap()).to_path_buf();
+    // TODO: We need a more detailed error here.
+    // It might be hard for the user to detect in which file there was a problem
+    // in case of an error.
+    let get_trusted_apps = move || -> Result<_, NodeBinError> {
+        Ok(load_trusted_apps(&trusted_dir_path)
+        .map_err(|_| NodeBinError::LoadTrustedAppsError)?
+        .into_iter()
+        .map(|trusted_app| (trusted_app.public_key, trusted_app.permissions))
+        .collect::<HashMap<_,_>>())
+    };
+
+    let app_conn_transform = AppConnTransform::new(
+        version_transform,
+        encrypt_transform,
+        keepalive_transform,
+        thread_pool.clone(),
+        get_trusted_apps)?;
+
+    let (incoming_apps_sender, incoming_apps) = mpsc::channel(0);
+
+    // Apply transform over every incoming app connection:
+    let pool_fut = transform_pool_loop(
+            incoming_app_raw_conns,
+            incoming_apps_sender,
+            app_conn_transform,
+            MAX_CONCURRENT_INCOMING_APP,
+            thread_pool.clone())
+        .map_err(|e| error!("transform_pool_loop() error: {:?}", e))
+        .map(|_| ());
+
+    thread_pool.spawn(pool_fut)
+        .map_err(|_| NodeBinError::SpawnError)?;
     
-    /*
     let node_fut = node(
         node_config,
         identity_client,
@@ -215,14 +354,12 @@ fn run() -> Result<(), NodeBinError> {
         node_state,
         database_client,
         version_connector,
-        incoming_apps: IA,
+        incoming_apps,
         rng,
         thread_pool.clone());
 
     thread_pool.run(node_fut)
         .map_err(|e| NodeBinError::NodeError(e))
-    */
-    unimplemented!();
 }
 
 fn main() {
