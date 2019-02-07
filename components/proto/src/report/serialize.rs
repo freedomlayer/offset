ef45294d3c0b6e5a0ef3614b1366674ef93813fb
real <real@freedomlayer.org>
2019-02-07 13:12:48 +0200

    Updated serialization code for MoveTokenHashedReport.

--- components/proto/src/report/serialize.rs
+++ components/proto/src/report/serialize.rs
@@ -11,59 +11,61 @@ use crate::capnp_common::{write_signature, read_signature,
                           write_public_key, read_public_key,
                           write_relay_address, read_relay_address,
                           write_index_server_address, read_index_server_address,
-                          write_receipt, read_receipt};
+                          write_receipt, read_receipt,
+                          write_hash, read_hash};
 
 use report_capnp;
 use crate::serialize::SerializeError;
-use crate::report::messages::MoveTokenHashedReport;
+use crate::report::messages::{MoveTokenHashedReport, FriendStatusReport, RequestsStatusReport,
+                            FriendLivenessReport, DirectionReport};
 
 
-fn ser_move_token_hashed(move_token_hashed_report: &MoveTokenHashedReport,
-                    move_token_hashed_builder: &mut app_server_capnp::reset_friend_channel::Builder) {
+fn ser_move_token_hashed_report(move_token_hashed_report: &MoveTokenHashedReport,
+                    move_token_hashed_report_builder: &mut report_capnp::move_token_hashed_report::Builder) {
 
     write_hash(&move_token_hashed_report.prefix_hash,
-        &mut move_token_hashed_builder.reborrow().init_prefix_hash());
+        &mut move_token_hashed_report_builder.reborrow().init_prefix_hash());
 
     write_public_key(&move_token_hashed_report.local_public_key,
-        &mut move_token_hashed_builder.reborrow().init_local_public_key());
+        &mut move_token_hashed_report_builder.reborrow().init_local_public_key());
 
     write_public_key(&move_token_hashed_report.remote_public_key,
-        &mut move_token_hashed_builder.reborrow().init_remote_public_key());
+        &mut move_token_hashed_report_builder.reborrow().init_remote_public_key());
 
-    move_token_hashed_builder.reborrow().set_inconsistency_counter(move_token_hashed_report.inconsistency_counter);
+    move_token_hashed_report_builder.reborrow().set_inconsistency_counter(move_token_hashed_report.inconsistency_counter);
 
-    write_custom_u_int128(&move_token_hashed_report.move_token_counter,
-        &mut move_token_hashed_builder.reborrow().init_move_token_counter());
+    write_custom_u_int128(move_token_hashed_report.move_token_counter,
+        &mut move_token_hashed_report_builder.reborrow().init_move_token_counter());
 
-    write_custom_int128(&move_token_hashed_report.balance,
-        &mut move_token_hashed_builder.reborrow().init_balance());
+    write_custom_int128(move_token_hashed_report.balance,
+        &mut move_token_hashed_report_builder.reborrow().init_balance());
 
-    write_custom_u_int128(&move_token_hashed_report.local_pending_debt,
-        &mut move_token_hashed_builder.reborrow().init_local_pending_debt());
+    write_custom_u_int128(move_token_hashed_report.local_pending_debt,
+        &mut move_token_hashed_report_builder.reborrow().init_local_pending_debt());
 
-    write_custom_u_int128(&move_token_hashed_report.remote_pending_debt,
-        &mut move_token_hashed_builder.reborrow().init_remote_pending_debt());
+    write_custom_u_int128(move_token_hashed_report.remote_pending_debt,
+        &mut move_token_hashed_report_builder.reborrow().init_remote_pending_debt());
 
     write_rand_nonce(&move_token_hashed_report.rand_nonce,
-        &mut move_token_hashed_builder.reborrow().init_rand_nonce());
+        &mut move_token_hashed_report_builder.reborrow().init_rand_nonce());
 
     write_signature(&move_token_hashed_report.new_token,
-        &mut move_token_hashed_builder.reborrow().init_new_token());
+        &mut move_token_hashed_report_builder.reborrow().init_new_token());
 }
 
-fn deser_move_token_hahsed(move_token_hashed_reader: &app_server_capnp::reset_friend_channel::Reader)
+fn deser_move_token_hashed_report(move_token_hashed_report_reader: &report_capnp::move_token_hashed_report::Reader)
     -> Result<MoveTokenHashedReport, SerializeError> {
 
     Ok(MoveTokenHashedReport {
-        prefix_hash: read_hash(&move_token_hashed_reader.get_prefix_hash()?)?,
-        local_public_key: read_public_key(&move_token_hashed_reader.get_local_public_key()?)?,
-        remote_public_key: read_public_key(&move_token_hashed_reader.get_remote_public_key()?)?,
-        inconsistency_counter: move_token_hashed_reader.get_inconsistency_counter()?,
-        move_token_counter: read_custom_u_int128(&move_token_hashed_reader.get_move_token_counter()?)?,
-        balance: read_custom_int128(&move_token_hashed_reader.get_balance()?)?,
-        local_pending_debt: read_custom_u_int128(&move_token_hashed_reader.get_local_pending_debt()?)?,
-        remote_pending_debt: read_custom_u_int128(&move_token_hashed_reader.get_remote_pending_debt()?)?,
-        rand_nonce: read_rand_nonce(&move_token_hashed_reader.get_rand_nonce()?)?,
-        new_token: read_signature(&move_token_hashed_reader.get_new_token()?)?,
+        prefix_hash: read_hash(&move_token_hashed_report_reader.get_prefix_hash()?)?,
+        local_public_key: read_public_key(&move_token_hashed_report_reader.get_local_public_key()?)?,
+        remote_public_key: read_public_key(&move_token_hashed_report_reader.get_remote_public_key()?)?,
+        inconsistency_counter: move_token_hashed_report_reader.get_inconsistency_counter(),
+        move_token_counter: read_custom_u_int128(&move_token_hashed_report_reader.get_move_token_counter()?)?,
+        balance: read_custom_int128(&move_token_hashed_report_reader.get_balance()?)?,
+        local_pending_debt: read_custom_u_int128(&move_token_hashed_report_reader.get_local_pending_debt()?)?,
+        remote_pending_debt: read_custom_u_int128(&move_token_hashed_report_reader.get_remote_pending_debt()?)?,
+        rand_nonce: read_rand_nonce(&move_token_hashed_report_reader.get_rand_nonce()?)?,
+        new_token: read_signature(&move_token_hashed_report_reader.get_new_token()?)?,
     })
 }
