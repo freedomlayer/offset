911d0e8d7732555dee270cac9edf3447d10c40e4
real <real@freedomlayer.org>
2019-02-14 14:12:54 +0200

    Exporting NetAddress.

--- components/app/src/proto.rs
+++ components/app/src/proto.rs
@@ -1,4 +1,5 @@
 // TODO: We might need to export more types here.
 // What is the clean way to do this?
+pub use proto::net::messages::NetAddress;
 pub use proto::app_server::messages::{AppToAppServer, AppServerToApp, 
     NodeReport, NodeReportMutation};
