d59763ab0d9c5f0743e016f19a254637066a5254
real <real@freedomlayer.org>
2019-02-15 17:28:06 +0200

    Some work on AppConfig interface.

--- components/app/src/node_connection/config.rs
+++ components/app/src/node_connection/config.rs
@@ -1,6 +1,99 @@
 use futures::channel::mpsc;
-use proto::app_server::messages::{AppToAppServer};
+use crypto::identity::{PublicKey, Signature};
+use proto::app_server::messages::{AppToAppServer, RelayAddress, NamedRelayAddress};
 
 pub struct AppConfig {
     sender: mpsc::Sender<AppToAppServer>,
 }
+
+impl AppConfig {
+    pub fn new(sender: mpsc::Sender<AppToAppServer>) -> Self {
+        AppConfig {
+            sender,
+        }
+    }
+    /*
+    /// Set relay address to be used locally:
+    SetRelays(NRA),
+    /// Sending funds:
+    RequestSendFunds(UserRequestSendFunds),
+    ReceiptAck(ReceiptAck),
+    /// Friend management:
+    AddFriend(AddFriend<RA>),
+    SetFriendRelays(SetFriendAddress<RA>),
+    SetFriendName(SetFriendName),
+    RemoveFriend(PublicKey),
+    EnableFriend(PublicKey),
+    DisableFriend(PublicKey),
+    OpenFriend(PublicKey),
+    CloseFriend(PublicKey),
+    SetFriendRemoteMaxDebt(SetFriendRemoteMaxDebt),
+    ResetFriendChannel(ResetFriendChannel),
+    /// Request routes from one node to another:
+    RequestRoutes(RequestRoutes),
+    /// Manage index servers:
+    AddIndexServer(AddIndexServer<ISA>),
+    RemoveIndexServer(PublicKey),
+    */
+
+    pub async fn set_relays(named_relays: Vec<NamedRelayAddress>) {
+        unimplemented!();
+    }
+
+    pub async fn add_friend(friend_public_key: PublicKey,
+                            relays: Vec<RelayAddress>,
+                            name: String,
+                            balance: i128) {
+        unimplemented!();
+    }
+
+    pub async fn set_friend_relays(relays: Vec<RelayAddress>) {
+        unimplemented!();
+    }
+
+    pub async fn remove_friend(friend_public_key: PublicKey) {
+        unimplemented!();
+    }
+
+    pub async fn enable_friend(friend_public_key: PublicKey) {
+        unimplemented!();
+    }
+
+    pub async fn disable_friend(friend_public_key: PublicKey) {
+        unimplemented!();
+    }
+
+    pub async fn open_friend(friend_public_key: PublicKey) {
+        unimplemented!();
+    }
+
+    pub async fn close_friend(friend_public_key: PublicKey) {
+        unimplemented!();
+    }
+
+    pub async fn set_friend_remote_max_debt(friend_public_key: PublicKey,
+                                            remote_max_debt: u128) {
+        unimplemented!();
+    }
+
+    pub async fn reset_friend_channel(friend_public_key: PublicKey,
+                                      reset_token: Signature) {
+        unimplemented!();
+    }
+
+    /*
+    pub public_key: PublicKey,
+    pub address: ISA,
+    pub name: String,
+    */
+
+    // TODO:
+    pub async fn add_index_server() {
+        unimplemented!();
+    }
+
+    pub async fn remove_index_server(index_public_key: PublicKey) {
+        unimplemented!();
+    }
+}
+
