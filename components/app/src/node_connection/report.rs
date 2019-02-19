c79e17dacfd29ad9743ce4cf96e6fe099bc9a165
real <real@freedomlayer.org>
2019-02-19 17:02:32 +0200

    Added outline for AppReport.

--- components/app/src/node_connection/report.rs
+++ components/app/src/node_connection/report.rs
@@ -1,4 +1,10 @@
+use common::mutable_state::BatchMutable;
+use common::state_service::StateClient;
+use proto::app_server::messages::{NodeReport, NodeReportMutation};
 
 #[derive(Clone)]
 pub struct AppReport {
+    report_client: StateClient<BatchMutable<NodeReport>,Vec<NodeReportMutation>>,
 }
+
+
