2603f9e474dcb94f4fc995414257e4d22148e3e2
real <real@freedomlayer.org>
2019-03-12 20:12:40 +0200

    Added exports to offst-app.

--- components/app/src/lib.rs
+++ components/app/src/lib.rs
@@ -13,5 +13,31 @@
 #[macro_use]
 extern crate log;
 
-pub mod identity;
-pub mod connect;
+mod identity;
+mod connect;
+pub mod uid;
+
+pub use self::identity::{identity_from_file, IdentityFromFileError};
+pub use self::connect::{connect, ConnectError};
+pub use node::connect::{AppReport, AppConfig, 
+    AppRoutes, AppSendFunds};
+pub use proto::app_server::messages::AppPermissions;
+
+
+// TODO: Possibly reduce what we export from report in the future?
+pub mod report {
+    pub use proto::report::messages::{ChannelStatusReport,
+        MoveTokenHashedReport, SentLocalRelaysReport,
+        FriendStatusReport, RequestsStatusReport, McRequestsStatusReport,
+        McBalanceReport, DirectionReport, FriendLivenessReport, TcReport,
+        ResetTermsReport, ChannelInconsistentReport, FriendReport, FunderReport,
+        FriendReportMutation, AddFriendReport, FunderReportMutation, 
+        FunderReportMutations, FunderReportMutateError};
+
+    use proto::app_server::messages::{NodeReport, NodeReportMutation};
+    use proto::index_client::messages::{IndexClientReport, AddIndexServer, IndexClientReportMutation};
+}
+
+pub mod invoice {
+    pub use proto::funder::messages::{InvoiceId, INVOICE_ID_LEN};
+}
---
+++ components/app/src/uid.rs
@@ -0,0 +1,15 @@
+use crypto::crypto_rand::{system_random};
+use crypto::uid::Uid;
+pub use crypto::uid::UID_LEN;
+
+use node::connect::{node_connect, NodeConnection};
+
+/// Generate a random uid
+pub fn gen_uid() -> Uid {
+    // TODO: Is it safe to create a new `system_random` every time?
+    // Obtain secure cryptographic random:
+    let rng = system_random();
+
+    Uid::new(&rng)
+}
+ 
