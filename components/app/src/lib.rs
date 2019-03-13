8c43e6f9acc83da8c4ede7113be441d3a551d85c
real <real@freedomlayer.org>
2019-03-13 13:10:03 +0200

    Exporting load_node_from_file at offst-app.

--- components/app/src/lib.rs
+++ components/app/src/lib.rs
@@ -17,6 +17,7 @@ mod identity;
 mod connect;
 pub mod uid;
 
+pub use proto::file::node::load_node_from_file;
 pub use self::identity::{identity_from_file, IdentityFromFileError};
 pub use self::connect::{connect, ConnectError};
 pub use node::connect::{AppReport, AppConfig, 
