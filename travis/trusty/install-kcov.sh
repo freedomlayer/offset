38fd95b9e4fc66317f8d67ba0bf3b6bfdfd75338
Paweł Zmarzły <pawo2500@gmail.com>
2019-04-08 23:25:15 +0200

    Don't install kcov build deps when not needed

--- travis/trusty/install-kcov.sh
+++ travis/trusty/install-kcov.sh
@@ -5,10 +5,6 @@ set -ex
 KCOV_INSTALL_PREFIX="${HOME}/kcov-${TARGET}"
 KCOV_MINIMUM_REQUIRED=${KCOV_MINIMUM_REQUIRED:-34}
 
-# https://github.com/SimonKagstrom/kcov/blob/master/INSTALL.md
-sudo apt-get install -y cmake binutils-dev libcurl4-openssl-dev \
-                        zlib1g-dev libdw-dev libiberty-dev
-
 if [[ -f "$KCOV_INSTALL_PREFIX/bin/kcov" ]]; then
     KCOV_INSTALLED_VERSION=$(${KCOV_INSTALL_PREFIX}/bin/kcov --version)
     KCOV_INSTALLED_VERSION=${KCOV_INSTALLED_VERSION#*\ }
@@ -20,6 +16,10 @@ if [[ -f "$KCOV_INSTALL_PREFIX/bin/kcov" ]]; then
        rm -rf "$KCOV_INSTALL_PREFIX/bin/kcov"
     fi
 fi
+
+# https://github.com/SimonKagstrom/kcov/blob/master/INSTALL.md
+sudo apt-get install -y cmake binutils-dev libcurl4-openssl-dev \
+                        zlib1g-dev libdw-dev libiberty-dev
 
 curl -L https://github.com/SimonKagstrom/kcov/archive/v${KCOV_MINIMUM_REQUIRED}.tar.gz | tar -zxf -
 
