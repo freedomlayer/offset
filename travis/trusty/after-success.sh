997b41b6ac2214c095ba5f46fddf2e72b0be88d3
Paweł Zmarzły <pawo2500@gmail.com>
2019-04-08 22:57:35 +0200

    Add cargo-sweep to Travis

--- travis/trusty/after-success.sh
+++ travis/trusty/after-success.sh
@@ -20,3 +20,9 @@ if [[ "$KCOV" == "1" ]]; then
     # Automatically reads from CODECOV_TOKEN environment variable:
     bash <(curl -s https://codecov.io/bash)
 fi
+
+# Install cargo-sweep. If installed, cargo failure will be ignored.
+cargo install cargo-sweep --vers 0.4.1 || true
+
+# cargo-sweep produces a lot of output
+cargo sweep . -t 30 > /dev/null
