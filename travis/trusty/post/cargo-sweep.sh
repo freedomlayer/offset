435aac6fc385ff6c9e660dac6eb08c6fcf125bac
Paweł Zmarzły <pawo2500@gmail.com>
2019-04-09 16:26:33 +0200

    Add toolchain version printing to CI

--- travis/trusty/post/cargo-sweep.sh
+++ travis/trusty/post/cargo-sweep.sh
@@ -5,5 +5,7 @@
 #       maybe use cargo-update library for that
 cargo install cargo-sweep --vers 0.4.1 || true
 
+echo Rust toolchain version: $TRAVIS_RUST_VERSION
+
 # cargo-sweep produces a lot of output
 cargo sweep --toolchains $TRAVIS_RUST_VERSION > /dev/null
