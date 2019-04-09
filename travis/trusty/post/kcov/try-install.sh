c54d539d0d7cea9f1516056e3fa5140f9245d31d
Paweł Zmarzły <pawo2500@gmail.com>
2019-04-09 12:48:54 +0200

    Install kcov runtime dependency in CI

--- travis/trusty/post/kcov/try-install.sh
+++ travis/trusty/post/kcov/try-install.sh
@@ -5,6 +5,8 @@ set -ex
 KCOV_INSTALL_PREFIX="${HOME}/kcov-${TARGET}"
 KCOV_MINIMUM_REQUIRED=${KCOV_MINIMUM_REQUIRED:-34}
 
+sudo apt-get install -y libdw-dev
+
 if [[ -f "$KCOV_INSTALL_PREFIX/bin/kcov" ]]; then
     KCOV_INSTALLED_VERSION=$(${KCOV_INSTALL_PREFIX}/bin/kcov --version)
     KCOV_INSTALLED_VERSION=${KCOV_INSTALLED_VERSION#*\ }
@@ -19,7 +21,7 @@ fi
 
 # https://github.com/SimonKagstrom/kcov/blob/master/INSTALL.md
 sudo apt-get install -y cmake binutils-dev libcurl4-openssl-dev \
-                        zlib1g-dev libdw-dev libiberty-dev
+                        zlib1g-dev libiberty-dev
 
 curl -L https://github.com/SimonKagstrom/kcov/archive/v${KCOV_MINIMUM_REQUIRED}.tar.gz | tar -zxf -
 
