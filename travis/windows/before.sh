b33a805e5326c104438d08d3491fe4fdf39294bf
real <real@freedomlayer.org>
2019-04-16 18:27:23 +0300

    Inlined before.sh into before_script:

--- travis/windows/before.sh
+++ travis/windows/before.sh
@@ -7,5 +7,3 @@ mkdir windows_build_tools
 
 # Download yasm yasm:
 curl -o windows_build_tools/yasm.exe https://www.tortall.net/projects/yasm/releases/yasm-1.3.0-win64.exe
-
-# set PATH=windows_build_tools;%PATH%
