85706a3b131f2f6f63a16cb618a0d1d55575f86a
real <real@freedomlayer.org>
2019-05-14 19:20:24 +0300

    Adaptation to new futures 0.3 interface.

--- components/bin/src/lib.rs
+++ components/bin/src/lib.rs
@@ -1,8 +1,8 @@
-#![feature(futures_api, async_await, await_macro, arbitrary_self_types)]
+#![feature(async_await, await_macro, arbitrary_self_types)]
 #![feature(nll)]
 #![feature(generators)]
 #![feature(never_type)]
-#![type_length_limit = "3770813"]
+#![type_length_limit = "4624323"]
 #![deny(trivial_numeric_casts, warnings)]
 #![allow(intra_doc_link_resolution_failure)]
 #![allow(
