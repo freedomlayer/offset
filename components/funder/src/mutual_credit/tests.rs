a67d118271712bf86fc192562d4b68751e2f094e
real <real@freedomlayer.org>
2019-05-15 19:45:34 +0300

    Added test_request_response_cancel_send_funds().

--- components/funder/src/mutual_credit/tests.rs
+++ components/funder/src/mutual_credit/tests.rs
@@ -155,7 +155,6 @@ fn test_request_response_commit_send_funds() {
     let rand_nonce = RandValue::from(&[5; RAND_VALUE_LEN]);
     let dest_plain_lock = PlainLock::from(&[2; PLAIN_LOCK_LEN]);
 
-    // TODO: Add signature here:
     let mut response_send_funds = ResponseSendFundsOp {
         request_id: request_id.clone(),
         dest_hashed_lock: dest_plain_lock.hash(),
@@ -252,6 +251,108 @@ fn test_request_cancel_send_funds() {
     )
     .unwrap();
 
+    assert_eq!(mutual_credit.state().balance.balance, 0);
+    assert_eq!(mutual_credit.state().balance.local_max_debt, 100);
+    assert_eq!(mutual_credit.state().balance.remote_max_debt, 0);
+    assert_eq!(mutual_credit.state().balance.local_pending_debt, 10 + 5);
+    assert_eq!(mutual_credit.state().balance.remote_pending_debt, 0);
+
+    // -----[CancelSendFunds]--------
+    // ------------------------------
+    let mut cancel_send_funds = CancelSendFundsOp { request_id };
+
+    apply_incoming(
+        &mut mutual_credit,
+        FriendTcOp::CancelSendFunds(cancel_send_funds),
+    )
+    .unwrap();
+
+    assert_eq!(mutual_credit.state().balance.balance, 0);
+    assert_eq!(mutual_credit.state().balance.local_max_debt, 100);
+    assert_eq!(mutual_credit.state().balance.remote_max_debt, 0);
+    assert_eq!(mutual_credit.state().balance.local_pending_debt, 0);
+    assert_eq!(mutual_credit.state().balance.remote_pending_debt, 0);
+}
+
+#[test]
+fn test_request_response_cancel_send_funds() {
+    let local_public_key = PublicKey::from(&[0xaa; PUBLIC_KEY_LEN]);
+    let remote_public_key = PublicKey::from(&[0xbb; PUBLIC_KEY_LEN]);
+    let balance = 0;
+    let mut mutual_credit = MutualCredit::new(&local_public_key, &remote_public_key, balance);
+
+    // -----[SetRemoteMaxDebt]------
+    // -----------------------------
+    // Make enough trust from remote side, so that we will be able to send credits:
+    apply_incoming(&mut mutual_credit, FriendTcOp::SetRemoteMaxDebt(100)).unwrap();
+
+    // -----[EnableRequests]--------
+    // -----------------------------
+    // Remote side should open his requests status:
+    apply_incoming(&mut mutual_credit, FriendTcOp::EnableRequests).unwrap();
+
+    // -----[RequestSendFunds]--------
+    // -----------------------------
+    let rng = DummyRandom::new(&[1u8]);
+    let pkcs8 = generate_pkcs8_key_pair(&rng);
+    let identity = SoftwareEd25519Identity::from_pkcs8(&pkcs8).unwrap();
+    let public_key_c = identity.get_public_key();
+
+    let request_id = Uid::from(&[3; UID_LEN]);
+    let route = FriendsRoute {
+        public_keys: vec![
+            PublicKey::from(&[0xaa; PUBLIC_KEY_LEN]),
+            PublicKey::from(&[0xbb; PUBLIC_KEY_LEN]),
+            public_key_c.clone(),
+        ],
+    };
+    let invoice_id = InvoiceId::from(&[0; INVOICE_ID_LEN]);
+    let src_plain_lock = PlainLock::from(&[1; PLAIN_LOCK_LEN]);
+
+    let request_send_funds = RequestSendFundsOp {
+        request_id: request_id.clone(),
+        src_hashed_lock: src_plain_lock.hash(),
+        route,
+        dest_payment: 10,
+        invoice_id,
+        left_fees: 5,
+    };
+
+    let pending_transaction = create_pending_transaction(&request_send_funds);
+    apply_outgoing(
+        &mut mutual_credit,
+        &FriendTcOp::RequestSendFunds(request_send_funds),
+    )
+    .unwrap();
+
+    assert_eq!(mutual_credit.state().balance.balance, 0);
+    assert_eq!(mutual_credit.state().balance.local_max_debt, 100);
+    assert_eq!(mutual_credit.state().balance.remote_max_debt, 0);
+    assert_eq!(mutual_credit.state().balance.local_pending_debt, 10 + 5);
+    assert_eq!(mutual_credit.state().balance.remote_pending_debt, 0);
+
+    // -----[ResponseSendFunds]--------
+    // --------------------------------
+    let rand_nonce = RandValue::from(&[5; RAND_VALUE_LEN]);
+    let dest_plain_lock = PlainLock::from(&[2; PLAIN_LOCK_LEN]);
+
+    let mut response_send_funds = ResponseSendFundsOp {
+        request_id: request_id.clone(),
+        dest_hashed_lock: dest_plain_lock.hash(),
+        rand_nonce: rand_nonce.clone(),
+        signature: Signature::from(&[0; SIGNATURE_LEN]),
+    };
+
+    let sign_buffer = create_response_signature_buffer(&response_send_funds, &pending_transaction);
+    response_send_funds.signature = identity.sign(&sign_buffer);
+
+    apply_incoming(
+        &mut mutual_credit,
+        FriendTcOp::ResponseSendFunds(response_send_funds),
+    )
+    .unwrap();
+
+    // We expect that no changes to balance happened yet:
     assert_eq!(mutual_credit.state().balance.balance, 0);
     assert_eq!(mutual_credit.state().balance.local_max_debt, 100);
     assert_eq!(mutual_credit.state().balance.remote_max_debt, 0);
