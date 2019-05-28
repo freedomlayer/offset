4278d6a56ad251215d4dcc90bc62d018c5b08266
real <real@freedomlayer.org>
2019-05-28 11:40:12 +0300

    offst-app-server tests: Restored some tests.

--- components/app_server/src/tests/mod.rs
+++ components/app_server/src/tests/mod.rs
@@ -1,8 +1,7 @@
-// mod all_apps_closed;
-// mod funder_command;
-// mod index_client_command;
-// mod request_routes;
+mod all_apps_closed;
+mod funder_command;
+mod index_client_command;
+mod request_routes;
 // mod request_send_funds;
-// mod two_apps;
-#[allow(unused)]
+mod two_apps;
 mod utils;
