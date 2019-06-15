d21aabd2efaef2f79238be9e118976939a3cac56
Paweł Zmarzły <pawo2500@gmail.com>
2019-06-15 20:22:06 +0200

    Have rust version pinned in a single place

--- travis/trusty/post/cargo-sweep.sh
+++ travis/trusty/post/cargo-sweep.sh
@@ -5,7 +5,8 @@
 #       maybe use cargo-update library for that
 cargo install cargo-sweep --vers 0.4.1 || true
 
-echo Rust toolchain version: $TRAVIS_RUST_VERSION
+RUST_VERSION=$(head -n 1 rust-toolchain)
+echo "Rust toolchain version: $RUST_VERSION"
 
 # cargo-sweep produces a lot of output
-cargo sweep --toolchains $TRAVIS_RUST_VERSION > /dev/null
+cargo sweep --toolchains "$RUST_VERSION" > /dev/null
