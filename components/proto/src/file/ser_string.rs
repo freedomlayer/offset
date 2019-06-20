7149761e48cbd0d856020085933b461310a6e9ed
real <real@freedomlayer.org>
2019-06-20 16:48:37 +0300

    Added to_base64(), from_base64() to ser_string.rs

--- components/proto/src/file/ser_string.rs
+++ components/proto/src/file/ser_string.rs
@@ -1,4 +1,7 @@
+use std::convert::TryFrom;
+
 use base64::{self, URL_SAFE_NO_PAD};
+
 use crypto::hash::{HashResult, HASH_RESULT_LEN};
 use crypto::hash_lock::{HashedLock, PlainLock, HASHED_LOCK_LEN, PLAIN_LOCK_LEN};
 use crypto::identity::{PublicKey, Signature, PUBLIC_KEY_LEN, SIGNATURE_LEN};
@@ -11,6 +14,31 @@ use crypto::rand::{RandValue, RAND_VALUE_LEN};
 
 #[derive(Debug)]
 pub struct SerStringError;
+
+use serde::de::Error;
+use serde::{Deserialize, Deserializer, Serializer};
+
+/// Serializes `buffer` to a lowercase hex string.
+pub fn to_base64<T, S>(to_base64: &T, serializer: S) -> Result<S::Ok, S::Error>
+where
+    T: AsRef<[u8]>,
+    S: Serializer,
+{
+    let base64_str = base64::encode_config(&to_base64.as_ref(), URL_SAFE_NO_PAD);
+    serializer.serialize_str(&base64_str)
+}
+
+/// Deserializes a lowercase hex string to a `Vec<u8>`.
+pub fn from_base64<'t, 'de, D, T>(deserializer: D) -> Result<T, D::Error>
+where
+    T: TryFrom<Vec<u8>>,
+    D: Deserializer<'de>,
+{
+    let string = String::deserialize(deserializer)?;
+    let vec = base64::decode_config(&string, URL_SAFE_NO_PAD)
+        .map_err(|err| Error::custom(err.to_string()))?;
+    T::try_from(vec).map_err(|_| Error::custom("Length mismatch"))
+}
 
 /// Define conversion to/from String:
 macro_rules! str_convert_funcs {
