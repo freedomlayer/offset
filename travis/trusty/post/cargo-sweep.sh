a63b27450e77f45c213d54e977e8c7e2cdcbdf3c
Paweł Zmarzły <pawo2500@gmail.com>
2019-06-20 22:29:34 +0200

    Remove unnecessary check in CI

--- travis/trusty/post/cargo-sweep.sh
+++ travis/trusty/post/cargo-sweep.sh
@@ -2,11 +2,7 @@
 
 CARGO_SWEEP_VERSION=0.4.1
 
-# Install cargo-sweep if absent
-if [ ! -f ~/.cargo/bin/cargo-sweep ]; then
-    cargo install cargo-sweep --vers $CARGO_SWEEP_VERSION
-fi
-
+# FOUND_VERSION can be empty, don't set -o pipefail
 FOUND_VERSION=$(grep cargo-sweep ~/.cargo/.crates.toml | cut -d' ' -f2)
 
 # Update cargo-sweep if necessary
