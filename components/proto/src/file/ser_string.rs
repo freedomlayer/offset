3a94f37b06afad74ee34034942677ba10e61648d
real <real@freedomlayer.org>
2019-06-24 23:07:37 +0300

    Added to_string() and from_string() to ser_string.rs.

--- components/proto/src/file/ser_string.rs
+++ components/proto/src/file/ser_string.rs
@@ -1,4 +1,6 @@
 use std::convert::TryFrom;
+use std::str::FromStr;
+use std::string::ToString;
 
 use base64::{self, URL_SAFE_NO_PAD};
 
@@ -38,6 +40,26 @@ where
     let vec = base64::decode_config(&string, URL_SAFE_NO_PAD)
         .map_err(|err| Error::custom(err.to_string()))?;
     T::try_from(&vec).map_err(|_| Error::custom("Length mismatch"))
+}
+
+/// Serializes value as a string
+pub fn to_string<T, S>(input: &T, serializer: S) -> Result<S::Ok, S::Error>
+where
+    T: ToString,
+    S: Serializer,
+{
+    serializer.serialize_str(&input.to_string())
+}
+
+/// Deserializes a string into a value
+pub fn from_string<'de, D, T>(deserializer: D) -> Result<T, D::Error>
+where
+    T: FromStr,
+    D: Deserializer<'de>,
+{
+    String::deserialize(deserializer)?
+        .parse()
+        .map_err(|_| Error::custom("Failed to parse as string"))
 }
 
 /// Define conversion to/from String:
