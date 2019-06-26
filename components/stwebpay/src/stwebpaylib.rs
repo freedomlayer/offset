06ecc90510f8f6de72c0087312c35bfa372d6aa9
real <real@freedomlayer.org>
2019-06-26 19:39:21 +0300

    Updated stwebpay to use new serde ergonomics.

--- components/stwebpay/src/stwebpaylib.rs
+++ components/stwebpay/src/stwebpaylib.rs
@@ -1,16 +1,21 @@
+use std::fs;
 use std::io;
 use std::net::SocketAddr;
 use std::path::PathBuf;
+
+use derive_more::From;
 
 use futures::executor::ThreadPool;
 
 use structopt::StructOpt;
 
-use app::{connect, identity_from_file, load_node_from_file};
+use app::file::NodeAddressFile;
+use app::ser_string::{deserialize_from_string, StringSerdeError};
+use app::{connect, identity_from_file};
 
 use crate::web_app::serve_app;
 
-#[derive(Debug)]
+#[derive(Debug, From)]
 pub enum StWebPayError {
     CreateThreadPoolError,
     IdFileDoesNotExist,
@@ -21,6 +26,8 @@ pub enum StWebPayError {
     NoBuyerPermissions,
     NoRoutesPermissions,
     GetReportError,
+    IoError(std::io::Error),
+    StringSerdeError(StringSerdeError),
 }
 
 /// stwebpay: offST WEB PAYment system
@@ -30,10 +37,10 @@ pub enum StWebPayError {
 pub struct StWebPayCmd {
     /// StWebPay app identity file path
     #[structopt(parse(from_os_str), short = "I", long = "idfile")]
-    pub idfile: PathBuf,
+    pub idfile_path: PathBuf,
     /// Node ticket file path
     #[structopt(parse(from_os_str), short = "T", long = "ticket")]
-    pub node_ticket: PathBuf,
+    pub node_ticket_path: PathBuf,
     /// Listening address (For HTTP server)
     #[structopt(short = "l", long = "laddr")]
     pub laddr: SocketAddr,
@@ -46,34 +53,34 @@ pub fn stwebpay(
     let mut thread_pool = ThreadPool::new().map_err(|_| StWebPayError::CreateThreadPoolError)?;
 
     let StWebPayCmd {
-        idfile,
-        node_ticket,
+        idfile_path,
+        node_ticket_path,
         laddr,
     } = st_web_cmd;
 
     // Get application's identity:
-    if !idfile.exists() {
+    if !idfile_path.exists() {
         return Err(StWebPayError::IdFileDoesNotExist);
     }
 
     // Get node's connection information (node-ticket):
-    if !node_ticket.exists() {
+    if !node_ticket_path.exists() {
         return Err(StWebPayError::NodeTicketFileDoesNotExist);
     }
 
     // Get node information from file:
-    let node_address =
-        load_node_from_file(&node_ticket).map_err(|_| StWebPayError::InvalidNodeTicketFile)?;
+    let node_address_file: NodeAddressFile =
+        deserialize_from_string(&fs::read_to_string(&node_ticket_path)?)?;
 
     // Spawn identity service:
-    let app_identity_client = identity_from_file(&idfile, thread_pool.clone())
+    let app_identity_client = identity_from_file(&idfile_path, thread_pool.clone())
         .map_err(|_| StWebPayError::SpawnIdentityServiceError)?;
 
     let c_thread_pool = thread_pool.clone();
     let mut app_conn = thread_pool
         .run(connect(
-            node_address.public_key,
-            node_address.address,
+            node_address_file.public_key,
+            node_address_file.address,
             app_identity_client,
             c_thread_pool.clone(),
         ))
