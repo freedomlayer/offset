6d73d83194f8b5bd352b7afbcb03f8cbb26d5175
real <real@freedomlayer.org>
2019-06-27 22:45:31 +0300

    offst-mutual-from-derive: Added basic tests for offst-mutual-from-derive.

--- components/mutual_from_derive/tests/basic.rs
+++ components/mutual_from_derive/tests/basic.rs
@@ -14,7 +14,7 @@ struct Hello2 {
 }
 
 #[test]
-fn basic() {
+fn test_mutual_from_named_struct() {
     let hello1 = Hello1 {
         a: 0u32,
         b: "some_string".to_owned(),
@@ -25,4 +25,40 @@ fn basic() {
 
     assert_eq!(hello1, hello1_new);
     assert_eq!(hello2, hello2_new);
+}
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+struct MyStruct1(u32, u32);
+
+#[mutual_from(MyStruct1)]
+#[derive(PartialEq, Eq, Clone, Debug)]
+struct MyStruct2(u32, u32);
+
+#[test]
+fn test_mutual_from_unnamed_struct() {
+    let my_struct1 = MyStruct1(1u32, 2u32);
+    let my_struct2 = MyStruct2::from(my_struct1.clone());
+    let my_struct1_new = MyStruct1::from(my_struct2.clone());
+    let my_struct2_new = MyStruct2::from(my_struct1.clone());
+
+    assert_eq!(my_struct1, my_struct1_new);
+    assert_eq!(my_struct2, my_struct2_new);
+}
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+struct World1;
+
+#[mutual_from(World1)]
+#[derive(PartialEq, Eq, Clone, Debug)]
+struct World2;
+
+#[test]
+fn test_mutual_from_unit_struct() {
+    let world1 = World1;
+    let world2 = World2::from(world1.clone());
+    let world1_new = World1::from(world2.clone());
+    let world2_new = World2::from(world1_new.clone());
+
+    assert_eq!(world1, world1_new);
+    assert_eq!(world2, world2_new);
 }
