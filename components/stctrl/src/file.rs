7a7e467e876fad7276756f97f9223758ae67b882
real <real@freedomlayer.org>
2019-06-27 23:00:22 +0300

    Refactored offst-stctrl to use mutual_from_derive.

--- components/stctrl/src/file.rs
+++ components/stctrl/src/file.rs
@@ -9,6 +9,8 @@ use app::{
 use app::payment::PaymentId;
 use app::report::MoveTokenHashedReport;
 
+use mutual_from_derive::mutual_from;
+
 #[derive(Serialize, Deserialize)]
 pub struct InvoiceFile {
     #[serde(serialize_with = "to_base64", deserialize_with = "from_base64")]
@@ -20,6 +22,7 @@ pub struct InvoiceFile {
 }
 
 /// Representing a Commit in an easy to serialize representation.
+#[mutual_from(Commit)]
 #[derive(Serialize, Deserialize)]
 pub struct CommitFile {
     #[serde(serialize_with = "to_base64", deserialize_with = "from_base64")]
@@ -52,6 +55,7 @@ pub struct PaymentFile {
 }
 
 /// A helper structure for serialize and deserializing Receipt.
+#[mutual_from(Receipt)]
 #[derive(Serialize, Deserialize)]
 pub struct ReceiptFile {
     #[serde(serialize_with = "to_base64", deserialize_with = "from_base64")]
@@ -71,6 +75,7 @@ pub struct ReceiptFile {
 }
 
 /// A helper structure for serialize and deserializing Token.
+#[mutual_from(MoveTokenHashedReport)]
 #[derive(Serialize, Deserialize)]
 pub struct TokenFile {
     #[serde(serialize_with = "to_base64", deserialize_with = "from_base64")]
@@ -94,32 +99,6 @@ pub struct TokenFile {
     pub new_token: Signature,
 }
 
-// TODO: Create a macro that does the following ergonomic conversion automatically:
-
-impl std::convert::From<Commit> for CommitFile {
-    fn from(input: Commit) -> Self {
-        CommitFile {
-            response_hash: input.response_hash.clone(),
-            dest_payment: input.dest_payment,
-            src_plain_lock: input.src_plain_lock.clone(),
-            dest_hashed_lock: input.dest_hashed_lock.clone(),
-            signature: input.signature.clone(),
-        }
-    }
-}
-
-impl std::convert::From<CommitFile> for Commit {
-    fn from(input: CommitFile) -> Self {
-        Commit {
-            response_hash: input.response_hash.clone(),
-            dest_payment: input.dest_payment,
-            src_plain_lock: input.src_plain_lock.clone(),
-            dest_hashed_lock: input.dest_hashed_lock.clone(),
-            signature: input.signature.clone(),
-        }
-    }
-}
-
 impl std::convert::From<MultiCommit> for MultiCommitFile {
     fn from(input: MultiCommit) -> Self {
         MultiCommitFile {
@@ -136,68 +115,6 @@ impl std::convert::From<MultiCommitFile> for MultiCommit {
             invoice_id: input.invoice_id,
             total_dest_payment: input.total_dest_payment,
             commits: input.commits.into_iter().map(Commit::from).collect(),
-        }
-    }
-}
-
-impl std::convert::From<Receipt> for ReceiptFile {
-    fn from(input: Receipt) -> Self {
-        ReceiptFile {
-            response_hash: input.response_hash,
-            invoice_id: input.invoice_id,
-            src_plain_lock: input.src_plain_lock,
-            dest_plain_lock: input.dest_plain_lock,
-            dest_payment: input.dest_payment,
-            total_dest_payment: input.total_dest_payment,
-            signature: input.signature,
-        }
-    }
-}
-
-impl std::convert::From<ReceiptFile> for Receipt {
-    fn from(input: ReceiptFile) -> Self {
-        Receipt {
-            response_hash: input.response_hash,
-            invoice_id: input.invoice_id,
-            src_plain_lock: input.src_plain_lock,
-            dest_plain_lock: input.dest_plain_lock,
-            dest_payment: input.dest_payment,
-            total_dest_payment: input.total_dest_payment,
-            signature: input.signature,
-        }
-    }
-}
-
-impl std::convert::From<MoveTokenHashedReport> for TokenFile {
-    fn from(input: MoveTokenHashedReport) -> Self {
-        TokenFile {
-            prefix_hash: input.prefix_hash,
-            local_public_key: input.local_public_key,
-            remote_public_key: input.remote_public_key,
-            inconsistency_counter: input.inconsistency_counter,
-            move_token_counter: input.move_token_counter,
-            balance: input.balance,
-            local_pending_debt: input.local_pending_debt,
-            remote_pending_debt: input.remote_pending_debt,
-            rand_nonce: input.rand_nonce,
-            new_token: input.new_token,
-        }
-    }
-}
-
-impl std::convert::From<TokenFile> for MoveTokenHashedReport {
-    fn from(input: TokenFile) -> Self {
-        MoveTokenHashedReport {
-            prefix_hash: input.prefix_hash,
-            local_public_key: input.local_public_key,
-            remote_public_key: input.remote_public_key,
-            inconsistency_counter: input.inconsistency_counter,
-            move_token_counter: input.move_token_counter,
-            balance: input.balance,
-            local_pending_debt: input.local_pending_debt,
-            remote_pending_debt: input.remote_pending_debt,
-            rand_nonce: input.rand_nonce,
-            new_token: input.new_token,
         }
     }
 }
