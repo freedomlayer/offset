e6c80080209c41599df3601be874df1061db25bb
real <real@freedomlayer.org>
2019-10-20 18:26:23 +0300

    offst-test: basic_cli: Some adjustments to multi-currency. Not done yet (Missing SetFriendCurrencies).

--- components/test/src/cli_tests/basic_cli.rs
+++ components/test/src/cli_tests/basic_cli.rs
@@ -7,12 +7,12 @@ use bin::stnodelib::{stnode, StNodeCmd};
 use bin::strelaylib::{strelay, StRelayCmd};
 
 use stctrl::config::{
-    AddFriendCmd, AddIndexCmd, AddRelayCmd, CloseFriendCmd, ConfigCmd, DisableFriendCmd,
-    EnableFriendCmd, OpenFriendCmd, SetFriendMaxDebtCmd, SetFriendCurrencyRateCmd,
+    AddFriendCmd, AddIndexCmd, AddRelayCmd, CloseFriendCurrencyCmd, ConfigCmd, DisableFriendCmd,
+    EnableFriendCmd, OpenFriendCurrencyCmd, SetFriendCurrencyMaxDebtCmd, SetFriendCurrencyRateCmd,
 };
 
 use stctrl::buyer::{BuyerCmd, BuyerError, PayInvoiceCmd, PaymentStatusCmd};
-use stctrl::info::{BalanceCmd, ExportTicketCmd, FriendLastTokenCmd, FriendsCmd, InfoCmd};
+use stctrl::info::{ExportTicketCmd, FriendLastTokenCmd, FriendsCmd, InfoCmd};
 use stctrl::seller::{CancelInvoiceCmd, CommitInvoiceCmd, CreateInvoiceCmd, SellerCmd};
 use stctrl::stctrllib::{stctrl, StCtrlCmd, StCtrlError, StCtrlSubcommand};
 use stctrl::stverifylib::{stverify, StVerifyCmd, VerifyReceiptCmd, VerifyTokenCmd};
@@ -260,8 +260,6 @@ fn configure_mutual_credit(stctrl_setup: &StCtrlSetup) {
     // ------------
 
     for j in 0..2 {
-        // node0 has a plus of 20 credits
-        let balance = if j == 0 { 20 } else { -20 };
         // Node0: Add node1 as a friend
         let add_friend_cmd = AddFriendCmd {
             friend_path: stctrl_setup
@@ -269,7 +267,6 @@ fn configure_mutual_credit(stctrl_setup: &StCtrlSetup) {
                 .join(format!("app{}", 1 - j))
                 .join(format!("node{}.friend", 1 - j)),
             friend_name: format!("node{}", 1 - j),
-            balance,
         };
         let config_cmd = ConfigCmd::AddFriend(add_friend_cmd);
         let subcommand = StCtrlSubcommand::Config(config_cmd);
@@ -295,6 +292,7 @@ fn configure_mutual_credit(stctrl_setup: &StCtrlSetup) {
     for j in 0..2 {
         let set_friend_currency_rate_cmd = SetFriendCurrencyRateCmd {
             friend_name: format!("node{}", 1 - j),
+            currency_name: "FST".to_owned(),
             mul: 0,
             add: 1,
         };
@@ -368,13 +366,38 @@ fn configure_mutual_credit(stctrl_setup: &StCtrlSetup) {
         }
     }
 
+    // Setup active currencies:
+    // -------------------------
+    for j in 0..2 {
+        let open_friend_cmd = OpenFriendCurrencyCmd {
+            friend_name: format!("node{}", 1 - j),
+            currency_name: "FST".to_owned(),
+        };
+        let config_cmd = ConfigCmd::OpenFriendCurrency(open_friend_cmd);
+        let subcommand = StCtrlSubcommand::Config(config_cmd);
+
+        let st_ctrl_cmd = StCtrlCmd {
+            idfile: stctrl_setup
+                .temp_dir_path
+                .join(format!("app{}", j))
+                .join(format!("app{}.ident", j)),
+            node_ticket: stctrl_setup
+                .temp_dir_path
+                .join(format!("node{}", j))
+                .join(format!("node{}.ticket", j)),
+            subcommand,
+        };
+        stctrl(st_ctrl_cmd, &mut Vec::new()).unwrap();
+    }
+
     // Open friends
     // ---------------
     for j in 0..2 {
-        let open_friend_cmd = OpenFriendCmd {
+        let open_friend_cmd = OpenFriendCurrencyCmd {
             friend_name: format!("node{}", 1 - j),
+            currency_name: "FST".to_owned(),
         };
-        let config_cmd = ConfigCmd::OpenFriend(open_friend_cmd);
+        let config_cmd = ConfigCmd::OpenFriendCurrency(open_friend_cmd);
         let subcommand = StCtrlSubcommand::Config(config_cmd);
 
         let st_ctrl_cmd = StCtrlCmd {
@@ -425,11 +448,12 @@ fn configure_mutual_credit(stctrl_setup: &StCtrlSetup) {
 /// Set max_debt for node1
 fn set_max_debt(stctrl_setup: &StCtrlSetup) {
     // node0 sets remote max debt for node1:
-    let set_friend_max_debt_cmd = SetFriendMaxDebtCmd {
+    let set_friend_currency_max_debt_cmd = SetFriendCurrencyMaxDebtCmd {
         friend_name: "node1".to_owned(),
+        currency_name: "FST".to_owned(),
         max_debt: 200,
     };
-    let config_cmd = ConfigCmd::SetFriendMaxDebt(set_friend_max_debt_cmd);
+    let config_cmd = ConfigCmd::SetFriendCurrencyMaxDebt(set_friend_currency_max_debt_cmd);
     let subcommand = StCtrlSubcommand::Config(config_cmd);
 
     let st_ctrl_cmd = StCtrlCmd {
@@ -475,6 +499,7 @@ fn create_cancel_invoice(stctrl_setup: &StCtrlSetup) {
     // Node0: generate an invoice:
     // ---------------------------
     let create_invoice_cmd = CreateInvoiceCmd {
+        currency_name: "FST".to_owned(),
         amount: 50,
         invoice_path: stctrl_setup
             .temp_dir_path
@@ -531,6 +556,7 @@ fn pay_invoice(stctrl_setup: &StCtrlSetup) {
     // Node0: generate an invoice:
     // ---------------------------
     let create_invoice_cmd = CreateInvoiceCmd {
+        currency_name: "FST".to_owned(),
         amount: 50,
         invoice_path: stctrl_setup
             .temp_dir_path
@@ -677,6 +703,7 @@ fn pay_invoice(stctrl_setup: &StCtrlSetup) {
     assert!(str::from_utf8(&output).unwrap().contains("is valid!"));
 }
 
+/*
 /// View balance of node1
 fn check_balance(stctrl_setup: &StCtrlSetup) {
     let balance_cmd = BalanceCmd {};
@@ -696,6 +723,7 @@ fn check_balance(stctrl_setup: &StCtrlSetup) {
     stctrl(st_ctrl_cmd, &mut output).unwrap();
     assert!(str::from_utf8(&output).unwrap().contains("-70"));
 }
+*/
 
 /// Export a friend's last token and then verify it
 fn export_token(stctrl_setup: &StCtrlSetup) {
@@ -736,10 +764,11 @@ fn close_disable(stctrl_setup: &StCtrlSetup) {
     // Close friends:
     // ---------------
     for j in 0..2 {
-        let close_friend_cmd = CloseFriendCmd {
+        let close_friend_currency_cmd = CloseFriendCurrencyCmd {
             friend_name: format!("node{}", 1 - j),
+            currency_name: "FST".to_owned(),
         };
-        let config_cmd = ConfigCmd::CloseFriend(close_friend_cmd);
+        let config_cmd = ConfigCmd::CloseFriendCurrency(close_friend_currency_cmd);
         let subcommand = StCtrlSubcommand::Config(config_cmd);
 
         let st_ctrl_cmd = StCtrlCmd {
@@ -854,7 +883,7 @@ fn basic_cli() {
     set_max_debt(&stctrl_setup);
     create_cancel_invoice(&stctrl_setup);
     pay_invoice(&stctrl_setup);
-    check_balance(&stctrl_setup);
+    // check_balance(&stctrl_setup);
     export_token(&stctrl_setup);
     close_disable(&stctrl_setup);
 }
