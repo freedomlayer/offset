cb6c85b25ec0a7d746ee1f915fd58493be9a8521
real <real@freedomlayer.org>
2019-11-13 23:21:31 +0200

    offst-net: Some work on tcp_stream_to_conn_pair(). Not working yet (Stall bug).

--- components/net/src/utils.rs
+++ components/net/src/utils.rs
@@ -1,10 +1,11 @@
 // TODO: Remove this later
 #![allow(unused)]
-// use bytes::Bytes;
+use bytes::Bytes;
 
-// use futures::channel::mpsc;
+use futures::channel::mpsc;
 use futures::task::{Spawn, SpawnExt};
-// use futures::{FutureExt, SinkExt, StreamExt};
+use futures::{future, FutureExt, SinkExt, StreamExt, TryFutureExt, TryStreamExt};
+use futures_codec::{Framed, LengthCodec};
 
 // use tokio::codec::{Framed, LengthDelimitedCodec};
 // use tokio::net::TcpStream;
@@ -15,26 +16,46 @@ use common::conn::ConnPairVec;
 
 pub fn tcp_stream_to_conn_pair<S>(
     tcp_stream: TcpStream,
-    max_frame_length: usize,
+    _max_frame_length: usize,
     spawner: &mut S,
 ) -> ConnPairVec
 where
     S: Spawn + Send,
 {
-    // TODO
-    unimplemented!();
-    /*
-    let mut codec = LengthDelimitedCodec::new();
-    codec.set_max_frame_length(max_frame_length);
-    let (sender_01, receiver_01) = Framed::new(tcp_stream, codec).split();
+    // TODO: Return support for max_frame_length
+    let mut codec = LengthCodec;
+    // codec.set_max_frame_length(max_frame_length);
+    let (sender, receiver) = Framed::new(tcp_stream, codec).split();
 
     // Conversion layer between Vec<u8> to Bytes:
-    let sender_01 = sender_01
-        .sink_map_err(|_| ())
-        .with(|vec: Vec<u8>| -> Result<Bytes, ()> { Ok(Bytes::from(vec)) });
+    let mut vec_sender =
+        sender
+            .sink_map_err(|_| ())
+            .with(|vec: Vec<u8>| -> future::Ready<Result<Bytes, ()>> {
+                future::ready(Ok(Bytes::from(vec)))
+            });
 
-    let receiver_01 = receiver_01.map(|bytes| bytes.to_vec());
+    let mut vec_receiver = receiver.map_ok(|bytes| bytes.to_vec())
+                                .map_err(|_| ());
 
-    (sender_01, receiver_01)
-    */
+    let (user_sender, local_receiver) = mpsc::channel::<Vec<u8>>(1);
+    let (local_sender, user_receiver) = mpsc::channel::<Vec<u8>>(1);
+
+
+    spawner.spawn(async move {
+        local_sender
+            .sink_map_err(|_| ())
+            .send_all(&mut vec_receiver)
+            .map_err(|e| warn!("Failure to send to local_sender: {:?}", e))
+            .await;
+    });
+
+    spawner.spawn(async move {
+        vec_sender
+            .send_all(&mut local_receiver.map(Ok))
+            .map_err(|e| warn!("Failure to send to vec_senderr: {:?}", e))
+            .await;
+    });
+
+    (user_sender, user_receiver)
 }
