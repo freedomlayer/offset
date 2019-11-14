460dc7f65f1d30d42852d81ccbe97db72eeb1794
real <real@freedomlayer.org>
2019-11-14 17:51:12 +0200

    offst-net: Fixed unused warnings

--- components/net/src/utils.rs
+++ components/net/src/utils.rs
@@ -1,14 +1,9 @@
-// TODO: Remove this later
-#![allow(unused)]
 use bytes::Bytes;
 
 use futures::channel::mpsc;
 use futures::task::{Spawn, SpawnExt};
-use futures::{future, FutureExt, SinkExt, StreamExt, TryFutureExt, TryStreamExt};
+use futures::{future, SinkExt, StreamExt};
 use futures_codec::{Framed, LengthCodec};
-
-// use tokio::codec::{Framed, LengthDelimitedCodec};
-// use tokio::net::TcpStream;
 
 use async_std::net::TcpStream;
 
@@ -23,7 +18,7 @@ where
     S: Spawn + Send,
 {
     // TODO: Return support for max_frame_length
-    let mut codec = LengthCodec;
+    let codec = LengthCodec;
     // codec.set_max_frame_length(max_frame_length);
     let (sender, receiver) = Framed::new(tcp_stream, codec).split();
 
@@ -35,23 +30,23 @@ where
                 future::ready(Ok(Bytes::from(vec)))
             });
 
-    let mut vec_receiver = receiver
+    let vec_receiver = receiver
         .take_while(|res| future::ready(res.is_ok()))
         .map(|res| res.unwrap().to_vec());
 
     // Add a mechanism to make sure that the connection is dropped when we drop the sender.
     // Not fully sure why this doesn't happen automatically.
-    let (user_sender, mut user_sender_receiver) = mpsc::channel(0);
+    let (user_sender, user_sender_receiver) = mpsc::channel(0);
     let (mut user_receiver_sender, user_receiver) = mpsc::channel(0);
 
     let receiver_task = spawner.spawn_with_handle(async move {
-        user_receiver_sender.send_all(&mut vec_receiver.map(Ok)).await;
-    });
+        let _ = user_receiver_sender.send_all(&mut vec_receiver.map(Ok)).await;
+    }).unwrap();
 
     spawner.spawn(async move {
-        vec_sender.send_all(&mut user_sender_receiver.map(Ok)).await;
+        let _ = vec_sender.send_all(&mut user_sender_receiver.map(Ok)).await;
         drop(receiver_task);
-    });
+    }).unwrap();
 
     ConnPairVec::from_raw(user_sender, user_receiver)
 }
