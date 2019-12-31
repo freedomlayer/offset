e500e3102c78cb5a9c7faa8b99750eedec38960e
real <real@freedomlayer.org>
2019-12-31 13:25:31 +0200

    offst-stcompact: Fix: compact_server_loop() should only take a tcp_connector as an argument.

--- components/stcompact/src/stcompactlib.rs
+++ components/stcompact/src/stcompactlib.rs
@@ -9,7 +9,7 @@ use futures::task::{Spawn, SpawnExt};
 
 use structopt::StructOpt;
 
-use common::conn::{FuncFutTransform, FutTransform};
+// use common::conn::{FuncFutTransform, FutTransform};
 use common::int_convert::usize_to_u64;
 
 use crypto::rand::system_random;
@@ -17,12 +17,13 @@ use crypto::rand::system_random;
 use timer::create_timer;
 
 use net::TcpConnector;
-use version::VersionPrefix;
+// use version::VersionPrefix;
+// use connection::{create_encrypt_keepalive, create_secure_connector};
 
-use proto::consts::{MAX_FRAME_LENGTH, PROTOCOL_VERSION, TICK_MS};
+use proto::consts::{MAX_FRAME_LENGTH, TICK_MS};
 
 #[allow(unused)]
-use crate::server_loop::compact_server_loop;
+use crate::server_loop::{compact_server_loop, ServerError, ConnPairCompactServer};
 use crate::store::open_file_store;
 
 #[allow(clippy::enum_variant_names)]
@@ -31,6 +32,7 @@ pub enum CompactBinError {
     // CreateThreadPoolError,
     CreateTimerError,
     OpenFileStoreError,
+    ServerError(ServerError),
     /*
     LoadIdentityError,
     LoadDbError,
@@ -57,6 +59,21 @@ pub struct StCompactCmd {
 }
 
 #[allow(unused)]
+fn create_stdio_conn_pair() -> ConnPairCompactServer {
+    /*
+
+    let mut stdout = async_std::io::stdout();
+    let mut stdin = async_std::io::stdin();
+
+    // TODO: Things to prepare:
+        - async stdio
+        - serialization (json)
+            - Which seperator to use for messages? Newline?
+    */
+    todo!();
+}
+
+#[allow(unused)]
 pub async fn stcompact<S, FS>(
     st_compact_cmd: StCompactCmd,
     spawner: S,
@@ -74,11 +91,12 @@ where
         create_timer(dur, spawner.clone()).map_err(|_| CompactBinError::CreateTimerError)?;
 
     // A tcp connector, Used to connect to remote servers:
-    let tcp_connector = TcpConnector::new(MAX_FRAME_LENGTH, spawner.clone());
+    let _tcp_connector = TcpConnector::new(MAX_FRAME_LENGTH, spawner.clone());
 
     // Obtain secure cryptographic random:
     let _rng = system_random();
 
+    /*
     // Wrap net connector with a version prefix:
     let version_transform = VersionPrefix::new(PROTOCOL_VERSION, spawner.clone());
     let c_version_transform = version_transform.clone();
@@ -90,31 +108,25 @@ where
             Some(c_version_transform.transform(conn_pair).await)
         })
     });
+    */
+
+    todo!();
 
     let file_store = open_file_store(store_path.into(), spawner.clone(), file_spawner.clone())
         .await
         .map_err(|_| CompactBinError::OpenFileStoreError)?;
 
     /*
-    // TODO: Things to prepare:
-        - async stdio
-        - serialization (json)
-            - Which seperator to use for messages? Newline?
-    */
-
-    /*
-    let conn_pair = unimplemented!();
+    let conn_pair = create_stdio_conn_pair();
 
     let compact_server_fut = compact_server_loop(
-        conn_pair, // : ConnPairCompactServer,
+        conn_pair,
         file_store,
         timer_client,
         rng,
-        version_connector,
-        thread_pool,
-    );
+        tcp_connector,
+        encrypt_keepalive,
+        spawner.clone(),
+    ).await?;
     */
-
-    // block_on(node_fut).map_err(NodeBinError::NetNodeError)
-    unimplemented!();
 }
