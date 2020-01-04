3bf2cbb0a02c07a3cc99db74690acf740ab247b9
real <real@freedomlayer.org>
2020-01-04 15:19:22 +0200

    offst-common: Restored ser_vec_b64 shim

--- components/common/src/ser_utils.rs
+++ components/common/src/ser_utils.rs
@@ -451,7 +451,7 @@ pub mod ser_seq_b64 {
             {
                 let mut res_vec: V = V::default();
                 while let Some(str_item) = seq.next_element::<String>()? {
-                    let item_vec = base64::decode_config(&str_item, URL_SAFE_NO_PAD)
+                    let item_vec: Vec<u8> = base64::decode_config(&str_item, URL_SAFE_NO_PAD)
                         .map_err(|err| Error::custom(err.to_string()))?;
                     let item =
                         T::try_from(&item_vec).map_err(|_| Error::custom("Length mismatch"))?;
@@ -469,6 +469,63 @@ pub mod ser_seq_b64 {
     }
 }
 
+// ============================================================================
+
+// TODO: Left here as a shim until ser_seq_b64 compiles.
+pub mod ser_vec_b64 {
+    use super::*;
+
+    pub fn serialize<T, S>(input_vec: &Vec<T>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+        T: Serialize + AsRef<[u8]>,
+    {
+        let items_iter = input_vec
+            .iter()
+            .map(|item| base64::encode_config(item.as_ref(), URL_SAFE_NO_PAD));
+        serializer.collect_seq(items_iter)
+    }
+
+    pub fn deserialize<'de, T, D>(deserializer: D) -> Result<Vec<T>, D::Error>
+    where
+        D: Deserializer<'de>,
+        T: Deserialize<'de> + for<'t> TryFrom<&'t [u8]>,
+    {
+        struct SeqVisitor<T> {
+            item: PhantomData<T>,
+        }
+
+        impl<'de, T> Visitor<'de> for SeqVisitor<T>
+        where
+            T: Deserialize<'de> + for<'t> TryFrom<&'t [u8]>,
+        {
+            type Value = Vec<T>;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("A vector")
+            }
+
+            fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+            where
+                A: SeqAccess<'de>,
+            {
+                let mut res_vec = Vec::new();
+                while let Some(str_item) = seq.next_element::<String>()? {
+                    let item_vec = base64::decode_config(&str_item, URL_SAFE_NO_PAD)
+                        .map_err(|err| Error::custom(err.to_string()))?;
+                    let item =
+                        T::try_from(&item_vec).map_err(|_| Error::custom("Length mismatch"))?;
+                    res_vec.push(item);
+                }
+                Ok(res_vec)
+            }
+        }
+
+        let visitor = SeqVisitor { item: PhantomData };
+        deserializer.deserialize_seq(visitor)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -482,6 +539,13 @@ mod tests {
         my_vec: Vec<[u8; 16]>,
     }
     */
+
+    #[allow(unused)]
+    #[derive(Deserialize)]
+    struct MyVecStruct {
+        #[serde(with = "ser_vec_b64")]
+        my_vec: Vec<[u8; 16]>,
+    }
 
     #[allow(unused)]
     #[derive(Serialize, Deserialize)]
