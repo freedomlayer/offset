6ce06272ed0f7038a79b188af3056b32f22e7a47
real <real@freedomlayer.org>
2020-01-07 12:04:47 +0200

    offst-test: serialize: Added 'ser_de_test' macro. Added tests for proto/file structs.

--- components/test/src/tests/serialize.rs
+++ components/test/src/tests/serialize.rs
@@ -1,10 +1,47 @@
 use rand::{self, rngs::StdRng};
 
 use funder::FunderState;
+use proto::file::{
+    FriendAddressFile, FriendFile, IdentityFile, IndexServerFile, NodeAddressFile,
+    RelayAddressFile, TrustedAppFile,
+};
 use proto::net::messages::NetAddress;
 
 use quickcheck::QuickCheck;
 
+/// Define conversion to/from String:
+macro_rules! ser_de_test {
+    ($test_name:ident, $msg_type:ty) => {
+        #[test]
+        fn $test_name() {
+            fn ser_de(msg: $msg_type) -> bool {
+                let ser_str = serde_json::to_string_pretty(&msg).unwrap();
+                let msg2: $msg_type = serde_json::from_str(&ser_str).unwrap();
+                msg == msg2
+            }
+
+            let rng_seed: [u8; 32] = [1; 32];
+            let rng: StdRng = rand::SeedableRng::from_seed(rng_seed);
+
+            // Limit size, to avoid blowup to type size:
+            let size = 3usize;
+            QuickCheck::with_gen(quickcheck::StdGen::new(rng, size))
+                .max_tests(100)
+                .quickcheck(ser_de as fn($msg_type) -> bool);
+        }
+    };
+}
+
+ser_de_test!(qc_ser_de_funder_state, FunderState<NetAddress>);
+ser_de_test!(qc_ser_de_friend_address_file, FriendAddressFile);
+ser_de_test!(qc_ser_de_friend_file, FriendFile);
+ser_de_test!(qc_ser_de_identity_file, IdentityFile);
+ser_de_test!(qc_ser_de_index_server_file, IndexServerFile);
+ser_de_test!(qc_ser_de_node_address_file, NodeAddressFile);
+ser_de_test!(qc_ser_de_relay_address_file, RelayAddressFile);
+ser_de_test!(qc_ser_de_trusted_app_file, TrustedAppFile);
+
+/*
 #[test]
 fn qc_ser_de_funder_state_json() {
     fn ser_de(state: FunderState<NetAddress>) -> bool {
@@ -22,6 +59,7 @@ fn qc_ser_de_funder_state_json() {
         .max_tests(100)
         .quickcheck(ser_de as fn(FunderState<NetAddress>) -> bool);
 }
+*/
 
 // TODO: Add serialization tests for proto::file
 // Possibly create a macro to do all the boilerplate for quickcheck tests.
