4a13e451615f10f0348ae32abde182c555d44b16
real <real@freedomlayer.org>
2020-03-31 20:35:46 +0300

    ci: cargo-config.sh: Attempt to not use

--- ci/pre/cargo-config.sh
+++ ci/pre/cargo-config.sh
@@ -3,14 +3,14 @@
 # See: https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -eux -o pipefail
 
-if [[ -f ".cargo/config" ]]; then
-    rm .cargo/config
-elif [[ ! -d ".cargo" ]]; then
-    mkdir .cargo
-fi
+#       if [[ -f ".cargo/config" ]]; then
+#           rm .cargo/config
+#       elif [[ ! -d ".cargo" ]]; then
+#           mkdir .cargo
+#       fi
 
-echo "[target.$TARGET]" > .cargo/config
-echo "linker= \"$CC\"" >> .cargo/config
+# echo "[target.$TARGET]" > .cargo/config
+# echo "linker= \"$CC\"" >> .cargo/config
 
 rustup update
 rustup component add clippy rustfmt
