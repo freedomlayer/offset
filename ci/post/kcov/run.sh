b2a9c976a9e2b35df66f7f0afe542ed1cdbdb4da
real <real@freedomlayer.org>
2020-04-01 12:15:49 +0300

    ci: Added RUST_LOG to kcov/run.sh

--- ci/post/kcov/run.sh
+++ ci/post/kcov/run.sh
@@ -5,6 +5,9 @@
 # See: https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -eux -o pipefail
 
+# Show rust logs:
+export RUST_LOG=warn
+
 exes=$(find target/${TARGET}/debug -maxdepth 1 -executable -type f)
 for exe in ${exes}; do
     ${HOME}/install/kcov-${TARGET}/bin/kcov \
