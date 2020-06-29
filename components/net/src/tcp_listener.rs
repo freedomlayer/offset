fb6d5dc3e9972e89c0fecf3636b85881592784b9
real <real@freedomlayer.org>
2020-06-29 16:30:45 +0300

    net: Using async style listener

--- components/net/src/tcp_listener.rs
+++ components/net/src/tcp_listener.rs
@@ -1,3 +1,4 @@
+use std::io;
 use std::net::SocketAddr;
 
 use async_std::net::TcpListener as AsyncStdTcpListener;
@@ -7,7 +8,7 @@ use futures::task::{Spawn, SpawnExt};
 use futures::{SinkExt, StreamExt};
 
 use crate::utils::tcp_stream_to_conn_pair;
-use common::conn::{ConnPairVec, Listener};
+use common::conn::{BoxFuture, ConnPairVec, Listener, ListenerClient};
 
 /// Listen for incoming TCP connections
 pub struct TcpListener<S> {
@@ -24,47 +25,58 @@ impl<S> TcpListener<S> {
     }
 }
 
+#[derive(Debug)]
+pub enum TcpListenerError {
+    BindError(SocketAddr, io::Error),
+    SpawnError,
+}
+
 impl<S> Listener for TcpListener<S>
 where
     S: Spawn + Send + Clone + 'static,
 {
     type Connection = ConnPairVec;
     type Config = ();
+    type Error = TcpListenerError;
     type Arg = SocketAddr;
 
     fn listen(
         self,
         socket_addr: Self::Arg,
-    ) -> (mpsc::Sender<Self::Config>, mpsc::Receiver<Self::Connection>) {
+    ) -> BoxFuture<'static, Result<ListenerClient<Self::Config, Self::Connection>, Self::Error>>
+    {
         let (config_sender, _config_sender_receiver) = mpsc::channel(0);
         let (mut conn_receiver_sender, conn_receiver) = mpsc::channel(0);
 
         let mut c_spawner = self.spawner.clone();
         let c_max_frame_length = self.max_frame_length;
-        let _ = self.spawner.spawn(async move {
-            let listener = match AsyncStdTcpListener::bind(&socket_addr).await {
-                Ok(listener) => listener,
-                Err(e) => {
-                    warn!("Failed listening on {:?}: {:?}", socket_addr, e);
-                    return;
-                }
-            };
-            let mut incoming_conns = listener.incoming();
+        Box::pin(async move {
+            let listener = AsyncStdTcpListener::bind(&socket_addr)
+                .await
+                .map_err(|error| TcpListenerError::BindError(socket_addr, error))?;
 
-            while let Some(Ok(tcp_stream)) = incoming_conns.next().await {
-                info!(
-                    "TcpListener: Incoming connection from: {:?}",
-                    tcp_stream.peer_addr(),
-                );
-                let conn_pair =
-                    tcp_stream_to_conn_pair(tcp_stream, c_max_frame_length, &mut c_spawner);
-                if let Err(e) = conn_receiver_sender.send(conn_pair).await {
-                    warn!("TcpListener::listen(): Send error: {:?}", e);
-                    return;
-                }
-            }
-        });
+            self.spawner
+                .spawn(async move {
+                    let mut incoming_conns = listener.incoming();
+                    while let Some(Ok(tcp_stream)) = incoming_conns.next().await {
+                        info!(
+                            "TcpListener: Incoming connection from: {:?}",
+                            tcp_stream.peer_addr(),
+                        );
+                        let conn_pair =
+                            tcp_stream_to_conn_pair(tcp_stream, c_max_frame_length, &mut c_spawner);
+                        if let Err(e) = conn_receiver_sender.send(conn_pair).await {
+                            warn!("TcpListener::listen(): Send error: {:?}", e);
+                            return;
+                        }
+                    }
+                })
+                .map_err(|_| TcpListenerError::SpawnError)?;
 
-        (config_sender, conn_receiver)
+            Ok(ListenerClient {
+                config_sender,
+                conn_receiver,
+            })
+        })
     }
 }
