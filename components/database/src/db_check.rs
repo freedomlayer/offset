9cdadb961e1a3587b7fc34786052f551704c760b
real <real@freedomlayer.org>
2020-08-04 20:06:26 +0300

    Some foreign keys checks

--- components/database/src/db_check.rs
+++ components/database/src/db_check.rs
@@ -7,43 +7,169 @@ fn create_database(conn: &Connection) -> rusqlite::Result<()> {
     conn.execute(
         "CREATE TABLE funder(
              local_public_key         BLOB NOT NULL PRIMARY KEY
-
-
             );",
         params![],
     )?;
 
     conn.execute(
         "CREATE TABLE friends(
-             friend_public_key         BLOB NOT NULL PRIMARY KEY
+             friend_public_key        BLOB NOT NULL PRIMARY KEY,
+             sent_local_relays        BLOB NOT NULL,
+             name                     TEXT NOT NULL
+            );",
+        params![],
+    )?;
 
+    // TODO: Fix primary key?
+    conn.execute(
+        "CREATE TABLE local_active_currencies(
+             friend_public_key        BLOB NOT NULL,
+             currency                 TEXT NOT NULL,
+             FOREIGN KEY(friend_public_key) 
+                REFERENCES friends(friend_public_key)
+                ON DELETE CASCADE
+            );",
+        params![],
+    )?;
 
+    // TODO: Fix primary key?
+    conn.execute(
+        "CREATE TABLE remote_active_currencies(
+             friend_public_key        BLOB NOT NULL,
+             currency                 TEXT NOT NULL,
+             FOREIGN KEY(friend_public_key) 
+                REFERENCES friends(friend_public_key)
+                ON DELETE CASCADE
+            );",
+        params![],
+    )?;
+
+    // TODO:
+    conn.execute(
+        "CREATE TABLE active_currencies(
+             friend_public_key        BLOB NOT NULL,
+             currency                 TEXT NOT NULL,
+             FOREIGN KEY(friend_public_key, currency) 
+                REFERENCES local_active_currencies(friend_public_key, currency)
+                ON DELETE CASCADE
+             FOREIGN KEY(friend_public_key, currency) 
+                REFERENCES remote_active_currencies(friend_public_key, currency)
+                ON DELETE CASCADE
+            );",
+        params![],
+    )?;
+
+    // TODO:
+    conn.execute(
+        "CREATE TABLE local_pending_transactions(
+             friend_public_key        BLOB NOT NULL,
+             currency                 TEXT NOT NULL,
+             FOREIGN KEY(friend_public_key, currency) 
+                REFERENCES active_currencies(friend_public_key, currency)
+                ON DELETE CASCADE
+            );",
+        params![],
+    )?;
+
+    // TODO:
+    conn.execute(
+        "CREATE TABLE remote_pending_transactions(
+             friend_public_key        BLOB NOT NULL,
+             currency                 TEXT NOT NULL,
+             FOREIGN KEY(friend_public_key, currency) 
+                REFERENCES active_currencies(friend_public_key, currency)
+                ON DELETE CASCADE
+            );",
+        params![],
+    )?;
+
+    conn.execute(
+        "CREATE TABLE currency_configs(
+             friend_public_key          BLOB NOT NULL PRIMARY KEY,
+             currency                   TEXT NOT NULL,
+             rate                       BLOB NOT NULL,
+             remote_max_debt            BLOB NOT NULL,
+             is_open                    BOOL NOT NULL,
+             is_enabled                 BOOL NOT NULL,
+             is_consistent              BOOL NOT NULL,
+             is_token_channel_incoming  BOOL
+            );",
+        params![],
+    )?;
+
+    // TODO:
+    conn.execute(
+        "CREATE TABLE pending_user_requests(
+             friend_public_key        BLOB NOT NULL PRIMARY KEY
+            );",
+        params![],
+    )?;
+
+    // TODO:
+    conn.execute(
+        "CREATE TABLE pending_requests(
+             friend_public_key        BLOB NOT NULL,
+             currency                 BLOB NOT NULL PRIMARY KEY
+            );",
+        params![],
+    )?;
+
+    // TODO:
+    conn.execute(
+        "CREATE TABLE pending_responses(
+             friend_public_key        BLOB NOT NULL PRIMARY KEY
+            );",
+        params![],
+    )?;
+
+    conn.execute(
+        "CREATE TABLE friend_relays(
+             friend_public_key        BLOB NOT NULL,
+             relay_public_key         BLOB NOT NULL,
+             address                  TEXT
+            );",
+        params![],
+    )?;
+
+    conn.execute(
+        "CREATE TABLE relays(
+             relay_public_key         BLOB NOT NULL PRIMARY KEY,
+             address                  TEXT,
+             name                     TEXT
+            );",
+        params![],
+    )?;
+
+    conn.execute(
+        "CREATE TABLE index_servers(
+             index_public_key         BLOB NOT NULL PRIMARY KEY,
+             address                  TEXT,
+             name                     TEXT
             );",
         params![],
     )?;
 
     conn.execute(
         "CREATE TABLE currencies(
-             public_key         BLOB NOT NULL PRIMARY KEY
-
+             friend_public_key         BLOB NOT NULL PRIMARY KEY
 
             );",
         params![],
     )?;
 
+    // TODO:
     conn.execute(
         "CREATE TABLE payments(
-             uid         BLOB NOT NULL PRIMARY KEY
-
+             counter         BLOB NOT NULL PRIMARY KEY
 
             );",
         params![],
     )?;
 
+    // TODO:
     conn.execute(
         "CREATE TABLE invoices (
-             uid         BLOB NOT NULL PRIMARY KEY
-
+             counter         BLOB NOT NULL PRIMARY KEY
 
             );",
         params![],
@@ -66,6 +192,8 @@ mod tests {
 
     fn inner_db_check() -> rusqlite::Result<()> {
         let conn = Connection::open_in_memory()?;
+
+        create_database(&conn).unwrap();
 
         conn.execute(
             "CREATE TABLE person (
