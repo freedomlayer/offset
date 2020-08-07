7b197d1f02f6758ebd2edbe4fd36a2452a292df9
real <real@freedomlayer.org>
2020-08-07 14:13:50 +0300

    db: Added external transaction + some work on tables

--- components/database/src/db_check.rs
+++ components/database/src/db_check.rs
@@ -1,10 +1,17 @@
 use rusqlite::{self, params, Connection};
 
+const TC_CONSISTENT_IN: &str = &"C-IN";
+const TC_CONSISTENT_OUT: &str = &"C-OUT";
+const TC_INCONSISTENT: &str = &"I";
+
 #[allow(unused)]
-fn create_database(conn: &Connection) -> rusqlite::Result<()> {
-    // A single row table. TODO: How to enforce a single row?
-    // See also: https://stackoverflow.com/questions/2300356/using-a-single-row-configuration-table-in-sql-server-database-bad-idea
-    conn.execute(
+fn create_database(conn: &mut Connection) -> rusqlite::Result<()> {
+    let tx = conn.transaction()?;
+    // TODO: A better way to do this?
+    tx.execute("PRAGMA foreign_keys = ON;", params![])?;
+
+    // Single row is enforced in this table according to https://stackoverflow.com/a/33104119
+    tx.execute(
         "CREATE TABLE funder(
              id                       INTEGER PRIMARY KEY CHECK (id = 0), -- enforce single row
              local_public_key         BLOB NOT NULL
@@ -12,41 +19,94 @@ fn create_database(conn: &Connection) -> rusqlite::Result<()> {
         params![],
     )?;
 
-    conn.execute(
+    tx.execute(
+        "CREATE TABLE token_channel_statuses(
+            status                      TEXT PRIMARY KEY
+        );",
+        params![],
+    )?;
+
+    // Initialize token_channel_status table:
+    for value in &[TC_CONSISTENT_IN, TC_CONSISTENT_OUT, TC_INCONSISTENT] {
+        tx.execute(
+            "INSERT INTO token_channel_statuses VALUES (?1);",
+            params![value],
+        )?;
+    }
+
+    // TODO: Add index on primary key?
+    tx.execute(
         "CREATE TABLE friends(
-             friend_public_key        BLOB NOT NULL PRIMARY KEY,
-             sent_local_relays        BLOB NOT NULL,
-             name                     TEXT NOT NULL
+             friend_public_key          BLOB NOT NULL PRIMARY KEY,
+             sent_local_relays          BLOB NOT NULL,
+             -- Last local relays we have sent to the friend
+             remote_relays              BLOB NOT NULL,
+             -- Friend's relays
+             name                       TEXT NOT NULL,
+             -- Friend's name
+             is_enabled                 BOOL NOT NULL,
+             -- Do we allow connectivity with this friend?
+             is_consistent              BOOL NOT NULL,
+             -- Is the channel with this friend consistent?
+             token_channel_status       TEXT NOT NULL,
+             -- Current status of the token channel
+             FOREIGN KEY(token_channel_status)
+                REFERENCES token_channel_statuses(status)
             );",
         params![],
     )?;
 
-    // TODO: Fix primary key?
-    conn.execute(
+    // TODO: Make sure that consistent and inconsistent channels tables are disjoint, but still
+    // refer to the friends table. Add constraints to make sure that when an item is removed from
+    // the consistent_channels table, active currencies will also be affected.
+
+    tx.execute(
+        "CREATE TABLE consistent_channels(
+             friend_public_key          BLOB NOT NULL PRIMARY KEY,
+             is_incoming                BOOL NOT NULL,
+             FOREIGN KEY(friend_public_key)
+                REFERENCES friends(friend_public_key)
+            );",
+        params![],
+    )?;
+
+    tx.execute(
+        "CREATE TABLE inconsistent_channels(
+             friend_public_key          BLOB NOT NULL PRIMARY KEY,
+             is_incoming                BOOL NOT NULL,
+             FOREIGN KEY(friend_public_key)
+                REFERENCES friends(friend_public_key)
+            );",
+        params![],
+    )?;
+
+    // Should only exist if relevant token channel is alive
+    tx.execute(
         "CREATE TABLE local_active_currencies(
              friend_public_key        BLOB NOT NULL,
              currency                 TEXT NOT NULL,
              FOREIGN KEY(friend_public_key) 
                 REFERENCES friends(friend_public_key)
-                ON DELETE CASCADE
+                ON DELETE CASCADE,
+             PRIMARY KEY(friend_public_key, currency)
             );",
         params![],
     )?;
 
-    // TODO: Fix primary key?
-    conn.execute(
+    tx.execute(
         "CREATE TABLE remote_active_currencies(
              friend_public_key        BLOB NOT NULL,
              currency                 TEXT NOT NULL,
              FOREIGN KEY(friend_public_key) 
                 REFERENCES friends(friend_public_key)
-                ON DELETE CASCADE
+                ON DELETE CASCADE,
+             PRIMARY KEY(friend_public_key, currency)
             );",
         params![],
     )?;
 
     // TODO:
-    conn.execute(
+    tx.execute(
         "CREATE TABLE active_currencies(
              friend_public_key        BLOB NOT NULL,
              currency                 TEXT NOT NULL,
@@ -61,7 +121,7 @@ fn create_database(conn: &Connection) -> rusqlite::Result<()> {
     )?;
 
     // TODO:
-    conn.execute(
+    tx.execute(
         "CREATE TABLE local_pending_transactions(
              friend_public_key        BLOB NOT NULL,
              currency                 TEXT NOT NULL,
@@ -73,7 +133,7 @@ fn create_database(conn: &Connection) -> rusqlite::Result<()> {
     )?;
 
     // TODO:
-    conn.execute(
+    tx.execute(
         "CREATE TABLE remote_pending_transactions(
              friend_public_key        BLOB NOT NULL,
              currency                 TEXT NOT NULL,
@@ -84,22 +144,19 @@ fn create_database(conn: &Connection) -> rusqlite::Result<()> {
         params![],
     )?;
 
-    conn.execute(
+    tx.execute(
         "CREATE TABLE currency_configs(
              friend_public_key          BLOB NOT NULL PRIMARY KEY,
              currency                   TEXT NOT NULL,
              rate                       BLOB NOT NULL,
              remote_max_debt            BLOB NOT NULL,
-             is_open                    BOOL NOT NULL,
-             is_enabled                 BOOL NOT NULL,
-             is_consistent              BOOL NOT NULL,
-             is_token_channel_incoming  BOOL
+             is_open                    BOOL NOT NULL
             );",
         params![],
     )?;
 
     // TODO:
-    conn.execute(
+    tx.execute(
         "CREATE TABLE pending_user_requests(
              friend_public_key        BLOB NOT NULL PRIMARY KEY
             );",
@@ -107,7 +164,7 @@ fn create_database(conn: &Connection) -> rusqlite::Result<()> {
     )?;
 
     // TODO:
-    conn.execute(
+    tx.execute(
         "CREATE TABLE pending_requests(
              friend_public_key        BLOB NOT NULL,
              currency                 BLOB NOT NULL PRIMARY KEY
@@ -116,14 +173,14 @@ fn create_database(conn: &Connection) -> rusqlite::Result<()> {
     )?;
 
     // TODO:
-    conn.execute(
+    tx.execute(
         "CREATE TABLE pending_responses(
              friend_public_key        BLOB NOT NULL PRIMARY KEY
             );",
         params![],
     )?;
 
-    conn.execute(
+    tx.execute(
         "CREATE TABLE friend_relays(
              friend_public_key        BLOB NOT NULL,
              relay_public_key         BLOB NOT NULL,
@@ -132,7 +189,7 @@ fn create_database(conn: &Connection) -> rusqlite::Result<()> {
         params![],
     )?;
 
-    conn.execute(
+    tx.execute(
         "CREATE TABLE relays(
              relay_public_key         BLOB NOT NULL PRIMARY KEY,
              address                  TEXT,
@@ -141,7 +198,7 @@ fn create_database(conn: &Connection) -> rusqlite::Result<()> {
         params![],
     )?;
 
-    conn.execute(
+    tx.execute(
         "CREATE TABLE index_servers(
              index_public_key         BLOB NOT NULL PRIMARY KEY,
              address                  TEXT,
@@ -150,7 +207,7 @@ fn create_database(conn: &Connection) -> rusqlite::Result<()> {
         params![],
     )?;
 
-    conn.execute(
+    tx.execute(
         "CREATE TABLE currencies(
              friend_public_key         BLOB NOT NULL PRIMARY KEY
 
@@ -159,7 +216,7 @@ fn create_database(conn: &Connection) -> rusqlite::Result<()> {
     )?;
 
     // TODO:
-    conn.execute(
+    tx.execute(
         "CREATE TABLE payments(
              counter         BLOB NOT NULL PRIMARY KEY
 
@@ -168,7 +225,7 @@ fn create_database(conn: &Connection) -> rusqlite::Result<()> {
     )?;
 
     // TODO:
-    conn.execute(
+    tx.execute(
         "CREATE TABLE invoices (
              counter         BLOB NOT NULL PRIMARY KEY
 
@@ -176,7 +233,7 @@ fn create_database(conn: &Connection) -> rusqlite::Result<()> {
         params![],
     )?;
 
-    Ok(())
+    tx.commit()
 }
 
 #[cfg(test)]
@@ -192,9 +249,9 @@ mod tests {
     }
 
     fn inner_db_check() -> rusqlite::Result<()> {
-        let conn = Connection::open_in_memory()?;
+        let mut conn = Connection::open_in_memory()?;
 
-        create_database(&conn).unwrap();
+        create_database(&mut conn).unwrap();
 
         conn.execute(
             "CREATE TABLE person (
