a3d2ce2032efd0a552a096f4e78ca98ffed3eee3
real <real@freedomlayer.org>
2020-09-29 12:26:43 +0300

    database: Updated comment

--- components/database/src/create.rs
+++ components/database/src/create.rs
@@ -676,7 +676,7 @@ fn create_database(conn: &mut Connection) -> rusqlite::Result<()> {
     )?;
 
     // Balances (per currency) in case of friend inconsistency event.
-    // Friend inconsistency can be one of: 1. Channel reset, 2. Friend removal
+    // Friend inconsistency can be one of: 1. Channel reset, 2. Friend removal, 3. currency removal
     tx.execute(
         "CREATE TABLE friend_inconsistency_event_balances (
              counter                BLOB NOT NULL,
