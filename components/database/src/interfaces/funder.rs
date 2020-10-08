c1939c79615cc608a30ea7512ffc153d452ee6e9
real <real@freedomlayer.org>
2020-10-08 12:59:39 +0300

    database: Some design work on FunderDbClient

--- components/database/src/interfaces/funder.rs
+++ components/database/src/interfaces/funder.rs
@@ -4,6 +4,8 @@ use proto::app_server::messages::{NamedRelayAddress, RelayAddress};
 use proto::crypto::PublicKey;
 use proto::funder::messages::{Currency, Rate};
 
+use futures::channel::{mpsc, oneshot};
+
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub enum SentLocalRelays<B> {
     NeverSent,
@@ -25,45 +27,93 @@ pub struct CurrencyConfig {
 
 pub enum FunderDbError {}
 
-// TODO: Remove unused hint:
-#[allow(unused)]
 pub type FunderDbResult<T> = Result<T, FunderDbError>;
+pub type SenderResult<T> = oneshot::Sender<FunderDbResult<T>>;
 
-pub trait FunderDb<B> {
-    fn local_public_key() -> PublicKey;
-    fn get_friend_remote_relays(
+// TODO: Remove this unused hint later:
+#[allow(unused)]
+pub enum FunderDbOp<B> {
+    GetLocalPublicKey(oneshot::Sender<FunderDbResult<PublicKey>>),
+    GetFriendIsEnabled(PublicKey, SenderResult<bool>),
+    SetFriendIsEnabled(PublicKey, bool, SenderResult<()>),
+    GetFriendRemoteRelays(PublicKey, SenderResult<Vec<RelayAddress<B>>>),
+    SetFriendRemoteRelays(PublicKey, Vec<RelayAddress<B>>, SenderResult<()>),
+    GetFriendSentLocalRelays(PublicKey, SenderResult<Vec<RelayAddress<B>>>),
+    SetFriendSentLocalRelays(PublicKey, Vec<RelayAddress<B>>, SenderResult<()>),
+    GetFriendName(PublicKey, SenderResult<String>),
+    SetFriendName(PublicKey, String, SenderResult<()>),
+    GetCurrencyConfig(PublicKey, SenderResult<Currency>),
+    SetFriendCurrencyRate(PublicKey, Currency, Rate, SenderResult<()>),
+    SetFriendCurrencyRemoteMaxDebt(PublicKey, Currency, u128, SenderResult<()>),
+    SetFriendCurrencyIsOpen(PublicKey, Currency, bool, SenderResult<()>),
+}
+
+// TODO: Remove this unused hint later:
+#[allow(unused)]
+pub struct FunderDbClient<B> {
+    sender: mpsc::Sender<FunderDbOp<B>>,
+}
+
+// TODO: Remove this unused hint later:
+#[allow(unused)]
+impl<B> FunderDbClient<B> {
+    pub async fn local_public_key() -> PublicKey {
+        unimplemented!();
+    }
+    pub async fn get_friend_remote_relays(
         friend_public_key: PublicKey,
-    ) -> FunderDbResult<Vec<RelayAddress<B>>>;
-    fn set_friend_remote_relays(
+    ) -> FunderDbResult<Vec<RelayAddress<B>>> {
+        unimplemented!();
+    }
+    pub async fn set_friend_remote_relays(
         friend_public_key: PublicKey,
         remote_relays: Vec<RelayAddress<B>>,
-    ) -> FunderDbResult<()>;
-    fn get_friend_sent_local_relays(
+    ) -> FunderDbResult<()> {
+        unimplemented!();
+    }
+    pub async fn get_friend_sent_local_relays(
         friend_public_key: PublicKey,
-    ) -> FunderDbResult<SentLocalRelays<B>>;
-    fn set_friend_sent_local_relays(
+    ) -> FunderDbResult<SentLocalRelays<B>> {
+        unimplemented!();
+    }
+    pub async fn set_friend_sent_local_relays(
         friend_public_key: PublicKey,
         sent_local_relays: SentLocalRelays<B>,
-    ) -> FunderDbResult<()>;
-    fn get_friend_name(friend_public_key: PublicKey) -> FunderDbResult<String>;
-    fn set_friend_name(friend_public_key: PublicKey, name: String) -> FunderDbResult<()>;
-    fn get_friend_currency_config(
+    ) -> FunderDbResult<()> {
+        unimplemented!();
+    }
+    pub async fn get_friend_name(friend_public_key: PublicKey) -> FunderDbResult<String> {
+        unimplemented!();
+    }
+    pub async fn set_friend_name(friend_public_key: PublicKey, name: String) -> FunderDbResult<()> {
+        unimplemented!();
+    }
+    pub async fn get_friend_currency_config(
         friend_public_key: PublicKey,
         currency: Currency,
-    ) -> FunderDbResult<CurrencyConfig>;
-    fn set_friend_currency_rate(
+    ) -> FunderDbResult<CurrencyConfig> {
+        unimplemented!();
+    }
+    pub async fn set_friend_currency_rate(
         friend_public_key: PublicKey,
         currency: Currency,
         rate: Rate,
-    ) -> FunderDbResult<()>;
-    fn set_friend_currency_remote_max_debt(
+    ) -> FunderDbResult<()> {
+        unimplemented!();
+    }
+    pub async fn set_friend_currency_remote_max_debt(
         friend_public_key: PublicKey,
         currency: Currency,
         remote_max_debt: u128,
-    ) -> FunderDbResult<()>;
-    fn set_friend_currency_is_open(
+    ) -> FunderDbResult<()> {
+        unimplemented!();
+    }
+
+    async fn set_friend_currency_is_open(
         friend_public_key: PublicKey,
         currency: Currency,
         is_open: bool,
-    ) -> FunderDbResult<()>;
+    ) -> FunderDbResult<()> {
+        unimplemented!();
+    }
 }
