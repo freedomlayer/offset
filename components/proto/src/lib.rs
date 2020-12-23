e89bc267d5016c7857993b16055069ef459e3f28
real <real@freedomlayer.org>
2020-12-23 19:23:22 +0200

    proto: Removed old comments

--- components/proto/src/lib.rs
+++ components/proto/src/lib.rs
@@ -13,8 +13,6 @@ extern crate quickcheck_derive;
 // Workaround for issue: https://github.com/rust-lang/rust/issues/64450
 extern crate offset_mutual_from as mutual_from;
 
-// #[macro_use]
-// pub mod macros;
 pub mod app_server;
 pub mod consts;
 pub mod crypto;
@@ -24,20 +22,7 @@ pub mod index_client;
 pub mod index_server;
 pub mod keepalive;
 pub mod net;
-// pub mod proto_ser;
 pub mod relay;
 // pub mod report;
 pub mod secure_channel;
 pub mod ser_string;
-// pub mod wrapper;
-
-/*
-// include_schema!(report_capnp, "report_capnp");
-include_schema!(app_server_capnp, "app_server_capnp");
-include_schema!(common_capnp, "common_capnp");
-include_schema!(dh_capnp, "dh_capnp");
-include_schema!(relay_capnp, "relay_capnp");
-// include_schema!(funder_capnp, "funder_capnp");
-include_schema!(keepalive_capnp, "keepalive_capnp");
-include_schema!(index_capnp, "index_capnp");
-*/
