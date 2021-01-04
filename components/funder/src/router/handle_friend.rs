feef07e8849f12b186cf60ca2bcb4b97736c7787
real <real@freedomlayer.org>
2021-01-04 15:12:26 +0200

    funder: Work on incoming_move_token_request()

--- components/funder/src/router/handle_friend.rs
+++ components/funder/src/router/handle_friend.rs
@@ -28,7 +28,9 @@ use crate::router::types::{
     BackwardsOp, CurrencyInfo, RouterDbClient, RouterError, RouterOutput, RouterState, SentRelay,
 };
 
-use crate::mutual_credit::incoming::IncomingMessage;
+use crate::mutual_credit::incoming::{
+    IncomingCancelSendFundsOp, IncomingMessage, IncomingResponseSendFundsOp,
+};
 use crate::router::utils::index_mutation::{
     create_index_mutations_from_move_token, create_update_index_mutation,
 };
@@ -38,6 +40,50 @@ use crate::router::utils::move_token::{
 use crate::token_channel::{
     handle_in_move_token, ReceiveMoveTokenOutput, TcDbClient, TcStatus, TokenChannelError,
 };
+
+async fn incoming_message_request<RC>(
+    mut router_db_client: &mut RC,
+    router_state: &mut RouterState,
+    friend_public_key: PublicKey,
+    identity_client: &mut IdentityClient,
+    router_output: &mut RouterOutput,
+    request_send_funds: RequestSendFundsOp,
+) {
+    todo!();
+}
+
+async fn incoming_message_request_cancel<RC>(
+    mut router_db_client: &mut RC,
+    router_state: &mut RouterState,
+    friend_public_key: PublicKey,
+    identity_client: &mut IdentityClient,
+    router_output: &mut RouterOutput,
+    request_send_funds: RequestSendFundsOp,
+) {
+    todo!();
+}
+
+async fn incoming_message_response<RC>(
+    mut router_db_client: &mut RC,
+    router_state: &mut RouterState,
+    friend_public_key: PublicKey,
+    identity_client: &mut IdentityClient,
+    router_output: &mut RouterOutput,
+    incoming_response: IncomingResponseSendFundsOp,
+) {
+    todo!();
+}
+
+async fn incoming_message_cancel<RC>(
+    mut router_db_client: &mut RC,
+    router_state: &mut RouterState,
+    friend_public_key: PublicKey,
+    identity_client: &mut IdentityClient,
+    router_output: &mut RouterOutput,
+    cancel_send_funds: IncomingCancelSendFundsOp,
+) {
+    todo!();
+}
 
 async fn incoming_move_token_request<RC>(
     mut router_db_client: &mut RC,
@@ -110,10 +156,50 @@ where
         ReceiveMoveTokenOutput::Received(move_token_received) => {
             for (currency, incoming_message) in move_token_received.incoming_messages {
                 match incoming_message {
-                    IncomingMessage::Request(request_send_funds) => todo!(),
-                    IncomingMessage::RequestCancel(request_send_funds) => todo!(),
-                    IncomingMessage::Response(response_send_funds) => todo!(),
-                    IncomingMessage::Cancel(cancel_send_funds) => todo!(),
+                    IncomingMessage::Request(request_send_funds) => {
+                        incoming_message_request(
+                            router_db_client,
+                            router_state,
+                            friend_public_key.clone(),
+                            identity_client,
+                            &mut output,
+                            request_send_funds,
+                        )
+                        .await
+                    }
+                    IncomingMessage::RequestCancel(request_send_funds) => {
+                        incoming_message_request_cancel(
+                            router_db_client,
+                            router_state,
+                            friend_public_key.clone(),
+                            identity_client,
+                            &mut output,
+                            request_send_funds,
+                        )
+                        .await
+                    }
+                    IncomingMessage::Response(incoming_response) => {
+                        incoming_message_response(
+                            router_db_client,
+                            router_state,
+                            friend_public_key.clone(),
+                            identity_client,
+                            &mut output,
+                            incoming_response,
+                        )
+                        .await
+                    }
+                    IncomingMessage::Cancel(incoming_cancel) => {
+                        incoming_message_cancel(
+                            router_db_client,
+                            router_state,
+                            friend_public_key.clone(),
+                            identity_client,
+                            &mut output,
+                            incoming_cancel,
+                        )
+                        .await
+                    }
                 }
             }
             todo!();
