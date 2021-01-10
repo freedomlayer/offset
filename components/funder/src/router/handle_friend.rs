94dbe8b553f38c20461cabe942636b36ad708556
real <real@freedomlayer.org>
2021-01-10 15:41:53 +0200

    funder: Removed unused imports

--- components/funder/src/router/handle_friend.rs
+++ components/funder/src/router/handle_friend.rs
@@ -32,9 +32,7 @@ use crate::mutual_credit::incoming::{
     IncomingCancelSendFundsOp, IncomingMessage, IncomingResponseSendFundsOp,
 };
 use crate::router::utils::flush::flush_friend;
-use crate::router::utils::index_mutation::{
-    create_index_mutations_from_outgoing_move_token, create_update_index_mutation,
-};
+use crate::router::utils::index_mutation::create_update_index_mutation;
 use crate::router::utils::move_token::{
     collect_outgoing_move_token, collect_outgoing_move_token_allow_empty, is_pending_move_token,
 };
--- components/funder/src/router/handle_route.rs
+++ components/funder/src/router/handle_route.rs
@@ -28,9 +28,7 @@ use crate::router::types::{
     BackwardsOp, CurrencyInfo, RouterDbClient, RouterError, RouterOutput, RouterState, SentRelay,
 };
 use crate::router::utils::flush::flush_friend;
-use crate::router::utils::index_mutation::{
-    create_index_mutations_from_outgoing_move_token, create_update_index_mutation,
-};
+use crate::router::utils::index_mutation::create_update_index_mutation;
 use crate::router::utils::move_token::{collect_outgoing_move_token, is_pending_move_token};
 use crate::token_channel::{
     handle_in_move_token, ReceiveMoveTokenOutput, TcDbClient, TcStatus, TokenChannelError,
