9e8b4bc022c2ab16f938bee028a30ffaf2af7150
real <real@freedomlayer.org>
2021-02-05 16:21:58 +0200

    proto: Removed old comment

--- components/proto/src/funder/messages.rs
+++ components/proto/src/funder/messages.rs
@@ -210,7 +210,6 @@ pub struct McBalance {
     /// Frozen credits by the remote side
     #[serde(with = "ser_string")]
     pub remote_pending_debt: u128,
-    // TODO: in_fees and out_fees should be u256, not u128!
     /// Fees that were received from remote side
     #[serde(with = "ser_string")]
     pub in_fees: U256,
