bc14a3c1b4ef0e7127bc0510d3c48f78bfd27fb0
real <real@freedomlayer.org>
2021-02-06 23:19:03 +0200

    funder: Removed todo!() statement from calc_capacities()

--- components/funder/src/router/utils/index_mutation.rs
+++ components/funder/src/router/utils/index_mutation.rs
@@ -29,12 +29,7 @@ use crate::token_channel::{TcDbClient, TcStatus, TokenChannelError};
 /// Calculate send and receive capacity for a certain currency
 /// This is the number we are going to report to an index server
 pub fn calc_capacities(currency_info: &CurrencyInfo) -> Result<(u128, u128), RouterError> {
-    // TODO:
-    // Should also take into account:
-    // - Liveness
-    // - Open/Closed currencies
-    // Maybe from the outside?
-    todo!();
+    // TODO: Should also take into account liveness?  Maybe from the outside?
 
     if !currency_info.is_open {
         return Ok((0, 0));
