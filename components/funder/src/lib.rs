54ca891dae2a59935d4d0deeffb21bdfdb6e9d94
Dan Cîrnaț <dan@alt.md>
2021-11-07 16:56:39 +0100

    Fix build with latest rust

--- components/funder/src/lib.rs
+++ components/funder/src/lib.rs
@@ -1,7 +1,7 @@
 #![crate_type = "lib"]
 #![cfg_attr(not(feature = "cargo-clippy"), allow(unknown_lints))]
 #![deny(trivial_numeric_casts, warnings)]
-#![allow(intra_doc_link_resolution_failure)]
+#![allow(allow_broken_intra_doc_links)]
 #![allow(
     clippy::too_many_arguments,
     clippy::implicit_hasher,
--- components/relay/src/lib.rs
+++ components/relay/src/lib.rs
@@ -1,7 +1,7 @@
 #![crate_type = "lib"]
 #![type_length_limit = "291239"]
 #![deny(trivial_numeric_casts, warnings)]
-#![allow(intra_doc_link_resolution_failure)]
+#![allow(broken_intra_doc_links)]
 #![allow(
     clippy::too_many_arguments,
     clippy::implicit_hasher,
